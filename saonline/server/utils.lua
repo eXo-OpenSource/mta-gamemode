15e1ed8d76c2b8a51e1ea6b012c8109d188fa290
Heaven <Heaven@c081b6c7-cb2b-41e9-843e-d4d68840ecf0>
2013-11-05 17:55:35 +0000

    Changed Gamemode Name

--- saonline/server/utils.lua
+++ saonline/server/utils.lua
@@ -1,6 +1,6 @@
 function critical_error(errmsg)
 	outputDebugString("[CRITICAL ERROR] "..tostring(errmsg))
-	outputDebugString("[CRITICAL ERROR] DayZ Script will now halt")
+	outputDebugString("[CRITICAL ERROR] GTA:SA Online Script will now halt")
 	outputDebugString("[CRITICAL ERROR] If you cannot solve this issue please report at fixme: forumurl ")
 	stopResource(getThisResource())
 	error("Critical Error")
