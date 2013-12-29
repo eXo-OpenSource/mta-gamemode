d1cebcb222d3a7389fe58361ef01075d86b160de
sbx320 <sbx320@c081b6c7-cb2b-41e9-843e-d4d68840ecf0>
2013-12-29 17:45:51 +0000

    added InventoryGUI.lua added /dcreload

--- saonline/client/classes/Debugging.lua
+++ saonline/client/classes/Debugging.lua
@@ -11,11 +11,33 @@ if DEBUG then
 
 	function Debugging:constructor()
 		addCommandHandler("dcrun", bind(Debugging.runString, self))
+		addCommandHandler("dcreload", bind(Debugging.reloadClass, self))
 	end
 
 	function Debugging:runString(cmd, ...)
 		local codeString = table.concat({...}, " ")
 		runString(codeString, root, player)
 	end
-
+	
+	function Debugging:reloadClass(cmd, file)
+		if not fileExists(file) then 
+			outputChatBox("file does not exist")
+		else
+			outputChatBox("reloading " .. file)
+			local fh = fileOpen(file)
+			local data = fileRead(fh, fileGetSize(fh))
+			fileClose(fh)
+			local fn, err = loadstring(data)
+			if not fn then
+				outputChatBox("Error Compiling "..err)
+				return
+			end
+			status, err = pcall(fn)
+			if not status then
+				outputChatBox("Error Launching "..err)
+				return
+			end
+			outputChatBox("Successfully reloaded")
+		end
+	end
 end
