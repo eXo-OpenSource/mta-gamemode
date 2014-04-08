2700363734bcdee71c5d0205747717f58b9eef04
revelse <revelse@c081b6c7-cb2b-41e9-843e-d4d68840ecf0>
2014-04-08 16:36:07 +0000

    Started AmmuNation  + AmmuNation.lua  + AmmuNationManager.lua

---
+++ vrp/server/classes/AmmuNation.lua
@@ -0,0 +1,23 @@
+AmmuNation = inherit(Object)
+
+AmmuNation.INTERIORID = 0
+AmmuNation.ENTERPOS = { X = 0, Y = 0, Z = 0 }
+
+function AmmuNation:constructor(name)
+	self.m_Name = name or "NO NAME"
+	
+	addEventHandler("onPlayerWeaponBuy",root,bind(self.buyWeapon,self))
+	addEventHandler("onPlayerMagazineBuy",root,bind(self.buyMagazine,self))
+end
+
+function AmmuNation:addEnter(x,y,z,dimension)
+	local instance = new(InteriorEnterExit,{X=x,Y=y,Z=z},{X=AmmuNation.ENTERPOS[1],Y=AmmuNation.ENTERPOS[2],Z=AmmuNation.ENTERPOS[3]},AmmuNation.INTERIORID,dimension)
+	
+	addEventHandler ("onMarkerHit",instance:getEnterMarker(),
+		function(hitElement,matchingDimension)
+			if matchingDimension and not isPedInVehicle(hitElement) then
+				outputChatBox(("Welcome %s, in the Ammu Nation \"%s\""):format(getPlayerName(hitElement),self.m_Name),hitElement,255,255,255,false)
+			end
+		end
+	)
+end
---
+++ vrp/server/classes/AmmuNationManager.lua
@@ -0,0 +1,28 @@
+AmmuNationManager = inherit(Singleton)
+
+AmmuNationManager.DATA = {
+	[1] = {
+		NAME = "Los Santos Main",
+		ENTER = 
+		{
+			{0,0,0}
+		},
+		DIMENSION = 0,
+	},
+}
+
+function AmmuNationManager:constructor()
+	self.m_AmmuNations = {}
+	
+	addEvent("onPlayerWeaponBuy",true)
+	addEvent("onPlayerMagazinBuy",true)
+	
+	for key, value in ipairs(AmmuNationManager.DATA) do
+		table.insert(self.m_AmmuNations,new(AmmuNation,value.NAME))
+		local instance = self.m_AmmuNations[#self.m_AmmuNations]
+		for k, coords in ipairs(v.ENTER) do
+			instance:addEnter(coords[1],coords[2],coods[3],value.DIMENSION)
+		end
+	end
+	
+end
