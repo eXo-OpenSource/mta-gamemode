3d37b65dc3f2cba9ee772237a455119b58b4d092
stivik <stivik@c081b6c7-cb2b-41e9-843e-d4d68840ecf0>
2015-02-07 10:37:09 +0000

    Changes @ShortMessage: - if the text is longer then the bounding box, the box heigh is getting higher (The Position for new boxes is currently not correct)

--- vrp/client/classes/GUI/MessageBoxes/ShortMessage.lua
+++ vrp/client/classes/GUI/MessageBoxes/ShortMessage.lua
@@ -8,10 +8,12 @@
 ShortMessage = inherit(DxElement)
 inherit(GUIFontContainer, ShortMessage)
 
+ShortMessage.compSize = 35
 ShortMessage.MessageQueue = {}
 
 function ShortMessage:constructor(text)
-	DxElement.constructor(self, 20, screenHeight - screenHeight*0.265 - 35 * (#ShortMessage.MessageQueue+1), 340*screenWidth/1600+6, 30)
+	--DxElement.constructor(self, 20, screenHeight - screenHeight*0.265 - 35 * (#ShortMessage.MessageQueue+1), 340*screenWidth/1600+6, 30)
+    DxElement.constructor(self, 20, screenHeight - screenHeight*0.265 - ShortMessage.compSize, 340*screenWidth/1600+6, 30)
 	GUIFontContainer.constructor(self, text, 1.4, "default")
 	
 	table.insert(ShortMessage.MessageQueue, self)
@@ -26,20 +28,46 @@ function ShortMessage:destructor()
 end
 
 function ShortMessage:drawThis()
+    local x, y, w, h = self.m_AbsoluteX, self.m_AbsoluteY, self.m_Width, self.m_Height
+
+
+    self.m_Lines = math.floor(dxGetTextWidth(self.m_Text, self.m_FontSize, self.m_Font)/self.m_Width) + 1
+    outputChatBox(self.m_Lines)
+
+    -- Calculate heigth
+    h = (h * self.m_Lines) - (15 * math.floor(self.m_Lines/2))
+
+    if self.m_Lines > 1 then
+        y = y - h + 25
+    end
+
 	-- Draw background
-	dxDrawRectangle(self.m_AbsoluteX, self.m_AbsoluteY, self.m_Width, self.m_Height, tocolor(0, 0, 0, 200))
-	
+	dxDrawRectangle(x, y, w, h, tocolor(0, 0, 0, 200))
+
+
+    -- Center the text
+    x = x + 5
+    w = w - 10
+
+    -- Draw the text bounding box (DEBUG)
+    --[[
+    dxDrawLine(x, y, x + w, y, Color.White, 1)
+    dxDrawLine(x, y, x, y + h, Color.White, 1)
+    dxDrawLine(x, y + h, x + w, y + h, Color.White, 1)
+    dxDrawLine(x + w, y, x + w, y + h, Color.White, 1)
+    --]]
+
 	-- Draw message text
-	dxDrawText(self.m_Text, self.m_AbsoluteX + self.m_Width * 0.01, self.m_AbsoluteY + self.m_Width * 0.01, self.m_AbsoluteX - self.m_Width * 0.01, self.m_AbsoluteY - self.m_Height * 0.01, Color.White, self.m_FontSize, self.m_Font, "left", "top", false, true)
+	dxDrawText(self.m_Text, x, y, x + w, y + h, Color.White, self.m_FontSize, self.m_Font, "left", "center", false, true)
 end
 
 ShortMessage.Timer = setTimer(
 	function()		
 		if #ShortMessage.MessageQueue > 0 then			
 			delete(ShortMessage.MessageQueue[1])
-			table.remove(ShortMessage.MessageQueue, 1)
-			
-			for k, v in ipairs(ShortMessage.MessageQueue) do
+            table.remove(ShortMessage.MessageQueue, 1)
+
+            for k, v in ipairs(ShortMessage.MessageQueue) do
 				local x, y = v:getPosition()
 				v:setPosition(x, y + 35)
 			end
