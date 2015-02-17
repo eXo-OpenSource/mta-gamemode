eXo-OpenSource/mta-gamemode

commit d2a686093bcf8eb87c435adebabfd0afd6200559

After deleting a group, the money is distributed (Task: 3187429)

vrp/server/classes/GroupManager.lua
@@ -122,10 +122,32 @@ function GroupManager:Event_groupDelete()
 		client:sendError(_("Du bist nicht berechtigt die Gruppe zu löschen!", client))
 		-- Todo: Report possible cheat attempt
 		return
-	end
-	
+    end
+
+    local leaderAmount = group.m_Money/2
+    group.m_Money = group.m_Money - leaderAmount
+
+    local memberAmount = 0
+    local groupSize = table.size(group.m_Players)
+    if groupSize == 1 then
+        leaderAmount = leaderAmount + group.m_Money
+    else
+        memberAmount = group.m_Money/(#groupSize-1)
+    end
+
 	-- Distribute group's money
-	-- Todo
+    for playerId, playerRank in pairs(group.m_Players) do
+        local player, isOffline = DatabasePlayer.get(playerId)
+        if playerRank == GroupRank.Leader then
+            player:giveMoney(leaderAmount)
+        else
+            player:giveMoney(memberAmount)
+        end
+
+        if isOffline then
+            delete(player)
+        end
+    end
 	
 	group:purge()
 	client:sendShortMessage(_("Deine Gruppe wurde soeben gelöscht", client))

commit 67f3b300a2077e8ce83ad9d6ecc0527cfcae4f97

Fixed an error occurring when someones tries to delete a group (introduced in d2a6860) Tried to fix the weird event bug once again Refreshed team help text to match the current team Increased nametag rendering priority (otherwise the nametag gets drawn in front of GUIs)

vrp/client/classes/HUD/Nametag.lua
@@ -27,7 +27,7 @@ function Nametag:constructor()
 	self.m_Draw = bind(self.draw,self)
 	self.m_ReciveBuffs = bind(self.reciveBuffs,self)
 	
-	addEventHandler("onClientRender",root,self.m_Draw)
+	addEventHandler("onClientRender", root, self.m_Draw, "high+999")
 	addEventHandler("reciveNametagBuffs",root,self.m_ReciveBuffs)
 	
 end

vrp/client/classes/HelpTextManager.lua
@@ -50,11 +50,13 @@ HelpTexts = {
 			Sarcasm <sarcasm@v-roleplay.net> (außerdem verantwortlich für den Webauftritt)
 
 			Moderation:
-			Alex <alex@v-roleplay.net> (außerdem verantwortlich für Maps)
+			Toxsi <toxsi@v-roleplay.net> (außerdem verantwortlich für Mapping)
 
 			Vielen Dank an:
+			thlefleshpound (für seine Zeit als Grafiker)
 			Schlumpf (für seine kurze Zeit als Mapper)
 			ReZ (für seine kurze Zeit als Mapper)
+			Alex (für seine Zeit als Mapper)
 		]];
 	};
 	Jobs = {

vrp/server/classes/Events/EventManager.lua
@@ -47,12 +47,12 @@ function EventManager:openEvent(eventClass)
 	local event = eventClass:new(self.m_EventIdCounter)
 	self.m_RunningEvents[self.m_EventIdCounter] = event
 	
-	for k, player in ipairs(getElementsByType("player")) do
+	for k, player in pairs(getElementsByType("player")) do
 		player:sendMessage(_("In 5min startet das Event '%s'! Begib dich zum Reifen-Blip, um daran teilzunehmen", player, event:getName()), 255, 255, 0)
 	end
 	
 	-- Start the event in 5min
-	setTimer(function() event:start() end, 0.5*60*1000, 1)
+	setTimer(event.start, 0.5*60*1000, 1, event)
 end
 
 function EventManager:Event_eventJoin(eventId)

vrp/server/classes/GroupManager.lua
@@ -132,7 +132,7 @@ function GroupManager:Event_groupDelete()
     if groupSize == 1 then
         leaderAmount = leaderAmount + group.m_Money
     else
-        memberAmount = group.m_Money/(#groupSize-1)
+        memberAmount = group.m_Money/(groupSize-1)
     end
 
 	-- Distribute group's money