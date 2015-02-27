9703ead2a38520835794375be8707a2770cf9da7
StiviiK <stefan.kuerzeder@gmail.com>
2015-02-27 17:56:20 +0100

    Changed HUDUI Style

--- vrp/client/classes/HUD/HUDUI.lua
+++ vrp/client/classes/HUD/HUDUI.lua
@@ -36,8 +36,8 @@ function HUDUI:draw()
 	end
 	
 	dxDrawRectangle(screenWidth-0.195*screenWidth,0.04*screenHeight,0.195*screenWidth,0.092*screenHeight,tocolor(0,0,0,150))
-	dxDrawText ("$",screenWidth-0.169*screenWidth,0.06*screenHeight,0.195*screenWidth,0.092*screenHeight,Color.White,1,self.m_Font)
-	dxDrawText (getPlayerMoney(localPlayer),screenWidth-0.14*screenWidth,0.0625*screenHeight,0.195*screenWidth,0.092*screenHeight,Color.White,1,self.m_Font)
+	dxDrawText ("$",screenWidth-0.169*screenWidth,0.092*screenHeight/2,0.195*screenWidth,0.092*screenHeight,Color.White,1,self.m_Font)
+	dxDrawText (getPlayerMoney(localPlayer),screenWidth-0.14*screenWidth,0.092*screenHeight/2,0.195*screenWidth,0.092*screenHeight,Color.White,1,self.m_Font)
 	
 	local munitionWindowActive = true
 	
@@ -62,8 +62,8 @@ function HUDUI:draw()
 	end
 	
 	-- Munition-Window
-	local addY = interpolateBetween(0,0,0,0.13*screenHeight,0,0,self.m_MunitionProgress,"Linear")
-	dxDrawRectangle(screenWidth-0.351*screenWidth,-0.09*screenHeight+addY,0.153*screenWidth,0.092*screenHeight,tocolor(0,0,0,150))
+	local addY = interpolateBetween(0,0,0,0.14*screenHeight,0,0,self.m_MunitionProgress,"Linear")
+	dxDrawRectangle(screenWidth-0.351*screenWidth,-0.1*screenHeight+addY,0.153*screenWidth,0.092*screenHeight,tocolor(0,0,0,150))
 	local inClip = getPedAmmoInClip(localPlayer)
 	local totalAmmo = getPedTotalAmmo(localPlayer)
 	local sMunition = ("%d - %d"):format(inClip,totalAmmo-inClip)
@@ -71,19 +71,28 @@ function HUDUI:draw()
 	
 	-- Karmabar
 	local karma = localPlayer:getKarma() or 0
-	dxDrawRectangle(screenWidth-0.25*screenWidth,0.157*screenHeight,0.25*screenWidth,0.045*screenHeight,karma >= 0 and tocolor(0,50,0,255) or tocolor(50,0,0,255))
+	dxDrawRectangle(screenWidth-0.25*screenWidth, 0.14*screenHeight, 0.195*screenWidth, 0.0425*screenHeight,karma >= 0 and tocolor(0,50,0,220) or tocolor(50,0,0,220))
 	if karma >= 0 then
-		dxDrawRectangle(screenWidth-0.25*screenWidth,0.157*screenHeight,(0.25*screenWidth)*karma/MAX_KARMA,0.045*screenHeight,tocolor(75,160,75,255))
+		dxDrawRectangle(screenWidth-0.25*screenWidth,0.14*screenHeight,(0.195*screenWidth)*karma/MAX_KARMA,0.0425*screenHeight,tocolor(75,160,75,220))
 	else
-		dxDrawRectangle(screenWidth-0.25*screenWidth,0.157*screenHeight,(0.25*screenWidth)*-karma/MAX_KARMA,0.045*screenHeight,tocolor(160,75,75,255))
+		dxDrawRectangle(screenWidth-0.25*screenWidth,0.14*screenHeight,(0.195*screenWidth)*-karma/MAX_KARMA,0.0425*screenHeight,tocolor(160,75,75,220))
 	end
 	local karma = (karma >= 0 and "+" or "")..math.floor(karma)
-	dxDrawText(karma,screenWidth-0.25*(screenWidth/2)-(dxGetTextWidth(karma,0.5,self.m_Font)/2),0.166*screenHeight,0,0,Color.White,0.5,self.m_Font)
-	
+	dxDrawText(karma,screenWidth-((0.195*screenWidth) - dxGetTextWidth(karma, 0.5, self.m_Font)),0.145*screenHeight,0,0,Color.White,0.5,self.m_Font)
+
+    --[[
+    -- Levelbar
+    local XP = 100
+    dxDrawRectangle(screenWidth-0.25*screenWidth, 0.185*screenHeight, 0.195*screenWidth, 0.0425*screenHeight, tocolor(0,50,0,220))
+    dxDrawRectangle(screenWidth-0.25*screenWidth, 0.185*screenHeight, 0.195*screenWidth, 0.0425*screenHeight, tocolor(0,50,0,220))
+    dxDrawRectangle(screenWidth-0.25*screenWidth,0.185*screenHeight,(0.195*screenWidth)*XP/175,0.0425*screenHeight,tocolor(75,160,75,220))
+    dxDrawText(XP.." XP",(screenWidth-0.25*screenWidth)+((0.195*screenWidth)/2-(dxGetTextWidth(XP.." XP", 0.5, self.m_Font)/2)),0.185*screenHeight,0,0,Color.White,0.5,self.m_Font)
+    --]]
+
 	-- Wantedlevel
-	dxDrawRectangle(screenWidth-0.05*screenWidth,0.23*screenHeight,0.05*screenWidth,0.09*screenHeight,tocolor(0,0,0,150))
-	dxDrawImage    (screenWidth-0.05*screenWidth+(0.05*screenWidth/2)-(0.025*screenWidth/2), 0.229*screenHeight+(0.09*screenHeight/2)-36, 0.025*screenWidth,0.044*screenHeight, "files/images/wanted.png", 0, 0, 0, getPlayerWantedLevel() > 0 and Color.Yellow or Color.White)
-	dxDrawText     (getPlayerWantedLevel(),screenWidth-0.05*screenWidth+(0.05*screenWidth/2)-6,0.24*screenHeight+(0.09*screenHeight/2),0,0,Color.White,0.5,self.m_Font)
+	dxDrawRectangle(screenWidth-0.05*screenWidth,0.14*screenHeight,0.05*screenWidth,0.09*screenHeight,tocolor(0,0,0,150))
+	dxDrawImage    (screenWidth-0.05*screenWidth+(0.05*screenWidth/2)-(0.025*screenWidth/2), 0.145*screenHeight+(0.09*screenHeight/2)-36, 0.025*screenWidth,0.044*screenHeight, "files/images/wanted.png", 0, 0, 0, getPlayerWantedLevel() > 0 and Color.Yellow or Color.White)
+	dxDrawText     (getPlayerWantedLevel(),screenWidth-0.05*screenWidth+(0.05*screenWidth/2)-5,0.148*screenHeight+(0.09*screenHeight/2),0,0,Color.White,0.5,self.m_Font)
 	
 	self:drawLevelRect()
 end
@@ -91,22 +100,22 @@ end
 function HUDUI:drawLevelRect()
 	local f = math.floor
 
-	-- Background
-	dxDrawRectangle(screenWidth*0.8, 0, screenWidth*0.2, screenHeight*0.03, Color.LightBlue)
+	-- Background screenWidth-0.195*screenWidth,0.04*screenHeight,0.195*screenWidth,0.092*screenHeight
+	dxDrawRectangle(screenWidth - screenWidth*0.195, 0, screenWidth*0.2, screenHeight*0.03, tocolor(77, 154, 202, 220))
 	
 	-- Joblevel
 	dxDrawImage(f(screenWidth*0.81), f(screenHeight*0.0045), f(screenWidth*0.016 / ASPECT_RATIO_MULTIPLIER), f(screenHeight*0.02), "files/images/HUD/JobLevel.png")
-	dxDrawText(localPlayer:getJobLevel(), screenWidth*0.835, screenHeight*0.0028, nil, nil, Color.White, 1.5, "arial")
+	dxDrawText(localPlayer:getJobLevel(), screenWidth*0.83, screenHeight*0.002, nil, nil, Color.White, 1.5, "arial")
 	
 	-- Weaponlevel
-	dxDrawImage(f(screenWidth*0.86), f(screenHeight*0.0045), f(screenWidth*0.016 / ASPECT_RATIO_MULTIPLIER), f(screenHeight*0.02), "files/images/HUD/WeaponLevel.png")
-	dxDrawText(localPlayer:getWeaponLevel(), screenWidth*0.885, screenHeight*0.0028, nil, nil, Color.White, 1.5, "arial")
+	dxDrawImage(f(screenWidth*0.85), f(screenHeight*0.0055), f(screenWidth*0.016 / ASPECT_RATIO_MULTIPLIER), f(screenHeight*0.02), "files/images/HUD/WeaponLevel.png")
+	dxDrawText(localPlayer:getWeaponLevel(), screenWidth*0.87, screenHeight*0.002, nil, nil, Color.White, 1.5, "arial")
 	
 	-- Vehiclelevel
-	dxDrawImage(f(screenWidth*0.91), f(screenHeight*0.0045), f(screenWidth*0.016 / ASPECT_RATIO_MULTIPLIER), f(screenHeight*0.02), "files/images/HUD/VehicleLevel.png")
-	dxDrawText(localPlayer:getVehicleLevel(), screenWidth*0.935, screenHeight*0.0028, nil, nil, Color.White, 1.5, "arial")
+	dxDrawImage(f(screenWidth*0.90), f(screenHeight*0.0055), f(screenWidth*0.016 / ASPECT_RATIO_MULTIPLIER), f(screenHeight*0.02), "files/images/HUD/VehicleLevel.png")
+	dxDrawText(localPlayer:getVehicleLevel(), screenWidth*0.92, screenHeight*0.002, nil, nil, Color.White, 1.5, "arial")
 	
 	-- Skinlevel
-	dxDrawImage(f(screenWidth*0.96), f(screenHeight*0.0045), f(screenWidth*0.016 / ASPECT_RATIO_MULTIPLIER), f(screenHeight*0.02), "files/images/HUD/SkinLevel.png")
-	dxDrawText(localPlayer:getSkinLevel(), screenWidth*0.985, screenHeight*0.0028, nil, nil, Color.White, 1.5, "arial")
+	dxDrawImage(f(screenWidth*0.95), f(screenHeight*0.0055), f(screenWidth*0.016 / ASPECT_RATIO_MULTIPLIER), f(screenHeight*0.02), "files/images/HUD/SkinLevel.png")
+	dxDrawText(localPlayer:getSkinLevel(), screenWidth*0.97, screenHeight*0.002, nil, nil, Color.White, 1.5, "arial")
 end
