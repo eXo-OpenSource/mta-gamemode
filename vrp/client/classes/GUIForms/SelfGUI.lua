2215e9f6f28211610791d09a499f7f2c11feaef6
StiviiK <stefan.kuerzeder@gmail.com>
2015-02-27 12:12:55 +0100

    Improved SelfGUI Design QuitJob-Button is now only visible if you have a job Playtime is now in following Style -> 0 hour(s) 0 min

--- vrp/client/classes/GUIForms/SelfGUI.lua
+++ vrp/client/classes/GUIForms/SelfGUI.lua
@@ -19,13 +19,12 @@ function SelfGUI:constructor()
 	local tabGeneral = self.m_TabPanel:addTab(_"Allgemein")
 	GUILabel:new(self.m_Width*0.02, self.m_Height*0.016, self.m_Width*0.3, self.m_Height*0.12, _"Allgemein", tabGeneral)
 	GUILabel:new(self.m_Width*0.02, self.m_Height*0.13, self.m_Width*0.25, self.m_Height*0.06, _"Spielzeit:", tabGeneral)
-	self.m_PlayTimeLabel = GUILabel:new(self.m_Width*0.3, self.m_Height*0.13, self.m_Width*0.4, self.m_Height*0.06, "0", tabGeneral)
+	self.m_PlayTimeLabel = GUILabel:new(self.m_Width*0.3, self.m_Height*0.13, self.m_Width*0.4, self.m_Height*0.06, "0 hour(s) 0 min", tabGeneral)
 
 	GUILabel:new(self.m_Width*0.02, self.m_Height*0.216, self.m_Width*0.25, self.m_Height*0.12, _"Job", tabGeneral)
 	GUILabel:new(self.m_Width*0.02, self.m_Height*0.33, self.m_Width*0.25, self.m_Height*0.06, _"Aktueller Job:", tabGeneral)
 	self.m_JobNameLabel = GUILabel:new(self.m_Width*0.3, self.m_Height*0.33, self.m_Width*0.4, self.m_Height*0.06, "", tabGeneral)
-	self.m_JobQuitButton = GUIButton:new(self.m_Width*0.5, self.m_Height*0.3, self.m_Width*0.35, self.m_Height*0.07, _"Job kündigen", tabGeneral):setBackgroundColor(Color.Red)
-
+	self.m_JobQuitButton = GUIButton:new(self.m_Width*0.02, self.m_Height*0.39, self.m_Width*0.25, self.m_Height*0.06, _"Job kündigen", tabGeneral):setBackgroundColor(Color.Red):setFontSize(1.35)
 	self.m_JobQuitButton.onLeftClick = bind(self.JobQuitButton_Click, self)
 
 	-- Tab: Groups
@@ -157,19 +156,16 @@ function SelfGUI:onShow()
     self.m_SkinLevelButton:setText(("+ (%sP)"):format(calculatePointsToNextLevel(localPlayer:getSkinLevel())))
     self.m_JobLevelButton:setText(("+ (%sP)"):format(calculatePointsToNextLevel(localPlayer:getJobLevel())))
 
-	self.m_PlayTimeLabel:setText(tostring(localPlayer:getPlayTime() or "").."min")
+    local hours, minutes = math.floor(localPlayer:getPlayTime()/60), (localPlayer:getPlayTime() - math.floor(localPlayer:getPlayTime()/60)*60)
+	--self.m_PlayTimeLabel:setText(tostring(localPlayer:getPlayTime() or "").."min")
+    self.m_PlayTimeLabel:setText(_("%s hour(s) %s min", hours, minutes))
 
 	if localPlayer:getJob() then
 		self.m_JobNameLabel:setText(localPlayer:getJob():getName())
+        self.m_JobQuitButton:setVisible(true)
 	else
 		self.m_JobNameLabel:setText("-")
-
-    end
-
-    if localPlayer:getJobLevel() then
-        self.m_JobLevelLabel:setText(tostring(localPlayer:getJobLevel()))
-    else
-        self.m_JobLevelLabel:setText("-")
+        self.m_JobQuitButton:setVisible(false)
     end
 end
 
