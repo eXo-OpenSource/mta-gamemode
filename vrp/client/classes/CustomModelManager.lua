3500848b4025416deb0c53dbde78e837add0f396
Jusonex <jusonex96@gmail.com>
2015-03-04 19:17:53 +0100

    Nearly finished race DM event

--- vrp/client/classes/CustomModelManager.lua
+++ vrp/client/classes/CustomModelManager.lua
@@ -9,15 +9,23 @@ CustomModelManager = inherit(Singleton)
 
 function CustomModelManager:constructor()
 	--self:loadImportDFF("files/models/dead_tree_18.dff", 846)
-	
+
 	-- Yoda
 	self:loadImportTXD("files/models/yoda.txd", 41)
 	self:loadImportDFF("files/models/yoda.dff", 41)
-	
+
 	-- vRP Logo
 	self:loadImportTXD("files/models/vrp_logo.txd", 2903)
 	self:loadImportDFF("files/models/vrp_logo.dff", 2903)
 	self:loadImportCOL("files/models/vrp_logo.col", 2903)
+
+	-- Race pickups
+	self:loadImportTXD("files/models/nitro.txd", 2221)
+	self:loadImportDFF("files/models/nitro.dff", 2221)
+	self:loadImportTXD("files/models/repair.txd", 2222)
+	self:loadImportDFF("files/models/repair.dff", 2222)
+	self:loadImportTXD("files/models/vehiclechange.txd", 2223)
+	self:loadImportDFF("files/models/vehiclechange.dff", 2223)
 end
 
 function CustomModelManager:loadImportDFF(filePath, modelId)
