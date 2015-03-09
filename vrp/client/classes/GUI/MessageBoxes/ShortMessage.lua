a6513c79b38bd7273e94458db21d3f37f24866d5
StiviiK <stefan.kuerzeder@gmail.com>
2015-03-09 19:38:21 +0100

    Converted to real Tabs

--- vrp/client/classes/GUI/MessageBoxes/ShortMessage.lua
+++ vrp/client/classes/GUI/MessageBoxes/ShortMessage.lua
@@ -12,24 +12,24 @@ ShortMessage.posOffSet = 35
 ShortMessage.MessageQueue = {}
 
 function ShortMessage:constructor(text, extratime)
-    local x, y, w, h = 20, screenHeight - screenHeight*0.265 - ShortMessage.posOffSet, 340*screenWidth/1600+6, 30
-    local lines = math.floor(dxGetTextWidth(text, 1.4, "default")/w) + 1
+	local x, y, w, h = 20, screenHeight - screenHeight*0.265 - ShortMessage.posOffSet, 340*screenWidth/1600+6, 30
+	local lines = math.floor(dxGetTextWidth(text, 1.4, "default")/w) + 1
 	if string.countChar(text, "\n") > 0 then
 		local extra = string.countChar(text, "\n")
-		lines = lines + extra -- Todo: Improve
+		lines = lines + extra -- Todo: improve
 	end
 
-    -- Calculate heigth
-    h = (h * lines) - (15 * math.floor(lines/2))
+	-- Calculate heigth
+	h = (h * lines) - (15 * math.floor(lines/2))
 
-    -- Calculate y position
-    y = y - h + 25
+	-- Calculate y position
+	y = y - h + 25
 
-    -- Recalculate the position offset for new Boxes
-    ShortMessage.posOffSet = ShortMessage.posOffSet + h + 5
+	-- Recalculate the position offset for new Boxes
+	ShortMessage.posOffSet = ShortMessage.posOffSet + h + 5
 
-    -- Create it
-    DxElement.constructor(self, x, y, w, h)
+	-- Create it
+	DxElement.constructor(self, x, y, w, h)
 	GUIFontContainer.constructor(self, text, 1.4, "default")
 	
 	table.insert(ShortMessage.MessageQueue, self)
@@ -46,20 +46,20 @@ function ShortMessage:destructor()
 end
 
 function ShortMessage:drawThis()
-    local x, y, w, h = self.m_AbsoluteX, self.m_AbsoluteY, self.m_Width, self.m_Height
+	local x, y, w, h = self.m_AbsoluteX, self.m_AbsoluteY, self.m_Width, self.m_Height
 
 	-- Draw background
 	dxDrawRectangle(x, y, w, h, tocolor(0, 0, 0, 200))
 
-    -- Center the text
-    x = x + 5
-    w = w - 10
+	-- Center the text
+	x = x + 5
+	w = w - 10
 
-    -- Draw the text bounding box (DEBUG)
-    --[[dxDrawLine(x, y, x + w, y, Color.White, 1)
-    dxDrawLine(x, y, x, y + h, Color.White, 1)
-    dxDrawLine(x, y + h, x + w, y + h, Color.White, 1)
-    dxDrawLine(x + w, y, x + w, y + h, Color.White, 1)]]
+	-- Draw the text bounding box (DEBUG)
+	--[[dxDrawLine(x, y, x + w, y, Color.White, 1)
+	dxDrawLine(x, y, x, y + h, Color.White, 1)
+	dxDrawLine(x, y + h, x + w, y + h, Color.White, 1)
+	dxDrawLine(x + w, y, x + w, y + h, Color.White, 1)]]
 
 	-- Draw message text
 	dxDrawText(self.m_Text, x, y, x + w, y + h, Color.White, self.m_FontSize, self.m_Font, "left", "top", false, true)
