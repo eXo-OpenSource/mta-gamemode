817b4a97f05752c5e02de34ec32562c1793480e4
StiviiK <stefan.kuerzeder@gmail.com>
2015-03-11 22:21:14 +0100

    Improved MessageBox Animation Changed Design a little bit

--- vrp/client/classes/GUI/MessageBoxes/MessageBox.lua
+++ vrp/client/classes/GUI/MessageBoxes/MessageBox.lua
@@ -10,24 +10,14 @@ inherit(GUIFontContainer, MessageBox)
 MessageBox.MessageBoxes = {}
 
 function MessageBox:constructor(text, timeout)
-	DxElement.constructor(self, screenWidth/2-360/2, screenHeight, 360, 140)
-	GUIFontContainer.constructor(self, text, 1.4, "default")
+	DxElement.constructor(self, screenWidth/2-350/2, screenHeight, 340, 110)
+	GUIFontContainer.constructor(self, text, 1, VRPFont(28))
 	timeout = timeout and timeout >= 50 and timeout or 3000
 	setTimer(function() delete(self) end, timeout, 1)
 	playSound(self:getSoundPath())
 
 	table.insert(MessageBox.MessageBoxes, self)
-
-	for i = #MessageBox.MessageBoxes, 1, -1 do
-		local obj = MessageBox.MessageBoxes[i]
-		local prevObj = MessageBox.MessageBoxes[i + 1]
-
-		if prevObj then
-			obj.m_Animation = Animation.Move:new(obj, 1000, obj.m_AbsoluteX, prevObj.m_Animation.m_TY - obj.m_Height - 5)
-		else
-			obj.m_Animation = Animation.Move:new(obj, 1000, obj.m_AbsoluteX, obj.m_AbsoluteY - obj.m_Height - 5)
-		end
-	end
+	MessageBox.resortPositions()
 end
 
 function MessageBox:virtual_constructor(text, timeout)
@@ -36,17 +26,36 @@ end
 
 function MessageBox:virtual_destructor ()
 	table.removevalue(MessageBox.MessageBoxes, self)
+	MessageBox.resortPositions()
 end
 
 function MessageBox:drawThis()
 	-- Draw background
 	dxDrawRectangle(self.m_AbsoluteX, self.m_AbsoluteY, self.m_Width, self.m_Height, tocolor(0, 0, 0, 150))
-	
+	dxDrawRectangle(self.m_AbsoluteX, self.m_AbsoluteY, self.m_Width, 5, Color.DarkLightBlue)
+
 	-- Draw icon
-	dxDrawImage(self.m_AbsoluteX + 10, self.m_AbsoluteY + self.m_Height/2 - 100/2, 100, 100, self:getImagePath())
-	
+	dxDrawImage(self.m_AbsoluteX + 10, self.m_AbsoluteY + self.m_Height/2 - 80/2, 85, 85, self:getImagePath())
+
 	-- Draw message text
-	dxDrawText(self.m_Text, self.m_AbsoluteX + 120, self.m_AbsoluteY + 10, self.m_AbsoluteX + self.m_Width - 5, self.m_AbsoluteY + self.m_Height - 10, Color.White, self.m_FontSize, self.m_Font, "left", "top", false, true)
+	dxDrawText(self.m_Text, self.m_AbsoluteX + 120, self.m_AbsoluteY + 20, self.m_AbsoluteX + self.m_Width - 5, self.m_AbsoluteY + self.m_Height - 10, Color.White, self.m_FontSize, self.m_Font, "left", "top", false, true)
+end
+
+function MessageBox.resortPositions ()
+	for i = #MessageBox.MessageBoxes, 1, -1 do
+		local obj = MessageBox.MessageBoxes[i]
+		local prevObj = MessageBox.MessageBoxes[i + 1]
+
+		if obj.m_Animation then
+			delete(obj.m_Animation)
+		end
+
+		if prevObj then
+			obj.m_Animation = Animation.Move:new(obj, 1000, obj.m_AbsoluteX, prevObj.m_Animation.m_TY - obj.m_Height - 5)
+		else
+			obj.m_Animation = Animation.Move:new(obj, 1000, obj.m_AbsoluteX, screenHeight - 130)
+		end
+	end
 end
 
 MessageBox.getImagePath = pure_virtual
