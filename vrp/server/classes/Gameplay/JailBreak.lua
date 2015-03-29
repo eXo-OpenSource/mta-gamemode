19c0c668a5a7a0c4449434e1fda931256735327f
Jusonex <jusonex96@gmail.com>
2015-03-29 20:37:32 +0200

    Worked on JailBreak stuff (major additions) Added new radiobutton image

--- vrp/server/classes/Gameplay/JailBreak.lua
+++ vrp/server/classes/Gameplay/JailBreak.lua
@@ -8,13 +8,14 @@
 JailBreak = inherit(Singleton)
 addEvent("keypadClick", true)
 addEvent("jailAnswersRetrieve", true)
-local TIME_BETWEEN_JAILBREAKS = 30*60*1000
+local TIME_BETWEEN_JAILBREAKS = 1*60*60*1000
+local JAIL_PIPES_DIMENSION = 5
 
 function JailBreak:constructor()
-	self.m_LastJailBreakTime = 0
-	self.m_OpenCellsKeypad = getElementByID("object (sec_keypad) (1)") -- TODO: Replace name by a proper name
-	self.m_ShutterDoor = getElementByID("ShutterDoor")
-	self.m_BombArea = BombArea:new(Vector3(3459.85, -2073.16, 16.82), function(area, player) player:triggerEvent("bankRobberyCountdown", 10) end, bind(self.Bomb_Explode, self), 10000)
+	self.m_CanBeStarted = true
+	self.m_OpenCellsKeypad = getElementByID("OpenCellKeypad")
+	self.m_CellRoomDoor = getElementByID("CellRoomDoor")
+	self.m_BombArea = BombArea:new(Vector3(3459.85, -2073.16, 16.82), bind(self.Bomb_Place, self), bind(self.Bomb_Explode, self), 10000)
 
 	self.m_GuardPed = getElementByID("JailGuardPed")
 	addEventHandler("onPedWasted", self.m_GuardPed, bind(self.GuardPed_Wasted, self))
@@ -27,12 +28,27 @@ function JailBreak:constructor()
 	self.m_MainGateState = true
 
 	self.m_CellGates = {}
-	for i = 1, 4 do
+	for i = 1, 6 do
 		self.m_CellGates[i] = getElementByID("CellGate"..i)
 	end
+	self.m_BreakFreeDoor = getElementByID("BreakFreeDoor")
+	self.m_HallDoorLeft = getElementByID("HallDoorLeft")
+	self.m_HallDoorRight = getElementByID("HallDoorRight")
 
 	addEventHandler("keypadClick", root, bind(self.Keypad_Click, self))
 	addEventHandler("jailAnswersRetrieve", root, bind(self.KeypadAnswers_Retrieve, self))
+
+	self.m_PipeOutShape = createColSphere(3490.72, -2093.12, 16.87, 3)
+	addEventHandler("onColShapeHit", self.m_PipeOutShape, bind(self.PipeOutShape_Hit, self))
+	self.m_PipeExit1Shape = createColSphere(3039.1006, -2069.7002, 1216.2, 3)
+	self.m_PipeExit2Shape = createColSphere(3073.9004, -2226.3994, 1208.2, 3)
+	self.m_PipeExit1Shape:setDimension(5)
+	self.m_PipeExit2Shape:setDimension(5)
+	addEventHandler("onColShapeHit", self.m_PipeExit1Shape, bind(self.PipeExitShape_Hit, self))
+	addEventHandler("onColShapeHit", self.m_PipeExit2Shape, bind(self.PipeExitShape_Hit, self))
+	self.m_PipeExitTrollShape = createColSphere(3010.5, -2126.0996, 1, 3)
+	self.m_PipeExitTrollShape:setDimension(5)
+	addEventHandler("onColShapeHit", self.m_PipeExitTrollShape, bind(self.PipeExitTrollShape, self))
 end
 
 function JailBreak:toggleMainGate(state) -- true: closed; false: open
@@ -59,6 +75,16 @@ function JailBreak:toggleCellGates(state)
 	for i in pairs(self.m_CellGates) do
 		self:toggleCellGate(i, state)
 	end
+
+	if state then
+		self.m_BreakFreeDoor:move(1500, self.m_BreakFreeDoor:getPosition() + Vector3(0, 0, 5))
+		self.m_HallDoorLeft:move(1500, self.m_HallDoorLeft:getPosition() + Vector3(2, 0, 0))
+		self.m_HallDoorRight:move(1500, self.m_HallDoorLeft:getPosition() - Vector3(2, 0, 0))
+	else
+		self.m_BreakFreeDoor:move(1500, self.m_BreakFreeDoor:getPosition() - Vector3(0, 0, 5))
+		self.m_HallDoorLeft:move(1500, self.m_HallDoorLeft:getPosition() - Vector3(2, 0, 0))
+		self.m_HallDoorRight:move(1500, self.m_HallDoorLeft:getPosition() + Vector3(2, 0, 0))
+	end
 end
 
 function JailBreak:getRandomQuestion()
@@ -82,9 +108,48 @@ function JailBreak:checkAnswers(answers)
 	return wrongQuestionCount <= 1
 end
 
+function JailBreak:respawnGuardPed()
+	self.m_GuardPed:destroy()
+	self.m_GuardPed = createPed(71, 3453.9004, -2153.8994, 17.1)
+end
+
+function JailBreak:reset()
+	self:respawnGuardPed()
+	self:toggleCellGates(true)
+
+	self.m_CellRoomDoor:move(500, self.m_CellRoomDoor:getPosition() + Vector3(0, 0, 6))
+	self.m_CanBeStarted = true
+end
+
 function JailBreak:GuardPed_Wasted(totalAmmo, killer)
 	if killer and killer:getKarma() < 0 then
+		-- Prevent new jail breaks for the specified time
+		self.m_CanBeStarted = false
+
+		-- Open main gate
 		self:toggleMainGate(false)
+
+		-- Report the kill crime, but do not report jailbreak yet
+		killer:reportCrime(Crime.Kill)
+
+		setTimer(
+			function()
+				-- Report jailbreak after a short delay
+				if isElement(killer) then
+					killer:reportCrime(Crime.JailBreak)
+				end
+
+				-- Close main gate | TODO: How does the police enter the interior? ==> Clicksystem
+				self:toggleMainGate(true)
+
+			end, 60*1000, 1
+		)
+
+		-- Reset everything after 1h, jailbreak can now be started again
+		setTimer(bind(self.reset, self), TIME_BETWEEN_JAILBREAKS, 1)
+	else
+		-- Respawn immediately
+		self:respawnGuardPed()
 	end
 end
 
@@ -92,12 +157,16 @@ function JailBreak:ControlPed_Wasted(totalAmmo, killer)
 
 end
 
+function JailBreak:Bomb_Place(bombArea, player)
+	player:triggerEvent("bankRobberyCountdown", 10)
+end
+
 function JailBreak:Bomb_Explode()
-	self.m_ShutterDoor:move(500, self.m_ShutterDoor:getPosition() + Vector3(0, 0, 6))
+	self.m_CellRoomDoor:move(500, self.m_CellRoomDoor:getPosition() - Vector3(0, 0, 6))
 end
 
 function JailBreak:Keypad_Click()
-	if source == self.m_OpenCellsKeypad and self.m_LastJailBreakTime + TIME_BETWEEN_JAILBREAKS > getTickCount() then
+	if source == self.m_OpenCellsKeypad and self.m_CanBeStarted then -- CanBeStarted is false when the jailbreak has started recently
 		client:sendError(_("Du kannst den Gefängnisausbruch zurzeit nicht starten", client))
 		return
 	end
@@ -109,7 +178,7 @@ function JailBreak:Keypad_Click()
 end
 
 function JailBreak:KeypadAnswers_Retrieve(answers, gateId)
-	if self:checkAnswers(answers) then
+	if self:checkAnswers(answers) and not self.m_CanBeStarted then
 		if gateId == 1 then
 			-- Open all cells
 			self:toggleCellGates(false)
@@ -117,6 +186,27 @@ function JailBreak:KeypadAnswers_Retrieve(answers, gateId)
 		client:sendSuccess("ACCESS GRANTED")
 	else
 		client:sendError(_("ACCESS DENIED. Zu viele falsche Fragen!", client))
+	end
+end
+
+function JailBreak:PipeOutShape_Hit(hitElement, matchingDimension)
+	if getElementType(hitElement) == "player" and matchingDimension then
+		hitElement:setPosition(2971.4004, -2132, 1211.4)
+		hitElement:setDimension(5)
+	end
+end
+
+function JailBreak:PipeExitShape_Hit(hitElement, matchingDimension)
+	if getElementType(hitElement) == "player" and matchingDimension then
+		hitElement:setDimension(0)
+	end
+end
+
+function JailBreak:PipeExitTrollShape(hitElement, matchingDimension)
+	if getElementType(hitElement) == "player" and matchingDimension then
+		hitElement:setDimension(0)
+		hitElement:setPosition(2868.82, -2125.05, 5.30)
+		hitElement:setRotation(0, 0, 270)
 	end
 end
 
