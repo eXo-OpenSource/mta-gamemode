c2f5ab699d32214cdad068d9b61ce5e8afde7b95
Jusonex <jusonex96@gmail.com>
2015-03-31 16:34:23 +0200

    Editortools: Added garbage placement tool
    Usage:
    - Press F2 to place garbage
    - /savegarbage saves everything to disk
    - /dellast removes the last placed garbage

--- vrp_editortools/client.lua
+++ vrp_editortools/client.lua
@@ -1,106 +1,11 @@
-addEventHandler("onClientElementPropertyChanged", root,
-	function(propertyName)
-		if getElementType(source) == "bus_stop" then
-			if propertyName == "markerdistance" then
-				local markerDistance = tonumber(exports.edf:edfGetElementProperty(source, "markerdistance"))
-				local object = getRepresentation(source, "object")
-				local marker = getRepresentation(source, "marker")
-				
-				if object and object[1] and marker and markerDistance then
-					detachElements(marker)
-					local x, y, z = getPositionFromElementOffset(object[1], -1 * markerDistance, 0, -1)
-					setElementData(marker, "position", {x, y, z})
-					exports.edf:edfSetElementPosition(marker, x, y, z)
-					attachElements(marker, object[1], -1 * markerDistance, 0, -1)
-				end
-			end
-		elseif getElementType(source) == "gangarea" then
-			
-		end
-	end
-)
-
-addEventHandler("onClientElementCreate", root,
+---------------------------------------
+-- Section: Garbage place tool
+---------------------------------------
+bindKey("f2", "down",
 	function()
-		if getElementType(source) == "bus_stop" then
-			local markerDistance = tonumber(exports.edf:edfGetElementProperty(source, "markerdistance"))
-			local object = getRepresentation(source, "object")
-			local marker = getRepresentation(source, "marker")
-				
-			if object and object[1] and marker and markerDistance then
-				detachElements(marker)
-				local x, y, z = getPositionFromElementOffset(object[1], -1 * markerDistance, 0, -1)
-				setElementData(marker, "position", {x, y, z})
-				exports.edf:edfSetElementPosition(marker, x, y, z)
-				attachElements(marker, object[1], -1 * markerDistance, 0, -1)
-			end
-		elseif getElementType(source) == "gangarea" then
-			local x, y, z = exports.edf:edfGetElementPosition(source)
-			exports.edf:edfSetElementProperty(source, "areaX", x)
-			exports.edf:edfSetElementProperty(source, "areaY", y)
-		end
+		local x, y, z = getElementPosition(localPlayer)
+		z = getGroundPosition(x, y, z) + 0.1
+	
+		triggerServerEvent("garbageAdd", localPlayer, x, y, z)
 	end
 )
-
-addEventHandler("onClientRender", root,
-	function()
-		for k, gangarea in pairs(getElementsByType("gangarea")) do
-			local areaX, areaY = exports.edf:edfGetElementProperty(gangarea, "areaX"), exports.edf:edfGetElementProperty(gangarea, "areaY")
-			local width, height = exports.edf:edfGetElementProperty(gangarea, "width"), exports.edf:edfGetElementProperty(gangarea, "height")
-			if areaX and areaY and width and height then
-				local z = getGroundPosition(areaX, areaY, 50)
-				if z then
-					dxDrawRectangle3D(areaX, areaY, z+1, width, height, tocolor(255, 255, 0, 200), 0, areaX, areaY-height/2, z+2)
-				end
-			end
-		end
-	end
-)
-
-bindKey("num_0", "down",
-	function()
-		local selectedObject = exports.editor_main:getSelectedElement()
-		if not selectedObject or getElementType(selectedObject) ~= "gangarea"  then
-			return
-		end
-		
-		local x, y = getElementPosition(localPlayer)
-		exports.edf:edfSetElementProperty(selectedObject, "areaX", x)
-		exports.edf:edfSetElementProperty(selectedObject, "areaY", y)
-	end
-)
-
-
--- Utils
-function getPositionFromElementOffset(element, offX, offY, offZ)
-	local m = getElementMatrix(element)
-	local x = offX * m[1][1] + offY * m[2][1] + offZ * m[3][1] + m[4][1]
-	local y = offX * m[1][2] + offY * m[2][2] + offZ * m[3][2] + m[4][2]
-	local z = offX * m[1][3] + offY * m[2][3] + offZ * m[3][3] + m[4][3]
-	return x, y, z
-end
-
-function getRepresentation(element,type)
-	local elemTable = {}
-	for i,elem in ipairs(getElementsByType(type,element)) do
-		if elem ~= exports.edf:edfGetHandle ( elem ) then
-			table.insert(elemTable, elem)
-		end
-	end
-	if #elemTable == 0 then
-		return false
-	elseif #elemTable == 1 then
-		return elemTable[1]
-	else
-		return elemTable
-	end
-end
-
-local dot = dxCreateRenderTarget(1,1)
-dxSetRenderTarget(dot)
-dxDrawRectangle(0, 0, 1, 1, tocolor(255,255,255,255))
-dxSetRenderTarget()
-function dxDrawRectangle3D(x,y,z,w,h,c,r,normalX,normalY,normalZ)
-	local lx, ly, lz = x+w, y, (z+tonumber(r or 0)) or z
-	return dxDrawMaterialLine3D(x, y-h/2, z, lx, ly-h/2, lz, dot, h, c, normalX, normalY, normalZ)
-end
---
+++ vrp_editortools/server.lua
@@ -0,0 +1,48 @@
+---------------------------------------
+-- Section: Garbage place tool
+---------------------------------------
+local file = false
+local cache = {}
+
+addEventHandler("onResourceStart", resourceRoot,
+	function()
+		if fileExists("objects.txt") then
+			file = fileOpen("objects.txt")
+		else
+			file = fileCreate("objects.txt")
+		end
+		
+		-- Seek to the end
+		--fileSetPos(file, fileGetSize(file))
+	end
+)
+
+addEvent("garbageAdd", true)
+addEventHandler("garbageAdd", root,
+	function(x, y, z)
+		local object = createObject(2670, x, y, z)
+		cache[#cache + 1] = {x, y, z, object}
+	end
+)
+
+addCommandHandler("savegarbage",
+	function(player)
+		for k, v in pairs(cache) do
+			local x, y, z = unpack(v)
+			fileWrite(file, string.format("{%.2f, %.2f, %.2f},\n", x, y, z))
+		end
+		fileFlush(file)
+		outputChatBox("Saved!", player, 255, 255, 0)
+	end
+)
+
+addCommandHandler("dellast",
+	function(player)
+		local lastIndex = #cache
+		if lastIndex > 0 then
+			local last = cache[lastIndex]
+			cache[lastIndex] = nil
+			destroyElement(last[4])
+		end
+	end
+)
