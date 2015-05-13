2331a03e6cb9b86b1322568d6ab3cd8a7373ee67
Jusonex <jusonex96@gmail.com>
2015-05-13 17:11:50 +0200

    Added missing weapon icons Added debug message to custom download stuff

--- vrp/client/classes/GUIForms/DownloadGUI.lua
+++ vrp/client/classes/GUIForms/DownloadGUI.lua
@@ -3,7 +3,7 @@ inherit(Singleton, DownloadGUI)
 
 function DownloadGUI:constructor()
 	GUIForm.constructor(self, 0, 0, screenWidth, screenHeight)
-	
+
 	self.m_Logo = GUIImage:new(screenWidth/2 - 250/2, screenHeight/2 - 250/2 - 120, 250, 250, "files/images/Logo.png", self)
 	self.m_Text = GUILabel:new(0, screenHeight/2 - 150/2 + 50, screenWidth, 50, "Bitte warten, bis alle Spieldateien geladen wurde...", self):setAlignX("center"):setFont(VRPFont(40))
 	if screenWidth < 1024 then
@@ -12,30 +12,30 @@ function DownloadGUI:constructor()
 	self.m_CurrentState = GUILabel:new(0, screenHeight/2 - 150/2 + 150, screenWidth, 50, "", self):setAlignX("center"):setFont(VRPFont(30))
 	self.m_MusicText = GUILabel:new(0, screenHeight - 30, screenWidth, 30, "Drücke 'm', um die Musik zu stoppen!", self):setAlignX("center")
 	self.m_ProgressBar = GUIProgressBar:new(screenWidth/2 - 500/2, screenHeight/2 - 150/2 + 110, 500, 30, self)
-	
+
 	fadeCamera(false) -- freeroam hack | todo: Remove when freeroam is no longer required
 	self:setStateText("Lade Datenarchiv: vrp.data...")
-	
+
 	setTimer(bind(DownloadGUI.launchMusic, self), 150, 1)
 end
 
 function DownloadGUI:launchMusic()
 	if not self:isVisible() then return end
 	self.m_Music = playSound("http://www.jusonex.net/public/saonline/downloadmusic.mp3", true)
-	self.m_StopMusicFunc = function() 
+	self.m_StopMusicFunc = function()
 		if self.m_Music then
-			destroyElement(self.m_Music) 
-			self.m_Music = nil 
+			destroyElement(self.m_Music)
+			self.m_Music = nil
 			self:bind("m", self.m_StartMusicFunc)
-		end 
+		end
 	end
-	self.m_StartMusicFunc = function() 
+	self.m_StartMusicFunc = function()
 		if not self.m_Music then
-			self.m_Music = playSound("http://www.jusonex.net/public/saonline/downloadmusic.mp3", true) 
+			self.m_Music = playSound("http://www.jusonex.net/public/saonline/downloadmusic.mp3", true)
 			self:bind("m", self.m_StopMusicFunc)
-		end 
+		end
 	end
-	
+
 	self:bind("m", self.m_StopMusicFunc)
 end
 
@@ -43,16 +43,18 @@ function DownloadGUI:destructor()
 	if self.m_Music and isElement(self.m_Music) then
 		stopSound(self.m_Music)
 	end
-	
+
 	GUIForm.destructor(self)
 end
 
 function DownloadGUI:onProgress(p, fullSize)
 	self.m_ProgressBar:setProgress(tonumber(p) or 0)
-	
+	outputConsole("Progress: "..tostring(p))
+	outputConsole("FullSize: "..tostring(fullSize))
+
 	local downloadedSize = (tonumber(p) or 0)*(fullSize/100)
 	self:setStateText(("Lade Datenarchiv: vrp.data... (%.2fMB/%.2fMB)"):format(downloadedSize/1024/1024, fullSize/1024/1024))
-	
+
 	fadeCamera(false) -- freeroam hack | todo: Remove when freeroam is no longer required
 end
 
@@ -66,12 +68,12 @@ function DownloadGUI:onComplete()
 	fadeCamera(true)
 	self:fadeOut(750)
 	setTimer(
-	function() 
-		delete(self) 
+	function()
+		delete(self)
 		lgi = LoginGUI:new()
 		lgi:setVisible(false)
 		lgi:fadeIn(750)
-		
+
 		local pwhash = core:get("Login", "password", "")
 		local username = core:get("Login", "username", "")
 		lgi.m_LoginEditUser:setText(username)
