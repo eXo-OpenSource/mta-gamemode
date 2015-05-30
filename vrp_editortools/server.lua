7243ac331947f4a1dea921dd8707c71673f55c31
Jusonex <jusonex96@gmail.com>
2015-05-31 01:07:20 +0200

    Added ped placing stuff and updated whitelist

--- vrp_editortools/server.lua
+++ vrp_editortools/server.lua
@@ -6,10 +6,10 @@ local cache = {}
 
 addEventHandler("onResourceStart", resourceRoot,
 	function()
-		if fileExists("objects.txt") then
-			file = fileOpen("objects.txt")
+		if fileExists("garbage.txt") then
+			file = fileOpen("garbage.txt")
 		else
-			file = fileCreate("objects.txt")
+			file = fileCreate("garbage.txt")
 		end
 		
 		-- Seek to the end
@@ -46,5 +46,56 @@ addCommandHandler("dellast",
 			destroyElement(last[4])
 			destroyElement(last[5])
 		end
+	end
+)
+
+
+---------------------------------------
+-- Section: Ped place tool
+---------------------------------------
+local shopPeds = {}
+
+function ShopPed_Key(player)
+	local x, y, z = getElementPosition(player)
+	local rx, ry, rz = getElementRotation(player)
+	local ped = createPed(getElementModel(player), x, y, z, rz)
+	setElementInterior(ped, getElementInterior(player))
+	setElementDimension(ped, getElementDimension(player))
+	setElementCollisionsEnabled(ped, false)
+	shopPeds[#shopPeds + 1] = ped
+end
+for k, v in pairs(getElementsByType("player")) do
+	bindKey(v, "f3", "down", ShopPed_Key)
+end
+addEventHandler("onPlayerJoin", root,
+	function()
+		bindKey(source, "f3", "down", ShopPed_Key)
+	end
+)
+
+addCommandHandler("savepeds",
+	function()
+		local file
+		if fileExists("peds.txt") then
+			file = fileOpen("peds.txt")
+		else
+			file = fileCreate("peds.txt")
+		end
+	
+		for k, ped in pairs(shopPeds) do
+			local x, y, z = getElementPosition(ped)
+			local rx, ry, rz = getElementRotation(ped)
+			fileWrite(file, string.format("{%d, %.2f, %.2f, %.2f, %.1f, %d, %d},\n", getElementModel(ped), x, y, z, rz, getElementInterior(ped), getElementDimension(ped)))
+		end
+		
+		outputChatBox("Shop peds were saved!", root, 255, 255, 0)
+	end
+)
+
+addCommandHandler("delped",
+	function()
+		local ped = shopPeds[#shopPeds]
+		shopPeds[#shopPeds] = nil
+		destroyElement(ped)
 	end
 )
