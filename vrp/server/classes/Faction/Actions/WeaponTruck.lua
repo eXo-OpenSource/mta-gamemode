2733499d537741cc97d0b92fa5a3e3d42b0676d5
Stumpy <danieltaucher@gmail.com>
2016-01-20 00:28:25 +0100

    Updated Faction System

--- vrp/server/classes/Faction/Actions/WeaponTruck.lua
+++ vrp/server/classes/Faction/Actions/WeaponTruck.lua
@@ -10,8 +10,24 @@ WeaponTruck = inherit(Singleton)
   -- implement by children
 
 function WeaponTruck:constructor()
+	self:createStartPoint(-1869.14, 1421.49, 7.18)
 	outputDebugString("WeaponTruck loaded")
 end
 
 function WeaponTruck:destructor()
+end
+
+function WeaponTruck:createStartPoint(x,y,z)
+	self.m_startPickup = createPickup(x,y,z,3,1279,0,0)
+	addEventHandler("onPickupHit", self.m_startPickup,
+		function(hitElement)
+			if getElementType(hitElement) == "player" then
+				local faction = hitElement:getFaction()
+				if faction:isEvilFaction() == true then
+					--ToDo
+				end
+			end
+			cancelEvent()
+		end
+	)
 end
--- vrp/server/constants/factions.lua
+++ vrp/server/constants/factions.lua
@@ -1,12 +1,12 @@
 ﻿factionColors = {}
 factionRankNames = {}
 factionSkins = {}
--- ID 1 = Police Departement
 
+-- ID 1 = Police Departement:
 factionColors[1] = {
-["r"] = 50,
-["g"] = 50,
-["b"] = 255
+["r"] = 0,
+["g"] = 255,
+["b"] = 150
 }
 
 factionRankNames[1] = {
@@ -19,3 +19,75 @@ factionRankNames[1] = {
 [6] = "Chief"
 }
 factionSkins[1] = {[93]=true,[265]=true, [266]=true, [267]=true,[280]=true,[281]=true,[282]=true, [283]=true, [284]=true, [288]=true}
+
+-- ID 2 = FBI:
+factionColors[2] = {
+["r"] = 50,
+["g"] = 50,
+["b"] = 255
+}
+
+factionRankNames[2] = {
+[0] = "Probationary Agent",
+[1] = "Agent",
+[2] = "Special Agent",
+[3] = "Supervisory Special Agent",
+[4] = "Section Chief",
+[5] = "Deputy Director",
+[6] = "FBI-Director"
+}
+factionSkins[2] = {[163]=true, [164]=true, [165]=true,[166]=true,[285]=true,[286]=true,[294]=true,[295]=true}
+
+-- ID 3 = Army:
+factionColors[3] = {
+["r"] = 0,
+["g"] = 125,
+["b"] = 0
+}
+
+factionRankNames[3] = {
+[0] = "Private",
+[1] = "Corporal",
+[2] = "Staff Sergeant",
+[3] = "Major",
+[4] = "Warrant Officer",
+[5] = "Colonel",
+[6] = "General"
+}
+factionSkins[3] = {[73]=true,[191]=true,[287]=true,[312]=true}
+
+-- ID 4 = Rescue Team:
+factionColors[4] = {
+["r"] = 255,
+["g"] = 85,
+["b"] = 0
+}
+
+factionRankNames[4] = {
+[0] = "Rescue-Trainee",
+[1] = "Rescue-Assistant",
+[2] = "Rescue-Member",
+[3] = "Chief of Operations",
+[4] = "Rescue-Secretary",
+[5] = "Rescue-Deputy",
+[6] = "Rescue-Chief"
+}
+factionSkins[4] = {[260]=true, [277]=true, [278]=true, [279]=true,[70]=true, [71]=true, [274]=true, [275]=true, [276]=true}
+
+-- ID 5 = La Cosa Nostra:
+factionColors[5] = {
+["r"] = 100,
+["g"] = 100,
+["b"] = 100
+}
+
+factionRankNames[5] = {
+[0] = "Giovane D'Honore",
+[1] = "Picciotto",
+[2] = "Sgarrista",
+[3] = "Caporegime",
+[4] = "Consigliere",
+[5] = "Capo Bastone",
+[6] = "Capo Crimini"
+}
+factionSkins[5] = {[111]=true, [112]=true, [113]=true, [124]=true, [125]=true, [126]=true, [127]=true,[237]=true,[272]=true}
