61adf66981f6e8623284af6a42f0f40a415df6de
Stumpy <danieltaucher@gmail.com>
2016-01-22 14:41:57 +0100

    Added exo_maps ressouce

---
+++ exo_maps/textur_removals.lua
@@ -0,0 +1,84 @@
+﻿--Mechaniker
+	local fensterNormalDistanz = engineGetModelLODDistance(4212)--1661.9000244141,-1826.6999511719,24.799999237061)
+	local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(4212),50,1661.9000244141,-1826.6999511719,24.799999237061)
+removeWorldModel(4212,50,1661.9000244141,-1826.6999511719,24.799999237061)
+
+--Usertreff
+	local fensterNormalDistanz = engineGetModelLODDistance(4215)--1661.9000244141,-1826.6999511719,24.799999237061)
+	local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(4215),50,1777.5999755859,-1775.0999755859,36.799999237061)
+removeWorldModel(4215,50,1777.5999755859,-1775.0999755859,36.799999237061)
+
+
+--Fahrschule
+	local fensterNormalDistanz = engineGetModelLODDistance(4220)--1661.9000244141,-1826.6999511719,24.799999237061)
+	local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(4220),50,1370.5999755859,-1643.4000244141,33.200000762939)
+removeWorldModel(4220,50,1370.5999755859,-1643.4000244141,33.200000762939)
+
+--Mechaniker2
+	local fensterNormalDistanz = engineGetModelLODDistance(4214)--1661.9000244141,-1826.6999511719,24.799999237061)
+	local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(4214),50,1589.4000244141,-1817.5,22.200000762939)
+removeWorldModel(4214,50,1589.4000244141,-1817.5,22.200000762939)
+
+
+--Mechaniker3
+	local fensterNormalDistanz = engineGetModelLODDistance(4227)--1661.9000244141,-1826.6999511719,24.799999237061)
+	local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(4227),50,1614.5999755859,-1862.3000488281,14.10000038147)
+removeWorldModel(4227,50,1614.5999755859,-1862.3000488281,14.10000038147)
+
+--LTR
+local fensterNormalDistanz = engineGetModelLODDistance(4715)--1661.9000244141,-1826.6999511719,24.799999237061)
+local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(4715),100,1572.6999511719,-1240.4000244141,71.5)
+removeWorldModel(4715,100,1572.6999511719,-1240.4000244141,71.5)
+
+--STADTHALLE
+
+--StadthalleFlagge
+	local fensterNormalDistanz = engineGetModelLODDistance(4003)--1661.9000244141,-1826.6999511719,24.799999237061)
+	local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(4003),10,1480.80000488281,-1745.69999511719,34.3)
+removeWorldModel(4003,10,1480.80000488281,-1745.69999511719,34.3)
+
+--StadthalleFenster
+	local fensterNormalDistanz = engineGetModelLODDistance(4217)--1661.9000244141,-1826.6999511719,24.799999237061)
+	local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(4217),100,1488.80000488281,-1852.5,22.3999999)
+removeWorldModel(4217,100,1488.80000488281,-1852.5,22.3999999)
+
+
+--Usertreff
+	local fensterNormalDistanz = engineGetModelLODDistance(647)--1661.9000244141,-1826.6999511719,24.799999237061)
+	local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(647),100,1781.29517 ,-1375.68250, 13.12458)
+removeWorldModel(647,100,1781.29517 ,-1375.68250, 13.12458)
+
+--Usertreff
+	local fensterNormalDistanz = engineGetModelLODDistance(762)--1661.9000244141,-1826.6999511719,24.799999237061)
+	local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(762),1000,1781.29517 ,-1375.68250, 13.12458)
+removeWorldModel(762,100,1781.29517 ,-1375.68250, 13.12458)
+
+
+--Usertreff
+	local fensterNormalDistanz = engineGetModelLODDistance(800)--1661.9000244141,-1826.6999511719,24.799999237061)
+	local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(800),1000,1781.29517 ,-1375.68250, 13.12458)
+removeWorldModel(800,100,1781.29517 ,-1375.68250, 13.12458)
+
+local fensterNormalDistanz = engineGetModelLODDistance(5788)--1661.9000244141,-1826.6999511719,24.799999237061)
+	local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(5788),1000,1071.5999755859 ,-1305, 13.199999809265)
+removeWorldModel(5788,100,1071.5999755859 ,-1305, 13.199999809265)
+
+--SCHRIFT MEDIC BASE
+local fensterNormalDistanz = engineGetModelLODDistance(5788)
+local actualLODDistance = math.min( 300, fensterNormalDistanz * ( dxGetStatus().SettingDrawDistance / 100 + 1 ) )
+removeWorldModel(engineGetModelLODDistance(5788),1000,1072.5 ,-1296.4000244141, 17.299999237061)
+removeWorldModel(5788,100,1072.5 ,-1296.4000244141, 17.299999237061)
+
+
