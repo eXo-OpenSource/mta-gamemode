319ba5b25e04ba73b96a26d973b79a66389854f1
Stumpy <danieltaucher@gmail.com>
2016-04-02 18:39:34 +0200

    Updated MostWanted

---
+++ exo_maps/jobs/skinPreviewHead.php
@@ -0,0 +1,12 @@
+﻿<style>
+	body{
+		overflow:hidden;
+	}
+</style>
+<center>
+<?php 
+	if (isset($_GET["skin"])){
+		echo '<img style="height:90%;" src="../../images/skins/Skin'.$_GET["skin"].'.jpg" />';
+	}
+?>
+</center>
--- vrp/client/classes/GUIForms/MostWanted3D.lua
+++ vrp/client/classes/GUIForms/MostWanted3D.lua
@@ -5,7 +5,7 @@ inherit(Singleton, MostWanted)
 local DRAWN_WANTEDS = {4, 5, 6}
 
 function MostWanted:constructor()
-	GUIForm3D.constructor(self, Vector3(1540.925, -1661.18, 15.24), Vector3(0, 0, 90), Vector2(3.73, 5), Vector2(900,1200), 5)
+	GUIForm3D.constructor(self, Vector3(1540.925, -1661.18, 15.24), Vector3(0, 0, 90), Vector2(3.73, 5), Vector2(900,1200), 50)
 end
 
 function MostWanted:onStreamIn(surface)
@@ -27,19 +27,15 @@ function MostWanted:refresh()
 end
 
 function MostWanted:outputWantedPlayer(player)
-	local count = self.m_WantedPlayerCount
+	if self.m_WantedPlayerCount < 8 then
+		self.m_WantedPlayer[player] = GUIWebView:new(10+self.m_Column*210, 10+self.m_Row*270, 200, 260, "http://exo-reallife.de/ingame/other/mostWanted.php?name="..player:getName().."&wanteds="..player:getWantedLevel().."&skin="..player:getModel(), true, self.m_Surface)
+		self.m_WantedPlayerCount = self.m_WantedPlayerCount + 1
 
-	self.m_WantedPlayer[player] = {}
-	self.m_WantedPlayer[player]["wanted"] = GUIImage:new(10+self.m_Column*175, 10+self.m_Row*175, 40, 30, "files/images/Nametag/w"..player:getWantedLevel()..".png", self.m_Surface)
-	self.m_WantedPlayer[player]["name"] = GUILabel:new(45+self.m_Column*175, 10+self.m_Row*175, 150, 30,player:getName(), self.m_Surface)
-	self.m_WantedPlayer[player]["skin"] = GUIWebView:new(30+self.m_Column*175, 10+self.m_Row*175, 170, 100, "http://exo-reallife.de/ingame/skinPreview/skinPreview.php?skin="..player:getModel(), true, self.m_Surface)
-
-	self.m_WantedPlayerCount = count + 1
-
-	if count == 4 or count == 9 then
-		self.m_Row = 0
-		self.m_Column = self.m_Column+1
-	else
-		self.m_Row = self.m_Row+1
+		if self.m_WantedPlayerCount == 4 or self.m_WantedPlayerCount == 8 then
+			self.m_Row = 0
+			self.m_Column = self.m_Column+1
+		else
+			self.m_Row = self.m_Row+1
+		end
 	end
 end
