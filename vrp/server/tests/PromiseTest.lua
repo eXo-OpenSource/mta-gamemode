cfe980307d0144640ea522fd0ae65b200d6da34e
StiviiK <stefan.kuerzeder@gmail.com>
2016-04-04 14:42:31 +0200

    Added an advanced promise test (to test promise.next)

--- vrp/server/tests/PromiseTest.lua
+++ vrp/server/tests/PromiseTest.lua
@@ -58,3 +58,46 @@ function PromiseTest:test_Thread()
 	Thread:new(func, THREAD_PRIORITY_HIGHEST).done(fullfill, reject)
 	self:yield()
 end
+
+function PromiseTest:test_AdvancedPromise()
+	-- See JS-Example: https://www.promisejs.org @ Constructing a promise + Transformation / Chaining
+
+	local readString = function ()
+		local promise = Promise:new(function (fullfill, reject)
+			-- Example (here shorten): We're reading a json string from a file
+			local err = false -- err is true, when file reading fails
+			if err then
+				reject("A error")
+			else
+				-- The string is the json string from a file
+				fullfill("[ { \"a_number\": 12, \"date_of_birth\": \"11.05.1998\", \"name\": \"StiviK\", \"a_boolean\": false } ]")
+			end
+		end)
+
+		Promise.addNext(promise)
+		return promise
+	end
+	--[[
+	local parseJSON = function (res) -- function which parses the json
+		return fromJSON(res)
+	end
+	--]]
+	local readJSON = function () -- function which reads json from the file and parses it
+		return readString().next(fromJSON)
+	end
+
+	readJSON().done( -- implement handlers for the readJSON function
+		function (result) -- function if the progress is successfull
+			self:assertTableEquals(result, {["name"] = "StiviK", ["date_of_birth"] = "11.05.1998", ["a_number"] = 12, ["a_boolean"] = false})
+			self:resume()
+		end,
+		function (err) -- err function if file reading fails
+			outputDebug(("[PromiseTest:test_AdvancedPromise]: %s"):format(tostring(err)))
+
+			self:assertTrue(true)
+			self:resume()
+		end
+	)
+
+	self:yield()
+end
