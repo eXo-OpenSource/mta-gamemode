48171ec9be407763b0f84edefdf3b84b29ed9763
Stumpy <danieltaucher@gmail.com>
2016-04-23 16:45:54 +0200

    Fixed Issue #7 - HelpBar hide

--- vrp/client/classes/GUIForms/HelpBar.lua
+++ vrp/client/classes/GUIForms/HelpBar.lua
@@ -24,11 +24,11 @@ function HelpBar:constructor()
 	self.m_Icon.onUnhover = function () self.m_Icon:setColor(Color.White) end
 
 	self.m_Rectangle = GUIRectangle:new(self.m_Width, 0, self.m_Width, self.m_Height, tocolor(0, 0, 0, 200), self)
-	self.m_TitleLabel = GUILabel:new(self.m_Width*0.05, self.m_Height*0.01, self.m_Width*0.9, self.m_Height*0.1, _"Hilfe", self.m_Rectangle):setColor(Color.LightBlue)
+	self.m_TitleLabel = GUILabel:new(self.m_Width*0.05, self.m_Height*0.01, self.m_Width*0.7, self.m_Height*0.1, _"Hilfe", self.m_Rectangle):setColor(Color.LightBlue)
 	self.m_SubTitleLabel = GUILabel:new(self.m_Width*0.05, self.m_Height*0.09, self.m_Width*0.9, self.m_Height*0.04, "Kein Text", self.m_Rectangle):setColor(Color.LightBlue)
 	self.m_TextLabel = GUILabel:new(self.m_Width*0.05, self.m_Height*0.15, self.m_Width*0.9, self.m_Height*0.8, "", self.m_Rectangle):setFont(VRPFont(self.m_Height*0.03))
 
-	self.m_CloseButton = GUILabel:new(self.m_Width*0.8, self.m_Height*0.005, self.m_Width*0.2, self.m_Height*0.1, "⇛", self.m_Rectangle):setColor(Color.LightBlue)
+	self.m_CloseButton = GUILabel:new(self.m_Width*0.75, self.m_Height*0.01, self.m_Width*0.25, self.m_Height*0.1, "⇛", self.m_Rectangle):setColor(Color.LightBlue)
 	self.m_CloseButton.onLeftClick = function() self:fadeOut() end
 	self.m_CloseButton.onHover = function () self.m_CloseButton:setColor(Color.White) end
 	self.m_CloseButton.onUnhover = function () self.m_CloseButton:setColor(Color.LightBlue) end
