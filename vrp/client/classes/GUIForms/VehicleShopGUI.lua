89c9f7f71bccc3d0e0855fa81e6425bd96df99c0
gatno <danieltaucher@gmail.com>
2016-04-25 11:28:38 +0200

    Added Vehicle-Info Window on VehicleBuy-Menu

--- vrp/client/classes/GUIForms/VehicleShopGUI.lua
+++ vrp/client/classes/GUIForms/VehicleShopGUI.lua
@@ -39,6 +39,7 @@ function VehicleShopGUI:destructor()
 	setTimer(function()
 		setCameraTarget(localPlayer, localPlayer)
 	end, 2000, 1)
+	if self.m_InfoInstance then delete(self.m_InfoInstance) end
 	GUIForm.destructor(self)
 end
 
@@ -69,6 +70,8 @@ function VehicleShopGUI:setVehicleList(list)
 		item.VehicleId = k
 		item.onLeftClick = function()
 			self.m_CurrentVehicle = v[1]
+			if not self.m_InfoInstance then self.m_InfoInstance = VehicleShopInfoGUI:new(self.m_CurrentVehicle) end
+			self.m_InfoInstance:updateVehicle(v[1])
 			self:updateMatrix()
 		end
 		item.onLeftDoubleClick = bind(self.buyVehicle, self)
@@ -104,3 +107,48 @@ addEventHandler("showVehicleShopMenu", root, function(id, name, image, vehicles)
 
 	VehicleShopGUI:getSingleton():setVisible(true)
 end)
+
+VehicleShopInfoGUI = inherit(GUIForm)
+inherit(Singleton, VehicleShopInfoGUI)
+
+function VehicleShopInfoGUI:constructor()
+	GUIForm.constructor(self, screenWidth-250, screenHeight-200, 250, 200)
+
+
+	self.m_Window = GUIWindow:new(0, 0, self.m_Width, self.m_Height, "Fahrzeug", true, false, self)
+	self.m_Gears = GUILabel:new(10, 35, self.m_Width-20, 20, "" , self.m_Window)
+	self.m_Weight = GUILabel:new(10, 55, self.m_Width-20, 20, "", self.m_Window)
+	self.m_MaxSpeed = GUILabel:new(10, 75, self.m_Width-20, 20, "", self.m_Window)
+	self.m_DriveType = GUILabel:new(10, 95, self.m_Width-20, 20, "", self.m_Window)
+	self.m_Seats = GUILabel:new(10, 115, self.m_Width-20, 20, "", self.m_Window)
+	self.m_ABS = GUILabel:new(10, 135, self.m_Width-20, 20, "", self.m_Window)
+
+	addEvent("vehicleBought", true)
+	addEventHandler("vehicleBought", root,
+		function()
+			delete(self)
+			SuccessBox:new(_"Glückwunsch! Du bist nun Besitzer eines neuen Fahrzeugs!", 0, 255, 0)
+		end
+	)
+
+	showChat(false)
+end
+
+function VehicleShopInfoGUI:updateVehicle(veh)
+	local handling = veh:getHandling()
+	local driveType = {["fwd"] = _"Front-Antrieb", ["rwd"] = _"Heck-Antrieb", ["awd"] = _"Allrad-Antrieb"}
+
+	self.m_Window:setTitleBarText(veh:getName())
+	self.m_Gears:setText(_("%d Gang Getriebe", handling["numberOfGears"]))
+	self.m_Weight:setText(_("%d kg Leergewicht", handling["mass"]))
+	self.m_MaxSpeed:setText(_("%d km/H Höchstgeschwindigkeit", handling["maxVelocity"]*0.9))
+	self.m_DriveType:setText(_("%s", driveType[handling["driveType"]]))
+	self.m_Seats:setText(_("%d Sitzplätze", veh:getMaxPassengers()+1))
+
+	if handling["ABS"] == true then
+		self.m_ABS:setText("ABS")
+	else
+		self.m_ABS:setText("")
+	end
+
+end
