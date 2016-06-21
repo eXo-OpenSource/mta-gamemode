1132bfad1e50ed677db29fdc865e7a874c1a490d
Strobe <devcxo@gmail.com>
2016-06-21 15:45:38 +0200

    2dae4c0

--- vrp/server/classes/Inventory/ItemSellContract.lua
+++ vrp/server/classes/Inventory/ItemSellContract.lua
@@ -43,14 +43,15 @@ function ItemSellContract:Event_OnTradeSuceed( player, price, car )
 		local money = source:getMoney()
 		price = tonumber( price )
 		if money >= price then
-			player:triggerEvent("closeVehicleAccept")
+			source:triggerEvent("closeVehicleAccept")
 			source:sendInfo(_("Der Handel wurde abgeschlossen!", source))
 			player:sendInfo(_("Der Handel wurde abgeschlossen!", player))
-			car:setOwner( player ) 
-			car:setData("OwnerName", player.name, true)
-			player:takeMoney( price ) 
-			source:giveMoney( price )
-			InventoryManager:getSingleton():getPlayerInventory( source ):removeItem("Handelsvertrag", 1)
+			car:setOwner( source ) 
+			car:setData("OwnerName", source.name, true)
+			source:takeMoney( price ) 
+			player:giveMoney( price )
+			car.m_Keys = {}
+			player:getInventory():removeItem("Handelsvertrag", 1)
 		else 
 			source:sendError(_("Du hast nicht genügend Geld!", source))
 			player:sendInfo(_("Der Käufer hat zu wenig Geld!", player))
@@ -62,9 +63,12 @@ function ItemSellContract:use(player)
 	local veh = getPedOccupiedVehicle( player )
 	if veh then 
 		if veh:getOwner() == player:getId() then
-			local time = getRealTime()
-			local dataTable = { time.monthday, time.month, time.year}
-			triggerClientEvent("vehicleStartSell", player,dataTable )
+			if veh:isPermanent() then
+				local time = getRealTime()
+				local dataTable = { time.monthday, time.month, time.year}
+				triggerClientEvent("vehicleStartSell", player,dataTable )
+			else player:sendError(_("Ungültiges Fahrzeug!", player))
+			end
 		else player:sendError(_("Dies ist nicht dein Fahrzeug!", player))
 		end
 	else player:sendError(_("Du musst in einem Fahrzeug drinnen sitzen!", player))
