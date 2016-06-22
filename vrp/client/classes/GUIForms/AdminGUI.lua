fa777d9699e4c67f5f655aa18ba7b193ee55204a
Strobe <devcxo@gmail.com>
2016-06-22 22:00:08 +0200

    Added Anounce Sound

--- vrp/client/classes/GUIForms/AdminGUI.lua
+++ vrp/client/classes/GUIForms/AdminGUI.lua
@@ -250,6 +250,7 @@ addEventHandler("setDamageFree", root,
 addEventHandler("announceText", root,
 	function(message)
 		AdminGUI.m_MoveText = GUIMovetext:new(0, 0, screenWidth, screenHeight*0.05,message,"",1,(screenWidth*0.1)*-1, "files/images/GUI/megafone.png",true)
+		playSound("files/audio/announcment.mp3")
 	end
 )
 
