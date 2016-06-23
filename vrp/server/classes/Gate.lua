c6d6a99c777dd7ee794978c1114e9e9da720f8a7
FHBane <devcxo@gmail.com>
2016-06-23 05:23:04 +0200

    Little issue fix in Gate.lua

--- vrp/server/classes/Gate.lua
+++ vrp/server/classes/Gate.lua
@@ -5,7 +5,7 @@ function Gate:constructor(model, pos, rot, openPos, customOffset)
     self.m_OpenPos = openPos
     self.m_Gate = createObject(model, pos, rot)
     self:createColshapes(customOffset)
-
+	self.m_Closed = true
     addEventHandler("onColShapeHit", self.m_ColShape1, bind(self.Event_onColShapeHit, self))
     addEventHandler("onColShapeHit", self.m_ColShape2, bind(self.Event_onColShapeHit, self))
 end
@@ -24,8 +24,8 @@ function Gate:createColshapes(customOffset)
         x1, y1 = getPointFromDistanceRotation(pos.x, pos.y, 4, -rot.z+80)
         x2, y2 = getPointFromDistanceRotation(pos.x, pos.y, 4, rot.z+60)
 	elseif model == 2938 then 
-		x1, y1 = getPointFromDistanceRotation(pos.x, pos.y, 4, rot.z-90)
-        x2, y2 = getPointFromDistanceRotation(pos.x, pos.y, 6, rot.z+90)
+		x1, y1 = getPointFromDistanceRotation(pos.x+2, pos.y, 4, rot.z-90)
+        x2, y2 = getPointFromDistanceRotation(pos.x-2, pos.y-1, 4, rot.z+90)
     end
     self.m_Marker1 = createMarker(Vector3(x1, y1, pos.z - 1.75) + self.m_Gate.matrix.forward*(customOffset and -customOffset or -2),"cylinder",1) -- Developement Test
     self.m_Marker2 = createMarker(Vector3(x2, y2, pos.z - 1.75) + self.m_Gate.matrix.forward*(customOffset or 2),"cylinder",1,255) -- Developement Test
