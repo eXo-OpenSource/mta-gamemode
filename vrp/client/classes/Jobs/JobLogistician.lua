3b47173200c6b97e2fc699fff0123edcbfbf626c
Stumpy <danieltaucher@gmail.com>
2016-07-04 00:14:01 +0200

    Fixed Bug #161 - Logistican-Job in Helpmenu

--- vrp/client/classes/Jobs/JobLogistician.lua
+++ vrp/client/classes/Jobs/JobLogistician.lua
@@ -12,7 +12,7 @@ function JobLogistician:constructor()
 	Job.constructor(self, 16, 2407.25, -2439.00, 13.63, 227, "Logistician.png", "files/images/Jobs/HeaderLogistician.png", _(HelpTextTitles.Jobs.Logistician):gsub("Job: ", ""), _(HelpTexts.Jobs.Logistician))
 
 	-- add job to help menu
-	--HelpTextManager:getSingleton():addText("Jobs", _(HelpTextTitles.Jobs.Logistician):gsub("Job: ", ""), _(HelpTexts.Jobs.Logistician))
+	HelpTextManager:getSingleton():addText("Jobs", _(HelpTextTitles.Jobs.Logistician):gsub("Job: ", ""), _(HelpTexts.Jobs.Logistician))
 end
 
 function JobLogistician:start()
--- vrp/client/constants.lua
+++ vrp/client/constants.lua
@@ -299,7 +299,7 @@ HelpTexts = {
 		]];
 		Logistician = [[
 			Als Logistiker ist es dein Job, Container zum anderen Verladezentrum zu fahren.
-			Dazu hast du einen DFT-300 zur verfügung, den du dir am roten Marker erstellen kannst.
+			Dazu hast du einen DFT-300 zur Verfügung, den du dir am roten Marker erstellen kannst.
 			Anschließend musst du unter den Ladekran fahren und warten bis der Container aufgeladen wurde.
 			Dein Ziel wird dir auf der Karte markiert.
 		]];
