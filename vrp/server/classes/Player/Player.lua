2dbad6c2a814fb8bf9fd2f93a5bfb0481ef8fb14
Stumpy <danieltaucher@gmail.com>
2016-08-01 00:07:56 +0200

    Added max weapon skills at login

--- vrp/server/classes/Player/Player.lua
+++ vrp/server/classes/Player/Player.lua
@@ -144,6 +144,10 @@ function Player:loadCharacter()
 	-- Gangwar
 	triggerEvent("onLoadCharacter",self)
 
+	for _, stat in ipairs({ 69, 70, 71, 72, 73, 74, 76, 77, 78, 79 }) do
+      setPedStat(self, stat, 1000)
+   end
+
 	-- Premium
 	Premium.constructor(self)
 end
