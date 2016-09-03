ceaf23f9f89d82e8f8959c5861cb65b250b5f6fc
Stumpy <danieltaucher@gmail.com>
2016-09-04 00:21:36 +0200

    Fixed Bug #215 - Skin set on Duty Factions at Invite

--- vrp/server/classes/Faction/Faction.lua
+++ vrp/server/classes/Faction/Faction.lua
@@ -165,7 +165,9 @@ function Faction:addPlayer(playerId, rank)
 	local player = Player.getFromId(playerId)
 	if player then
 		player:setFaction(self)
-		self:changeSkin(player)
+		if self:isEvilFaction() then
+			self:changeSkin(player)
+		end
 	end
 
 	sql:queryExec("UPDATE ??_character SET FactionId = ?, FactionRank = ? WHERE Id = ?", sql:getPrefix(), self.m_Id, rank, playerId)
