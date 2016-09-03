97d1fa61527d7633a78660e831b1d98f00e0498d
MegaThorx <megathorx@gmail.com>
2016-09-03 13:46:08 +0200

    Added debugging stuff for bug with Group:refreshAttachedMarker

--- vrp/server/classes/Groups/Group.lua
+++ vrp/server/classes/Groups/Group.lua
@@ -1,79 +1,79 @@
 -- ****************************************************************************
 -- *
--- *  PROJECT:     vRoleplay
--- *  FILE:        server/classes/Group.lua
--- *  PURPOSE:     Group class
+-- * PROJECT: vRoleplay
+-- * FILE: server/classes/Group.lua
+-- * PURPOSE: Group class
 -- *
 -- ****************************************************************************
 Group = inherit(Object)
 
 function Group:constructor(Id, name, money, players, karma, lastNameChange, rankNames, rankLoans, type)
-	self.m_Id = Id
+  self.m_Id = Id
 
-	self.m_Players = players or {}
-	self.m_Name = name
-	self.m_Money = money or 0
-	self.m_ProfitProportion = 0.5 -- Amount of money for the group fund
-	self.m_Invitations = {}
-	self.m_Karma = karma or 0
-	self.m_LastNameChange = lastNameChange or 0
-	self.m_VehiclesCanBeModified = true
-	self.m_Type = type
+  self.m_Players = players or {}
+  self.m_Name = name
+  self.m_Money = money or 0
+  self.m_ProfitProportion = 0.5 -- Amount of money for the group fund
+  self.m_Invitations = {}
+  self.m_Karma = karma or 0
+  self.m_LastNameChange = lastNameChange or 0
+  self.m_VehiclesCanBeModified = true
+  self.m_Type = type
 
-	local saveRanks = false
-	if rankNames == "" then	rankNames = {} for i=0,6 do rankNames[i] = "Rang "..i end rankNames = toJSON(rankNames) outputDebug("Created RankNames for group "..Id) saveRanks = true end
-	if rankLoans == "" then	rankLoans = {} for i=0,6 do rankLoans[i] = 0 end rankLoans = toJSON(rankLoans) outputDebug("Created RankLoans for group "..Id) saveRanks = true end
-	self.m_RankNames = fromJSON(rankNames)
-	self.m_RankLoans = fromJSON(rankLoans)
-	if saveRanks == true then
-		self:saveRankSettings()
-	end
+  local saveRanks = false
+  if rankNames == "" then rankNames = {} for i=0,6 do rankNames[i] = "Rang "..i end rankNames = toJSON(rankNames) outputDebug("Created RankNames for group "..Id) saveRanks = true end
+  if rankLoans == "" then rankLoans = {} for i=0,6 do rankLoans[i] = 0 end rankLoans = toJSON(rankLoans) outputDebug("Created RankLoans for group "..Id) saveRanks = true end
+  self.m_RankNames = fromJSON(rankNames)
+  self.m_RankLoans = fromJSON(rankLoans)
+  if saveRanks == true then
+    self:saveRankSettings()
+  end
 
-	self.m_PhoneNumber = (PhoneNumber.load(4, self.m_Id) or PhoneNumber.generateNumber(4, self.m_Id))
-	self.m_PhoneTakeOff = bind(self.phoneTakeOff, self)
+  self.m_PhoneNumber = (PhoneNumber.load(4, self.m_Id) or PhoneNumber.generateNumber(4, self.m_Id))
+  self.m_PhoneTakeOff = bind(self.phoneTakeOff, self)
 end
 
 function Group:destructor()
 end
 
 function Group.create(name, type)
-	if sql:queryExec("INSERT INTO ??_groups (Name,Type) VALUES(?,?)", sql:getPrefix(), name,type) then
-		local group = Group:new(sql:lastInsertId(), name)
+  if sql:queryExec("INSERT INTO ??_groups (Name,Type) VALUES(?,?)", sql:getPrefix(), name,type) then
+    local group = Group:new(sql:lastInsertId(), name)
 
-		-- Add refernece
-		GroupManager:getSingleton():addRef(group)
+    -- Add refernece
+    GroupManager:getSingleton():addRef(group)
 
-		return group
-	end
-	return false
+    return group
+  end
+  return false
 end
 
 function Group:purge()
-	if sql:queryExec("DELETE FROM ??_groups WHERE Id = ?", sql:getPrefix(), self.m_Id) then
-		-- Remove all players
-		for playerId in pairs(self.m_Players) do
-			self:removePlayer(playerId)
-		end
+  if sql:queryExec("DELETE FROM ??_groups WHERE Id = ?", sql:getPrefix(), self.m_Id) then
+    -- Remove all players
+    for playerId in pairs(self.m_Players) do
+      self:removePlayer(playerId)
+    end
 
-		-- Remove reference
-		GroupManager:getSingleton():removeRef(self)
+    -- Remove reference
+    GroupManager:getSingleton():removeRef(self)
 
-		-- Free owned gangareas
-	--	GangAreaManager:getSingleton():freeAreas()
+    -- Free owned gangareas
+    -- GangAreaManager:getSingleton():freeAreas()
 
-		-- TODO: Should we also free the number?
+    -- TODO: Should we also free the number?
 
-		return true
-	end
-	return false
+    return true
+  end
+  return false
 end
 
 function Group:getId()
-	return self.m_Id
+  return self.m_Id
 end
 
 function Group:getType()
-	return self.m_Type
+  return self.m_Type
 end
 
 function Group:getVehicles()
@@ -85,279 +85,280 @@ function Group:canVehiclesBeModified()
 end
 
 function Group:setName(name)
-	local timestamp = getRealTime().timestamp
-	if not sql:queryExec("UPDATE ??_groups SET Name = ?, lastNameChange = ?, RankNames = ?, RankLoans = ? WHERE Id = ?", sql:getPrefix(), name, timestamp, toJSON(self.m_RankNames), toJSON(self.m_RankLoans), self.m_Id) then
-		return false
-	end
-	triggerClientEvent("gangAreaOnGroupNameChange", root, self.m_Name, name)
+  local timestamp = getRealTime().timestamp
+  if not sql:queryExec("UPDATE ??_groups SET Name = ?, lastNameChange = ?, RankNames = ?, RankLoans = ? WHERE Id = ?", sql:getPrefix(), name, timestamp, toJSON(self.m_RankNames), toJSON(self.m_RankLoans), self.m_Id) then
+    return false
+  end
+  triggerClientEvent("gangAreaOnGroupNameChange", root, self.m_Name, name)
 
-	self.m_Name = name
-	self.m_LastNameChange = timestamp
+  self.m_Name = name
+  self.m_LastNameChange = timestamp
 
-	for i, player in pairs(self:getOnlinePlayers()) do
-		player:setPublicSync("GroupName", self:getName())
-	end
+  for i, player in pairs(self:getOnlinePlayers()) do
+    player:setPublicSync("GroupName", self:getName())
+  end
 
-	return true
+  return true
 end
 
 function Group:saveRankSettings()
-	if not sql:queryExec("UPDATE ??_groups SET RankNames = ?, RankLoans = ? WHERE Id = ?", sql:getPrefix(), toJSON(self.m_RankNames), toJSON(self.m_RankLoans), self.m_Id) then
-		return false
-	end
-	return true
+  if not sql:queryExec("UPDATE ??_groups SET RankNames = ?, RankLoans = ? WHERE Id = ?", sql:getPrefix(), toJSON(self.m_RankNames), toJSON(self.m_RankLoans), self.m_Id) then
+    return false
+  end
+  return true
 end
 
 function Group:getName()
-	return self.m_Name
+  return self.m_Name
 end
 
 function Group:getKarma()
-	return self.m_Karma
+  return self.m_Karma
 end
 
 function Group:setKarma(karma)
-	self.m_Karma = karma
+  self.m_Karma = karma
 
-	sql:queryExec("UPDATE ??_groups SET Karma = ? WHERE Id = ?", sql:getPrefix(), self.m_Karma, self.m_Id)
+  sql:queryExec("UPDATE ??_groups SET Karma = ? WHERE Id = ?", sql:getPrefix(), self.m_Karma, self.m_Id)
 end
 
 function Group:setRankName(rank,name)
-	self.m_RankNames[tostring(rank)] = name
+  self.m_RankNames[tostring(rank)] = name
 end
 
 function Group:setRankLoan(rank,amount)
-	self.m_RankLoans[tostring(rank)] = amount
+  self.m_RankLoans[tostring(rank)] = amount
 end
 
 function Group:paydayPlayer(player)
-	local rank = self.m_Players[player:getId()]
-	local loan = tonumber(self.m_RankLoans[tostring(rank)])
-	if self:getMoney() < loan then loan = self:getMoney() end
-	self:takeMoney(loan, "Payday-Auszahlung")
-	return loan
+  local rank = self.m_Players[player:getId()]
+  local loan = tonumber(self.m_RankLoans[tostring(rank)])
+  if self:getMoney() < loan then loan = self:getMoney() end
+  self:takeMoney(loan, "Payday-Auszahlung")
+  return loan
 end
 
 function Group:giveKarma(karma)
-	self:setKarma(self:getKarma() + karma)
+  self:setKarma(self:getKarma() + karma)
 end
 
 function Group:getKarma()
-	return self.m_Karma
+  return self.m_Karma
 end
 
 function Group:isEvil()
-	return self:getKarma() < 0
+  return self:getKarma() < 0
 end
 
 function Group:addPlayer(playerId, rank)
-	if type(playerId) == "userdata" then
-		playerId = playerId:getId()
-	end
+  if type(playerId) == "userdata" then
+    playerId = playerId:getId()
+  end
 
-	rank = rank or 0
-	self.m_Players[playerId] = rank
-	local player = Player.getFromId(playerId)
-	if player then
-		player:setGroup(self)
-	end
+  rank = rank or 0
+  self.m_Players[playerId] = rank
+  local player = Player.getFromId(playerId)
+  if player then
+    player:setGroup(self)
+  end
 
-	sql:queryExec("UPDATE ??_character SET GroupId = ?, GroupRank = ? WHERE Id = ?", sql:getPrefix(), self.m_Id, rank, playerId)
+  sql:queryExec("UPDATE ??_character SET GroupId = ?, GroupRank = ? WHERE Id = ?", sql:getPrefix(), self.m_Id, rank, playerId)
 end
 
 function Group:removePlayer(playerId)
-	if type(playerId) == "userdata" then
-		playerId = playerId:getId()
-	end
+  if type(playerId) == "userdata" then
+    playerId = playerId:getId()
+  end
 
-	self.m_Players[playerId] = nil
-	local player = Player.getFromId(playerId)
-	if player then
-		player:setGroup(nil)
-	end
+  self.m_Players[playerId] = nil
+  local player = Player.getFromId(playerId)
+  if player then
+    player:setGroup(nil)
+  end
 
-	sql:queryExec("UPDATE ??_character SET GroupId = 0, GroupRank = 0 WHERE Id = ?", sql:getPrefix(), playerId)
+  sql:queryExec("UPDATE ??_character SET GroupId = 0, GroupRank = 0 WHERE Id = ?", sql:getPrefix(), playerId)
 end
 
 function Group:invitePlayer(player)
-    client:sendShortMessage(("Du hast %s erfolgreich in deine Gruppe eingeladen."):format(getPlayerName(player)))
+  client:sendShortMessage(("Du hast %s erfolgreich in deine Gruppe eingeladen."):format(getPlayerName(player)))
 
-	player:triggerEvent("groupInvitationRetrieve", self:getId(), self:getName())
+  player:triggerEvent("groupInvitationRetrieve", self:getId(), self:getName())
 
-	self.m_Invitations[player] = true
+  self.m_Invitations[player] = true
 end
 
 function Group:removeInvitation(player)
-	self.m_Invitations[player] = nil
+  self.m_Invitations[player] = nil
 end
 
 function Group:hasInvitation(player)
-	return self.m_Invitations[player]
+  return self.m_Invitations[player]
 end
 
 function Group:isPlayerMember(playerId)
-	if type(playerId) == "userdata" then
-		playerId = playerId:getId()
-	end
+  if type(playerId) == "userdata" then
+    playerId = playerId:getId()
+  end
 
-	return self.m_Players[playerId] ~= nil
+  return self.m_Players[playerId] ~= nil
 end
 
-
 function Group:getPlayerRank(playerId)
-	if type(playerId) == "userdata" then
-		playerId = playerId:getId()
-	end
+  if type(playerId) == "userdata" then
+    playerId = playerId:getId()
+  end
 
-	return self.m_Players[playerId]
+  return self.m_Players[playerId]
 end
 
 function Group:setPlayerRank(playerId, rank)
-	if type(playerId) == "userdata" then
-		playerId = playerId:getId()
-	end
+  if type(playerId) == "userdata" then
+    playerId = playerId:getId()
+  end
 
-	self.m_Players[playerId] = rank
-	sql:queryExec("UPDATE ??_character SET GroupRank = ? WHERE Id = ?", sql:getPrefix(), rank, playerId)
+  self.m_Players[playerId] = rank
+  sql:queryExec("UPDATE ??_character SET GroupRank = ? WHERE Id = ?", sql:getPrefix(), rank, playerId)
 end
 
 function Group:getMoney()
-	return self.m_Money
+  return self.m_Money
 end
 
 function Group:giveMoney(amount, reason)
-	self:setMoney(self.m_Money + amount)
-	StatisticsLogger:getSingleton():addMoneyLog("group", self, amount, reason or "Unbekannt")
+  self:setMoney(self.m_Money + amount)
+  StatisticsLogger:getSingleton():addMoneyLog("group", self, amount, reason or "Unbekannt")
 end
 
 function Group:takeMoney(amount, reason)
-	self:setMoney(self.m_Money - amount)
-	StatisticsLogger:getSingleton():addMoneyLog("group", self, -amount, reason or "Unbekannt")
+  self:setMoney(self.m_Money - amount)
+  StatisticsLogger:getSingleton():addMoneyLog("group", self, -amount, reason or "Unbekannt")
 end
 
 function Group:setMoney(amount)
-	self.m_Money = amount
+  self.m_Money = amount
 
-	sql:queryExec("UPDATE ??_groups SET Money = ? WHERE Id = ?", sql:getPrefix(), self.m_Money, self.m_Id)
+  sql:queryExec("UPDATE ??_groups SET Money = ? WHERE Id = ?", sql:getPrefix(), self.m_Money, self.m_Id)
 end
 
 function Group:getPlayers(getIDsOnly)
-	if getIDsOnly then
-		return self.m_Players
-	end
+  if getIDsOnly then
+    return self.m_Players
+  end
 
-	local temp = {}
-	for playerId, rank in pairs(self.m_Players) do
-		temp[playerId] = {name = Account.getNameFromId(playerId), rank = rank}
-	end
-	return temp
+  local temp = {}
+  for playerId, rank in pairs(self.m_Players) do
+    temp[playerId] = {name = Account.getNameFromId(playerId), rank = rank}
+  end
+  return temp
 end
 
 function Group:getOnlinePlayers()
-	local players = {}
-	for playerId in pairs(self.m_Players) do
-		local player = Player.getFromId(playerId)
-		if player then
-			players[#players + 1] = player
-		end
-	end
-	return players
+  local players = {}
+  for playerId in pairs(self.m_Players) do
+    local player = Player.getFromId(playerId)
+    if player then
+      players[#players + 1] = player
+    end
+  end
+  return players
 end
 
 function Group:sendChatMessage(sourcePlayer,text)
-	local playerId = sourcePlayer:getId()
-	local rank = self.m_Players[playerId]
-	local rankName = self.m_RankNames[tostring(rank)]
-	local text = ("[%s] %s %s: %s"):format(self.m_Type, rankName, sourcePlayer:getName(), text)
-	for k, player in ipairs(self:getOnlinePlayers()) do
-		player:sendMessage(text, 0, 255, 150)
-	end
+  local playerId = sourcePlayer:getId()
+  local rank = self.m_Players[playerId]
+  local rankName = self.m_RankNames[tostring(rank)]
+  local text = ("[%s] %s %s: %s"):format(self.m_Type, rankName, sourcePlayer:getName(), text)
+  for k, player in ipairs(self:getOnlinePlayers()) do
+    player:sendMessage(text, 0, 255, 150)
+  end
 end
 
 function Group:sendMessage(text, r, g, b, ...)
-	for k, player in ipairs(self:getOnlinePlayers()) do
-		player:sendMessage(text, r, g, b, ...)
-	end
+  for k, player in ipairs(self:getOnlinePlayers()) do
+    player:sendMessage(text, r, g, b, ...)
+  end
 end
 
 function Group:sendShortMessage(text, ...)
-	for k, player in ipairs(self:getOnlinePlayers()) do
-		player:sendShortMessage(("%s:\n%s"):format(self:getName(), text), ...)
-	end
+  for k, player in ipairs(self:getOnlinePlayers()) do
+    player:sendShortMessage(("%s:\n%s"):format(self:getName(), text), ...)
+  end
 end
 
 function Group:distributeMoney(amount)
-	local moneyForFund = amount * self.m_ProfitProportion
-	self:giveMoney(moneyForFund, "Gang/Firma")
+  local moneyForFund = amount * self.m_ProfitProportion
+  self:giveMoney(moneyForFund, "Gang/Firma")
 
-	local moneyForPlayers = amount - moneyForFund
-	local onlinePlayers = self:getOnlinePlayers()
-	local amountPerPlayer = math.floor(moneyForPlayers / #onlinePlayers)
+  local moneyForPlayers = amount - moneyForFund
+  local onlinePlayers = self:getOnlinePlayers()
+  local amountPerPlayer = math.floor(moneyForPlayers / #onlinePlayers)
 
-	for k, player in pairs(onlinePlayers) do
-		player:giveMoney(amountPerPlayer, "Gang/Firma")
-	end
+  for k, player in pairs(onlinePlayers) do
+    player:giveMoney(amountPerPlayer, "Gang/Firma")
+  end
 end
 
 function Group:attachPlayerMarkers()
-	self.m_Markers = {}
-	for k, player in ipairs(self:getOnlinePlayers()) do
-		self.m_Markers[player] = createMarker(player:getPosition(),"arrow",0.4,255,0,0,125)
-		self.m_Markers[player]:setDimension(player:getDimension())
-		self.m_Markers[player]:setInterior(player:getInterior())
-		self.m_Markers[player]:attach(player,0,0,1.5)
-		self.m_RefreshAttachedMarker = bind(self.refreshAttachedMarker, self)
-		addEventHandler("onElementDimensionChange", player, self.m_RefreshAttachedMarker)
-		addEventHandler("onElementInteriorChange", player, self.m_RefreshAttachedMarker)
-	end
+  self.m_Markers = {}
+  for k, player in ipairs(self:getOnlinePlayers()) do
+    self.m_Markers[player] = createMarker(player:getPosition(),"arrow",0.4,255,0,0,125)
+    self.m_Markers[player]:setDimension(player:getDimension())
+    self.m_Markers[player]:setInterior(player:getInterior())
+    self.m_Markers[player]:attach(player,0,0,1.5)
+    self.m_RefreshAttachedMarker = bind(self.refreshAttachedMarker, self)
+    addEventHandler("onElementDimensionChange", player, self.m_RefreshAttachedMarker)
+    addEventHandler("onElementInteriorChange", player, self.m_RefreshAttachedMarker)
+  end
 end
 
 function Group:removePlayerMarkers()
-	self.m_Markers = {}
-	for k, player in ipairs(self:getOnlinePlayers()) do
-		self.m_Markers[player]:destroy()
-		removeEventHandler("onElementDimensionChange", self, self.m_RefreshAttachedMarker)
-		removeEventHandler("onElementInteriorChange", self, self.m_RefreshAttachedMarker)
-	end
+  self.m_Markers = {}
+  for k, player in ipairs(self:getOnlinePlayers()) do
+    self.m_Markers[player]:destroy()
+    removeEventHandler("onElementDimensionChange", self, self.m_RefreshAttachedMarker)
+    removeEventHandler("onElementInteriorChange", self, self.m_RefreshAttachedMarker)
+  end
 end
 
 function Group:refreshAttachedMarker()
-	self.m_Markers[source]:setInterior(source:getInterior())
-	self.m_Markers[source]:setDimension(source:getDimension())
+  outputDebugString("marker dimension / intererior wird aktualisert")
+  outputDebugString("int from "..tostring(self.m_Markers[source]:getInterior()).. " to "..tostring(source:getInterior()))
+  outputDebugString("dim from "..tostring(self.m_Markers[source]:getDimension()).. " to "..tostring(source:getDimension()))
+  self.m_Markers[source]:setInterior(source:getInterior())
+  self.m_Markers[source]:setDimension(source:getDimension())
 end
 
-
 function Group:phoneCall(caller)
-	if #self:getOnlinePlayers() > 0 then
-		for k, player in ipairs(self:getOnlinePlayers()) do
-			if not player:getPhonePartner() then
-				player:sendShortMessage(_("Der Spieler %s ruft eure Firma/Gang (%s) an!\nDrücke 'F5' um abzuheben.", player, caller:getName(), self:getName()))
-				bindKey(player, "F5", "down", self.m_PhoneTakeOff, caller)
-			end
-		end
-	else
-		caller:sendShortMessage(_("Es ist aktuell kein Spieler der Firma/Gang online!", caller))
-		caller:triggerEvent("callBusy", caller)
-	end
+  if #self:getOnlinePlayers() > 0 then
+    for k, player in ipairs(self:getOnlinePlayers()) do
+      if not player:getPhonePartner() then
+        player:sendShortMessage(_("Der Spieler %s ruft eure Firma/Gang (%s) an!\nDrücke 'F5' um abzuheben.", player, caller:getName(), self:getName()))
+        bindKey(player, "F5", "down", self.m_PhoneTakeOff, caller)
+      end
+    end
+  else
+    caller:sendShortMessage(_("Es ist aktuell kein Spieler der Firma/Gang online!", caller))
+    caller:triggerEvent("callBusy", caller)
+  end
 end
 
 function Group:phoneTakeOff(player, key, state, caller)
-	self:sendShortMessage(_("%s hat das Telefonat von %s angenommen!", player, player:getName(), caller:getName()))
-	caller:triggerEvent("callAnswer", player, false)
-	player:triggerEvent("callAnswer", caller, false)
-	caller:setPhonePartner(player)
-	player:setPhonePartner(caller)
-	for k, player in ipairs(self:getOnlinePlayers()) do
-		if isKeyBound(player, "F5", "down", self.m_PhoneTakeOff) then
-			unbindKey(player, "F5", "down", self.m_PhoneTakeOff)
-		end
-	end
+  self:sendShortMessage(_("%s hat das Telefonat von %s angenommen!", player, player:getName(), caller:getName()))
+  caller:triggerEvent("callAnswer", player, false)
+  player:triggerEvent("callAnswer", caller, false)
+  caller:setPhonePartner(player)
+  player:setPhonePartner(caller)
+  for k, player in ipairs(self:getOnlinePlayers()) do
+    if isKeyBound(player, "F5", "down", self.m_PhoneTakeOff) then
+      unbindKey(player, "F5", "down", self.m_PhoneTakeOff)
+    end
+  end
 end
 
 function Group:addLog(player, category, text)
-	StatisticsLogger:getSingleton():addGroupLog(player, "group", self, category, text)
+  StatisticsLogger:getSingleton():addGroupLog(player, "group", self, category, text)
 end
 
 function Group:getLog()
-	return StatisticsLogger:getSingleton():getGroupLogs("group", self.m_Id)
+  return StatisticsLogger:getSingleton():getGroupLogs("group", self.m_Id)
 end
--- vrp/server/classes/InteriorEnterExit.lua
+++ vrp/server/classes/InteriorEnterExit.lua
@@ -1,69 +1,71 @@
 -- ****************************************************************************
 -- *
--- *  PROJECT:     vRoleplay
--- *  FILE:        server/classes/InteriorEnterExit.lua
--- *  PURPOSE:     Interior enter/exit helper
+-- * PROJECT: vRoleplay
+-- * FILE: server/classes/InteriorEnterExit.lua
+-- * PURPOSE: Interior enter/exit helper
 -- *
 -- ****************************************************************************
 InteriorEnterExit = inherit(Object)
 
 function InteriorEnterExit:constructor(entryPosition, interiorPosition, enterRotation, exitRotation, interiorId, dimension, isRed)
-	self.m_EnterMarker = createMarker(entryPosition, "corona", 1.5, 255, 255, 255, 200)
-	self.m_ExitMarker = createMarker(interiorPosition, "corona", 1.5, 255, 255, 255, 200)
-	if isRed then -- Debug code
-		self.m_EnterMarker:setColor(255, 0, 0, 200)
-		self.m_ExitMarker:setColor(255, 0, 0, 200)
-	end
+  self.m_EnterMarker = createMarker(entryPosition, "corona", 1.5, 255, 255, 255, 200)
+  self.m_ExitMarker = createMarker(interiorPosition, "corona", 1.5, 255, 255, 255, 200)
+  if isRed then -- Debug code
+    self.m_EnterMarker:setColor(255, 0, 0, 200)
+    self.m_ExitMarker:setColor(255, 0, 0, 200)
+  end
 
-	interiorId = interiorId or 0
-	dimension = dimension or 0
-	self.m_ExitMarker:setInterior(interiorId)
-	self.m_ExitMarker:setDimension(dimension)
+  interiorId = interiorId or 0
+  dimension = dimension or 0
+  self.m_ExitMarker:setInterior(interiorId)
+  self.m_ExitMarker:setDimension(dimension)
 
-	addEventHandler("onMarkerHit", self.m_EnterMarker,
-		function(hitElement, matchingDimension)
-			if getElementType(hitElement) == "player" and matchingDimension and not isPedInVehicle(hitElement) and not hitElement.m_DontTeleport then
-				hitElement.m_DontTeleport = true
-				fadeCamera(hitElement,false,1,0,0,0)
-				setElementFrozen( hitElement, true)
-				setTimer( function() hitElement:setInterior(interiorId) end,1500,1)
-				setTimer( function() hitElement:setPosition(interiorPosition) end, 1500,1)
-				setTimer( function() fadeCamera( hitElement, true,1) end, 2500,1)
-				setTimer( function() hitElement:setDimension(dimension) end, 1500,1)
-				setTimer( function() hitElement:setRotation(0, 0, enterRotation) end, 1500,1)
-				setTimer( function() hitElement:setCameraTarget(hitElement) end, 1500,1)
-				setTimer(function() setElementFrozen( hitElement, false) end, 2500,1)
-				setTimer(function() hitElement.m_DontTeleport = false end, 2500, 1) -- Todo: this is a temp fix
-				triggerEvent("onElementInteriorChange", hitElement, interiorId)
-				triggerEvent("onElementDimensionChange", hitElement, dimension)
-			end
-		end
-	)
-	addEventHandler("onMarkerHit", self.m_ExitMarker,
-		function(hitElement, matchingDimension)
-			if getElementType(hitElement) == "player" and matchingDimension  and not hitElement.m_DontTeleport then
-				hitElement.m_DontTeleport = true
-				fadeCamera(hitElement,false,1,0,0,0)
-				setElementFrozen( hitElement, true)
-				setTimer( function() hitElement:setInterior(0, entryPosition.x, entryPosition.y, entryPosition.z) end,1500,1)
-				setTimer(function() hitElement:setPosition(entryPosition) end,1500,1)
-				setTimer( function() fadeCamera( hitElement, true,1) end, 2500,1)
-				setTimer( function() hitElement:setDimension(0) end, 1500,1)
-				setTimer( function() hitElement:setRotation(0, 0, exitRotation) end, 1500,1)
-				setTimer( function() hitElement:setCameraTarget(hitElement) end, 1500,1)
-				setTimer(function() setElementFrozen( hitElement, false) end, 2500,1)
-				setTimer(function() hitElement.m_DontTeleport = false end, 2500, 1) -- Todo: this is a temp fix
-				triggerEvent("onElementInteriorChange", hitElement, 0)
-				triggerEvent("onElementDimensionChange", hitElement, 0)
-			end
-		end
-	)
+  addEventHandler("onMarkerHit", self.m_EnterMarker,
+    function(hitElement, matchingDimension)
+      if getElementType(hitElement) == "player" and matchingDimension and not isPedInVehicle(hitElement) and not hitElement.m_DontTeleport then
+        hitElement.m_DontTeleport = true
+        fadeCamera(hitElement,false,1,0,0,0)
+        setElementFrozen( hitElement, true)
+        setTimer( function() hitElement:setInterior(interiorId) end,1500,1)
+        setTimer( function() hitElement:setPosition(interiorPosition) end, 1500,1)
+        setTimer( function() fadeCamera( hitElement, true,1) end, 2500,1)
+        setTimer( function() hitElement:setDimension(dimension) end, 1500,1)
+        setTimer( function() hitElement:setRotation(0, 0, enterRotation) end, 1500,1)
+        setTimer( function() hitElement:setCameraTarget(hitElement) end, 1500,1)
+        setTimer(function() setElementFrozen( hitElement, false) end, 2500,1)
+        setTimer(function() hitElement.m_DontTeleport = false end, 2500, 1) -- Todo: this is a temp fix
+        triggerEvent("onElementInteriorChange", hitElement, interiorId)
+        triggerEvent("onElementDimensionChange", hitElement, dimension)
+        outputDebugString("trigger int/dim change to "..tostring(interiorId).."/"..tostring(dimension))
+      end
+    end
+  )
+  addEventHandler("onMarkerHit", self.m_ExitMarker,
+    function(hitElement, matchingDimension)
+      if getElementType(hitElement) == "player" and matchingDimension and not hitElement.m_DontTeleport then
+        hitElement.m_DontTeleport = true
+        fadeCamera(hitElement,false,1,0,0,0)
+        setElementFrozen( hitElement, true)
+        setTimer( function() hitElement:setInterior(0, entryPosition.x, entryPosition.y, entryPosition.z) end,1500,1)
+        setTimer(function() hitElement:setPosition(entryPosition) end,1500,1)
+        setTimer( function() fadeCamera( hitElement, true,1) end, 2500,1)
+        setTimer( function() hitElement:setDimension(0) end, 1500,1)
+        setTimer( function() hitElement:setRotation(0, 0, exitRotation) end, 1500,1)
+        setTimer( function() hitElement:setCameraTarget(hitElement) end, 1500,1)
+        setTimer(function() setElementFrozen( hitElement, false) end, 2500,1)
+        setTimer(function() hitElement.m_DontTeleport = false end, 2500, 1) -- Todo: this is a temp fix
+        triggerEvent("onElementInteriorChange", hitElement, 0)
+        triggerEvent("onElementDimensionChange", hitElement, 0)
+        outputDebugString("trigger int/dim change to 0/0")
+      end
+    end
+  )
 end
 
 function InteriorEnterExit:getEnterMarker()
-	return self.m_EnterMarker
+  return self.m_EnterMarker
 end
 
 function InteriorEnterExit:getExitMarker()
-	return self.m_ExitMarker
+  return self.m_ExitMarker
 end
