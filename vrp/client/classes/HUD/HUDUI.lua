39d9cef1efcf16c6b3df3df510d4b902f00fe44e
gatno <danieltaucher@gmail.com>
2016-09-13 11:01:22 +0200

    Updated eXo-Hud

--- vrp/client/classes/HUD/HUDUI.lua
+++ vrp/client/classes/HUD/HUDUI.lua
@@ -318,15 +318,15 @@ function HUDUI:drawExo()
 		if wanted > 5 then dxDrawImage(screenWidth-width*0.69-r_os,width*0.86,width*0.1,height*0.1,"files/images/HUD/exo/wanted.png") end
 
 		local b_x = 100
-		local bar_x = hudStartX+width*0.3162
+		local bar_x = hudStartX+width*0.316
 		local bar_width = sx*0.145
-		local bar_height = sx*0.0078
+		local bar_height = sx*0.0095
 
 		b_x = localPlayer:getArmor()/100
-		dxDrawImageSection(bar_x, height*0.480,bar_width*b_x,bar_height,scroll_,0,207*b_x,15,'files/images/HUD/exo/blue_b.png',0,0,0,tocolor(255,255,255,200)) -- erster Balken
+		dxDrawImageSection(bar_x, height*0.479,bar_width*b_x,bar_height,scroll_,0,207*b_x,15,'files/images/HUD/exo/blue_b.png',0,0,0,tocolor(255,255,255,200)) -- erster Balken
 
 		b_x = localPlayer:getHealth()/100
-		dxDrawImageSection(bar_x ,height*0.578,bar_width*b_x,bar_height,scroll_,0,207*b_x,15,'files/images/HUD/exo/red_b.png',0,0,0,tocolor(255,255,255,200)) -- zweiter Balken
+		dxDrawImageSection(bar_x ,height*0.577,bar_width*b_x,bar_height,scroll_,0,207*b_x,15,'files/images/HUD/exo/red_b.png',0,0,0,tocolor(255,255,255,200)) -- zweiter Balken
 
 		local karma = localPlayer:getKarma()
 		b_x = math.abs(karma)/150
