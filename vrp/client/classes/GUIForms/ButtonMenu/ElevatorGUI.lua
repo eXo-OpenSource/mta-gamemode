6ce0fce471a31032461e06b162090da1102aee70
gatno <danieltaucher@gmail.com>
2016-11-11 17:45:02 +0100

    Added Elevator Camera Effect [WIP]

--- vrp/client/classes/GUIForms/ButtonMenu/ElevatorGUI.lua
+++ vrp/client/classes/GUIForms/ButtonMenu/ElevatorGUI.lua
@@ -14,6 +14,8 @@ function ElevatorGUI:constructor(elevatorId, stationName, stations)
 
 	self.m_ElevatorId = elevatorId
 
+	self.m_CamBind = bind(self.renderCam, self)
+
 	for stationId, data in pairs(stations) do
 		if data.name ~= stationName then
 			self:addItem(data.name, Color.LightBlue, bind(self.itemCallback, self, stationId))
@@ -27,6 +29,8 @@ function ElevatorGUI:itemCallback(stationId)
     setTimer( function() localPlayer:setPosition(1800.19, -1723.73, 5.89) end, 1250,1)
 	outputChatBox("Todo: Port to elevator interior, (made by krox)")
     setTimer(function() fadeCamera(true,1) end, 1500, 1)
+	setTimer(function() addEventHandler("onClientPreRender", root, self.m_CamBind) end, 1800 ,1)
+
 
 	setTimer(function()
 		Sound.create("files/audio/ElevatorDing.mp3")
@@ -34,10 +38,24 @@ function ElevatorGUI:itemCallback(stationId)
 		fadeCamera(false,1,0,0,0)
     	setTimer(function() fadeCamera(true,1) end, 1500, 1)
 		setTimer(function()
+			removeEventHandler("onClientPreRender", root, self.m_CamBind)
 			triggerServerEvent("elevatorDrive", localPlayer, self.m_ElevatorId, stationId)
+			setCameraTarget(localPlayer)
 		end, 1250, 1)
 	end, 8000, 1)
 	delete(self)
+end
+
+function ElevatorGUI:renderCam()
+	local pos1 = localPlayer:getPosition()
+
+	if not self.m_Object then
+		self.m_Object = createObject(1337, pos1.x+4, pos1.y, pos1.z-1)
+		self.m_Object:setAlpha(0)
+		self.m_Object:move(4000, pos1.x+4, pos1.y, pos1.z+3, 0, 0, 0, "InOutQuad")
+	end
+	local pos2 = self.m_Object:getPosition()
+	setCameraMatrix(pos1, pos2)
 end
 
 addEventHandler("showElevatorGUI", root,
