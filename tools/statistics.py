2e2c7473db40a6207c068ab98cde7dc5a0b97758
StiviiK <stefan.kuerzeder@gmail.com>
2016-11-16 12:12:05 +0100

    Updated statistics.py

--- tools/statistics.py
+++ tools/statistics.py
@@ -6,24 +6,32 @@ fileCount = 0
 scriptFileCount = 0
 lineCount = 0
 
-for root, dirs, files in os.walk("../vrp"):
+for root, dirs, files in os.walk("..\\vrp"):
 	for name in files:
 		fileCount += 1
-	
+
 		if name[-3:] == "lua" or name[-3:] == "xml":
 			scriptFileCount += 1
 			file = open(join(root, name))
-			
-			for line in file:
-				lineCount += 1
-			
+
+			try:
+				lineCount += file.read().count("\n") + 1
+
+			except Exception as e:
+				print("could not read " + join(root, name))
+				print(e)
+				print("")
+
 			file.close()
 
+print("")
 print("======= vRoleplay: Script statistics =======")
 print("")
 print("Number of files:         " + str(fileCount))
 print("Number of scripts:       " + str(scriptFileCount))
 print("Number of lines:         " + str(lineCount))
+print("")
+print("============================================")
 print("\n")
 
 input("Press enter to exit")
