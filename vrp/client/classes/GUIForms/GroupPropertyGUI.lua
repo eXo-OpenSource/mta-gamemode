40f598c8dc6f7ad19335139cbf95b3fae98c8fc4
Strobe <devcxo@gmail.com>
2016-11-18 04:43:33 +0100

    Start of GroupPropertyGUI

--- vrp/client/classes/GUIForms/GroupPropertyGUI.lua
+++ vrp/client/classes/GUIForms/GroupPropertyGUI.lua
@@ -6,3 +6,15 @@
 -- *
 -- ****************************************************************************
 GroupPropertyGUI = inherit(GUIForm)
+inherit(Singleton, GroupPropertyGUI)
+
+function GroupPropertyGUI:constructor() 
+	GUIForm.constructor(self, screenWidth/2 - screenWidth*0.4/2, screenHeight/2 - screenHeight*0.3/2, screenWidth*0.4, screenHeight*0.3)
+	self.m_TabPanel = GUITabPanel:new(0, 0, self.m_Width, self.m_Height, self)
+	local tabAccess = self.m_TabPanel:addTab(_"Berechtigung")
+	self.m_TabAccess = tabAccess
+	
+	GUILabel:new(self.m_Width*0.01, self.m_Height*0.1, self.m_Width*0.65, self.m_Height*0.10, _"Schlüssel:", tabAccess):setFont(VRPFont(self.m_Height*0.12))
+	self.m_PlayerEdit = GUIEdit:new(self.m_Width*0.42, self.m_Height*0.42, self.m_Width*0.5, self.m_Height*0.10, tabAccess)
+	self.m_CreateButton = GUIButton:new(self.m_Width*0.33, self.m_Height*0.8, self.m_Width*0.33, self.m_Height*0.15, _"Vergeben", tabAccess):setBackgroundColor(Color.Green)
+end
