9acfd968c934e621c5f365b4688b57b500b2ba29
Stumpy <danieltaucher@gmail.com>
2016-11-26 22:39:40 +0100

    Fixed SWT Box Load

--- vrp/shared/constants/factions.lua
+++ vrp/shared/constants/factions.lua
@@ -1,6 +1,12 @@
 VEHICLE_BOX_LOAD = {}
 VEHICLE_BOX_LOAD[543] = {["count"]= 1,	[1] = Vector3(0, -1.2, 0)}
-VEHICLE_BOX_LOAD[427] = {["count"]= 2,	[2] = Vector3(0, -1.5, -0.2), [3] = Vector3(0, -3, -0.2)}
+VEHICLE_BOX_LOAD[427] = {["count"]= 2,	[1] = Vector3(0, -1.5, -0.2), [2] = Vector3(0, -3, -0.2)}
+VEHICLE_BOX_LOAD[455] = {["count"]= 8,
+	[1] = Vector3(0.7, -0.1, 0.1), [2] = Vector3(-0.7, -0.1, 0.1), [3] = Vector3(0.7, -1.4, 0.1), [4] = Vector3(-0.7, -1.4, 0.1),
+	[5] = Vector3(-0.7, -2.7, 0.1), [6] = Vector3(0.7, -2.7, 0.1), [7] = Vector3(-0.7, -4, 0.1), [8] = Vector3(0.7, -4, 0.1)
+}
+
+
 
 VEHICLE_BAG_LOAD = {}
 VEHICLE_BAG_LOAD[428] = {["count"]=5,[1] = Vector3(0, -1.2, 0), [2] = Vector3(0, -1.2, 0), [3] = Vector3(0, -1.2, 0), [4] = Vector3(0, -1.2, 0), [5] = Vector3(0, -1.2, 0)}
