d393964ce9ae0720c364e287519c330dea907fc2
Stumpy <danieltaucher@gmail.com>
2016-11-27 03:27:42 +0100

    Added Elevator to Townhall Removed Map while Elevator Drive

--- vrp/server/classes/Gameplay/Townhall.lua
+++ vrp/server/classes/Gameplay/Townhall.lua
@@ -2,7 +2,9 @@ Townhall = inherit(Singleton)
 
 function Townhall:constructor()
 	self.m_Blip = Blip:new("Stadthalle.png", 1788.389, -1297.811,root, 600)
-	self.m_EnterExit = InteriorEnterExit:new(Vector3(1788.389, -1297.811, 13.375), Vector3(1786.800, -1301.099, 120.300), -360, 360, 0, 0, false)
+	local elevator = Elevator:new()
+	elevator:addStation("Ausgang", Vector3(1788.389, -1297.811, 13.375))
+	elevator:addStation("Stadthalle", Vector3(1786.800, -1301.099, 120.300), 120)
 end
 
 function Townhall:destructor()
