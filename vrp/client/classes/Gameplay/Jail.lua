6c7e37b9d085ace385db3df069d2a7c80f1c0b35
Stumpy <danieltaucher@gmail.com>
2016-11-30 01:03:22 +0100

    Addendum to last commit

--- vrp/client/classes/Gameplay/Jail.lua
+++ vrp/client/classes/Gameplay/Jail.lua
@@ -22,61 +22,6 @@ addEventHandler("playerJailed", root,
 				Jail.startCountdown()
 			end)
 		else
-<<<<<<< HEAD
-			if not jailCountdownGUI then
-				InfoBox:new(_("Willkommen im Gefängnis! Hier wirst du nun für die nächsten %d Minuten verweilen!", jailTime))
-				jailCountdownGUI = Countdown:getSingleton():startCountdown(jailTime*60, "Frei in:")
-			end
-=======
-			Jail.startCountdown()
->>>>>>> Added Countdown Event
-		end
-	end
-)
-
-function Jail.startCountdown()
-	InfoBox:new(_("Willkommen im Gefängnis! Hier wirst du nun für die nächsten %d Minuten verweilen!", jailTime))
-	jailCountdownGUI = Countdown:getSingleton():startCountdown(jailTime*60, "Frei in:")
-	jailCountdownGUI:addTickEvent(function()
-			toggleControl("fire", false)
-			toggleControl("aim_weapon", false)
-			toggleControl("jump", false)
-		end)
-end
-
-addEvent("playerLeftJail", true)
-addEventHandler("playerLeftJail", root,
-	function()
-		if jailCountdownGUI then
-			delete(jailCountdownGUI)
-			jailCountdownGUI = nil
-		end
-	end
-)
--- ****************************************************************************
--- *
--- *  PROJECT:     vRoleplay
--- *  FILE:        client/classes/Gameplay/Jail.lua
--- *  PURPOSE:     Client Jail class
--- *
--- ****************************************************************************
-
-local jailCountdownGUI
-
-local Jail = {}
-
-addEvent("playerJailed", true)
-addEventHandler("playerJailed", root,
-	function(jailTime, bCutscene)
-		local jailedTime = getTickCount()
-
-		-- Play arrest cutscene
-		if bCutscene then
-			CutscenePlayer:getSingleton():playCutscene("Arrest",
-			function()
-				Jail.startCountdown()
-			end)
-		else
 			Jail.startCountdown()
 		end
 	end
