f912a5a2a2b52c14b7d28231eaeb03013152420a
Stumpy <danieltaucher@gmail.com>
2016-11-30 01:04:49 +0100

    Addendum to last commit

--- vrp/client/classes/Gameplay/Jail.lua
+++ vrp/client/classes/Gameplay/Jail.lua
@@ -13,21 +13,19 @@ local Jail = {}
 addEvent("playerJailed", true)
 addEventHandler("playerJailed", root,
 	function(jailTime, bCutscene)
-		local jailedTime = getTickCount()
-
 		-- Play arrest cutscene
 		if bCutscene then
 			CutscenePlayer:getSingleton():playCutscene("Arrest",
 			function()
-				Jail.startCountdown()
+				Jail.startCountdown(jailTime)
 			end)
 		else
-			Jail.startCountdown()
+			Jail.startCountdown(jailTime)
 		end
 	end
 )
 
-function Jail.startCountdown()
+function Jail.startCountdown(jailTime)
 	InfoBox:new(_("Willkommen im Gefängnis! Hier wirst du nun für die nächsten %d Minuten verweilen!", jailTime))
 	jailCountdownGUI = Countdown:getSingleton():startCountdown(jailTime*60, "Frei in:")
 	jailCountdownGUI:addTickEvent(function()
