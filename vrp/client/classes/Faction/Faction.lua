91be4cccaf2aab615ec8363b26c3e7c61f00e939
Strobe <devcxo@gmail.com>
2016-12-07 01:24:46 +0100

    Addendum on #52acc32

--- vrp/client/classes/Faction/Faction.lua
+++ vrp/client/classes/Faction/Faction.lua
@@ -48,7 +48,7 @@ function FactionManager:Event_onPlayerCuff( bool )
 end
 
 function FactionManager:drawCuff()
-	dxDrawImage(w*0.88, h - w*0.1, w*0.08,w*0.08,"files/images/Other/cuff.png")
+	dxDrawImage(w*0.88, h - w*0.1, w*0.08,w*0.0436,"files/images/Other/cuff.png")
 end
 
 function FactionManager:stateFactionOfferTicket( cop )
