1a5950353878a3806cd3d69abd614ac4907c5b2e
Strobe <devcxo@gmail.com>
2016-12-08 02:07:25 +0100

    Fixed DoF while in Vehicle

--- [shader]/DoF/DoFShader.lua
+++ [shader]/DoF/DoFShader.lua
@@ -54,7 +54,10 @@ function onRender()
 		local x, y, z, lx, ly, lz = getCameraMatrix()
 		local playerPos = player:getPosition()
 		local viewDistance = getDistanceBetweenPoints3D(x, y, z, playerPos.x, playerPos.y, playerPos.z)
-		
+		if player.vehicle then 
+			playerPos = player.vehicle:getPosition()
+			viewDistance = getDistanceBetweenPoints3D(playerPos.x, playerPos.y, playerPos.z,Vector3(lx,ly,lz))
+		end
 		distance = 10 - viewDistance
 		 
 		if (distance <= 0) then
