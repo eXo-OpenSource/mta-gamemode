542dc9800d8d962cc2253ef7ac32d20379092d12
Stumpy <danieltaucher@gmail.com>
2016-12-13 01:06:04 +0100

    Addendum to last commit

--- vrp/client/classes/Groups/SprayWall.lua
+++ vrp/client/classes/Groups/SprayWall.lua
@@ -106,14 +106,14 @@ function SprayWall:renderTagTexture()
 	-- Render the text to the texture
 	dxSetRenderTarget(self.m_TagSectionTexture, true)
 	--dxDrawRectangle(0, 0, 128, 128, Color.Yellow)
-	dxDrawText(self.m_TagText, 5, 5, 128-5*2, 128-5*2, Color.White, 1, SprayWallManager:getSingleton():getFont(), "center", "center", false, true)
+	dxDrawText(self.m_TagText, 5, 5, 128-5*2, 128-5*2, Color.Red, 1, SprayWallManager:getSingleton():getFont(), "center", "center", false, true)
 	dxSetRenderTarget(nil)
 
 	-- Next, render the text as section to the actual tag texture
-	--dxSetRenderTarget(self.m_TagTexture, true)
-	--dxDrawText(self.m_OldTagText, 5, 5, 128-5*2, 128-5*2, Color.White, 1, SprayWallManager:getSingleton():getFont(), "center", "center", false, true)
-	--dxDrawImageSection(0, 0, 128, math.floor(self.m_TagProgress), 0, 0, 128, math.floor(self.m_TagProgress), self.m_TagSectionTexture)
-	--dxSetRenderTarget(nil)
+	dxSetRenderTarget(self.m_TagTexture, true)
+	dxDrawText(self.m_OldTagText, 5, 5, 128-5*2, 128-5*2, Color.White, 1, SprayWallManager:getSingleton():getFont(), "center", "center", false, true)
+	dxDrawImageSection(0, 0, 128, math.floor(self.m_TagProgress), 0, 0, 128, math.floor(self.m_TagProgress), self.m_TagSectionTexture)
+	dxSetRenderTarget(nil)
 end
 
 function SprayWall:setTagText(text)
