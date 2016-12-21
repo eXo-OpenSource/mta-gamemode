381083c8649da829eee1bcc88ce3de9627e12d31
Jusonex <jusonex96@gmail.com>
2016-12-21 01:16:32 +0100

    Set DEBUG to true for release/testing

--- vrp/shared/debug.lua
+++ vrp/shared/debug.lua
@@ -5,7 +5,7 @@
 -- *  PURPOSE:     Debug stuff
 -- *
 -- ****************************************************************************
-DEBUG = GIT_BRANCH == nil or GIT_BRANCH == "master" or GIT_BRANCH == "develop"
+DEBUG = GIT_BRANCH == nil or GIT_BRANCH == "master" or GIT_BRANCH == "develop" or GIT_BRANCH == "release/testing"
 
 if triggerClientEvent then
 	outputServerLog(("\n\nDebug information:\nDEBUG = %s\nBRANCH = %s\nVERSION = %s\n"):format(tostring(DEBUG), tostring(GIT_BRANCH), tostring(GIT_VERSION)))
