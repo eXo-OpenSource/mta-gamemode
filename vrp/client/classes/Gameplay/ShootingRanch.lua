fd578cd5d4c890544fcd34f7093fc1cdf66c3303
Stumpy <danieltaucher@gmail.com>
2016-12-29 02:03:39 +0100

    Updated ShootingRanch

--- vrp/client/classes/Gameplay/ShootingRanch.lua
+++ vrp/client/classes/Gameplay/ShootingRanch.lua
@@ -3,38 +3,43 @@ inherit(Singleton, ShootingRanch)
 
 addRemoteEvents{"startClientShootingRanch", "stopClientShootingRanch", "showShootingRanchResult"}
 
-function ShootingRanch:constructor( time )
+function ShootingRanch:constructor()
 	GUIForm.constructor(self, screenWidth-220, screenHeight/2-100/2, 180, 200, false)
 
 	self.m_Background = GUIRectangle:new(0, 0, self.m_Width, self.m_Height, tocolor(0,0,0,150), self)
 
 	GUILabel:new(0, 0, self.m_Width, 30, _"Schießstand", self):setAlignX("center"):setAlignY("center"):setColor(Color.LightBlue)
 	self.m_Hits = GUILabel:new(5, 30, self.m_Width-10, 25, _"Bitte schieße auf die Ziele!", self)
-	self.m_Time = GUILabel:new(5, 60, self.m_Width-10, 25, "", self)
+	self.m_TimeLabel = GUILabel:new(5, 60, self.m_Width-10, 25, "", self)
 	self.m_Accuracy = GUILabel:new(5, 90, self.m_Width-10, 25, "", self)
-	self.m_TimeMax = time
+	self.m_Time = 0
 	self.m_Timer = setTimer(bind(self.updateLabels, self), 500, 0)
-
 	self.m_WeaponFireBind = bind(self.onWeaponFire, self)
 	addEventHandler("onClientPlayerWeaponFire", root, self.m_WeaponFireBind)
 end
 
 function ShootingRanch:destructor()
 	removeEventHandler("onClientPlayerWeaponFire", localPlayer, self.m_WeaponFireBind)
-
+	if self.m_Timer then killTimer(self.m_Timer) end
+	if self.m_TimeIncrease then killTimer(self.m_TimeIncrease) end
 	GUIForm.destructor(self)
 end
 
 function ShootingRanch:updateLabels()
 	if getElementData(localPlayer, "ShootingRanch:Data") then
+		if not self.m_TimeIncrease then
+			 self.m_TimeIncrease = setTimer(function()
+			 	self.m_Time = self.m_Time + 1
+			 end, 1000, 0)
+		end
+
 		local data = getElementData(localPlayer, "ShootingRanch:Data")
-		self.m_TimeMax = self.m_TimeMax - 0.5
 		local acc =  data["Hits"]*100/(data["StartMuni"] - localPlayer:getTotalAmmo())
 
 		self.m_Hits:setText(_("Treffer: %d/%d", data["Hits"], data["TargetHits"]))
-		self.m_Time:setText(_("Zeit: %d/%d", math.floor(self.m_TimeMax), data["Time"]))
+		self.m_TimeLabel:setText(_("Zeit: %d/%d", self.m_Time, data["Time"]))
 		self.m_Accuracy:setText(_("Genauigkeit: %d/%d", acc, data["TargetAccuracy"]))
-		if math.floor(self.m_TimeMax) > data["Time"] then
+		if self.m_Time > data["Time"] then
 			triggerServerEvent("ShootingRanch:onTimeUp", localPlayer)
 		end
 	end
@@ -53,8 +58,8 @@ function ShootingRanch:onWeaponFire(weapon, ammo, ammoInClip, hitX, hitY, hitZ, 
 end
 
 
-addEventHandler("startClientShootingRanch", root, function(time)
-	ShootingRanch:new(time)
+addEventHandler("startClientShootingRanch", root, function()
+	ShootingRanch:new()
 end)
 
 addEventHandler("stopClientShootingRanch", root, function()
