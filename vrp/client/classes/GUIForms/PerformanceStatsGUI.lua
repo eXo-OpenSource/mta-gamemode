99ca3e1d70d8732d92dfe1ad9f85b0ba3a357dc6
gatno <danieltaucher@gmail.com>
2016-12-30 18:05:18 +0100

    Updated PerformanceStatsGUI

--- vrp/client/classes/GUIForms/PerformanceStatsGUI.lua
+++ vrp/client/classes/GUIForms/PerformanceStatsGUI.lua
@@ -8,26 +8,49 @@
 PerformanceStatsGUI = inherit(GUIForm)
 inherit(Singleton, PerformanceStatsGUI)
 function PerformanceStatsGUI:constructor()
-	GUIForm.constructor(self, screenWidth-5-screenWidth*0.3, screenHeight*0.3, screenWidth*0.3, screenHeight*0.3)
+	self.m_Elements = {
+		["player"] = "Spieler",
+		["ped"] = "Peds",
+		["vehicle"] = "Fahrzeuge",
+		["object"] = "Objekte",
+		["pickup"] = "Pickups",
+		["marker"] = "Marker",
+		["colshape"] = "Colshape",
+		["texture"] = "Texturen",
+		["shader"] = "Shader",
+
+	}
+
+	GUIForm.constructor(self, screenWidth-30-screenWidth*0.3, screenHeight*0.3, screenWidth*0.3, screenHeight*0.4)
 	self.m_Fields = {}
-	self.m_Window = GUIWindow:new(0, 0, self.m_Width, self.m_Height, "Performance Stats", true, false, self)
-	self:addField("FreeVideoMemory", function() return ("%sMB"):format(dxGetStatus().VideoMemoryFreeForMTA) end)
-	self:addField("VideoMemoryUsedByRenderTargets", function() return ("%sMB"):format(dxGetStatus().VideoMemoryUsedByRenderTargets) end)
-	self:addField("VideoMemoryUsedByTextures", function() return ("%sMB"):format(dxGetStatus().VideoMemoryUsedByTextures) end)
-	self:addField("VideoMemoryUsedByFonts", function() return ("%sMB"):format(dxGetStatus().VideoMemoryUsedByFonts) end)
+	self.m_TabPanel = GUITabPanel:new(0, 0, self.m_Width, self.m_Height, self)
+	local dxStats = self.m_TabPanel:addTab(_"DxStats")
+	self:addField(dxStats, "FreeVideoMemory", function() return ("%sMB"):format(dxGetStatus().VideoMemoryFreeForMTA) end)
+	self:addField(dxStats, "VideoMemoryUsedByRenderTargets", function() return ("%sMB"):format(dxGetStatus().VideoMemoryUsedByRenderTargets) end)
+	self:addField(dxStats, "VideoMemoryUsedByTextures", function() return ("%sMB"):format(dxGetStatus().VideoMemoryUsedByTextures) end)
+	self:addField(dxStats, "VideoMemoryUsedByFonts", function() return ("%sMB"):format(dxGetStatus().VideoMemoryUsedByFonts) end)
+
+	local elements = self.m_TabPanel:addTab(_"Elemente")
+	for type, name in pairs(self.m_Elements) do
+		self:addField(elements, name, function() return ("%d/%d"):format(#getElementsByType(type, root, true), #getElementsByType(type)) end)
+	end
+
 	self.m_RefreshTimer = false
 	self:refresh()
 	self:onShow()
 end
 function PerformanceStatsGUI:refresh()
-	for k, v in ipairs(self.m_Fields) do
-		v.label:setText(v.func())
+	for parentId, parent in pairs(self.m_Fields) do
+		for k, v in ipairs(parent) do
+			v.label:setText(v.func())
+		end
 	end
 end
-function PerformanceStatsGUI:addField(name, getFunc)
-	self.m_Fields[#self.m_Fields + 1] = {func = getFunc}
-	GUILabel:new(self.m_Width*0.02, #self.m_Fields*self.m_Height*0.12, self.m_Width*0.6, self.m_Height*0.11, name..":", self.m_Window)
-	self.m_Fields[#self.m_Fields].label = GUILabel:new(self.m_Width*0.65, #self.m_Fields*self.m_Height*0.12, self.m_Width*0.32, self.m_Height*0.11, "", self.m_Window):setAlignX("right")
+function PerformanceStatsGUI:addField(parent, name, getFunc)
+	if not self.m_Fields[parent] then self.m_Fields[parent] = {} end
+	self.m_Fields[parent][#self.m_Fields[parent] + 1] = {func = getFunc}
+	GUILabel:new(self.m_Width*0.02, #self.m_Fields[parent]*self.m_Height*0.08, self.m_Width*0.7, self.m_Height*0.08, name..":", parent)
+	self.m_Fields[parent][#self.m_Fields[parent]].label = GUILabel:new(self.m_Width*0.65, #self.m_Fields[parent]*self.m_Height*0.08, self.m_Width*0.32, self.m_Height*0.08, "", parent):setAlignX("right")
 end
 function PerformanceStatsGUI:onShow()
 	self.m_RefreshTimer = setTimer(bind(self.refresh, self), 1000, 0)
