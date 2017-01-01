ca566709474b022bcc992da3a3e925b28815f389
Stumpy <danieltaucher@gmail.com>
2017-01-01 23:55:38 +0100

    Removed /chess Command

--- vrp/server/classes/Gameplay/Minigames/Chess/ChessSessionManager.lua
+++ vrp/server/classes/Gameplay/Minigames/Chess/ChessSessionManager.lua
@@ -12,23 +12,23 @@ function ChessSessionManager:destructor()
 end
 
 function ChessSessionManager:Event_newGame( player1, player2)
-	if not self:getPlayerGame( player1 ) and not self:getPlayerGame( player2 ) then 
+	if not self:getPlayerGame( player1 ) and not self:getPlayerGame( player2 ) then
 		self.m_Map[#self.m_Map+1] = ChessSession:new(#self.m_Map+1, {player1,player2}, true, 6*60)
 	end
 end
 
 function ChessSessionManager:removeReference( obj )
-	for i = 1, #self.m_Map do 
-		if obj == self.m_Map[i] then 
+	for i = 1, #self.m_Map do
+		if obj == self.m_Map[i] then
 			table.remove(self.m_Map, i)
 		end
 	end
 end
 function ChessSessionManager:Event_GetChessMove( toIndex, fromIndex, team)
-	if client then 
-		if client == source then 
+	if client then
+		if client == source then
 			local gObject = self:getPlayerGame( client )
-			if gObject then 
+			if gObject then
 				gObject:movePlayerPiece( client, toIndex, fromIndex, team)
 			end
 		end
@@ -37,13 +37,13 @@ end
 
 function ChessSessionManager:Event_GetSurrender()
 	if client then
-		if client == source then 
+		if client == source then
 			local gObject = self:getPlayerGame( client )
-			if gObject then 
-				local winner 
-				if gObject.m_Players[1] == client then 
+			if gObject then
+				local winner
+				if gObject.m_Players[1] == client then
 					winner = gObject.m_Players[2]
-				else 
+				else
 					winner = gObject.m_Players[1]
 				end
 				gObject:endGame( winner, "Kapitulation!" )
@@ -53,25 +53,17 @@ function ChessSessionManager:Event_GetSurrender()
 end
 
 --// G/S
-function ChessSessionManager:getPlayerGame( player ) 
+function ChessSessionManager:getPlayerGame( player )
 	local gObject
-	for index = 1,#self.m_Map do 
+	for index = 1,#self.m_Map do
 		gObject = self.m_Map[index]
-		if gObject then 
-			if gObject:isThisPlayer( player ) then 
-				return gObject 
+		if gObject then
+			if gObject:isThisPlayer( player ) then
+				return gObject
 			end
-		else 
+		else
 			self.m_Map[index] = nil
 		end
 	end
 	return false
 end
-
-addCommandHandler("chess",function( source , cmd, player) 	
-	if player then 
-		if getPlayerFromName(player) then
-			ChessSessionManager:getSingleton():Event_newGame( source, getPlayerFromName(player) or source)
-		end
-	end
-end)
