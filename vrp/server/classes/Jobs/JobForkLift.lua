5ab6052ab52ae07a55303f40d3a6f82cb01dc126
Stumpy <danieltaucher@gmail.com>
2017-01-02 22:34:54 +0100

    Updated Forklift Spawn Position

--- vrp/server/classes/Jobs/JobForkLift.lua
+++ vrp/server/classes/Jobs/JobForkLift.lua
@@ -9,7 +9,7 @@ JobForkLift = inherit(Job)
 
 function JobForkLift:constructor()
 	Job.constructor(self)
-	self.m_VehicleSpawner = VehicleSpawner:new(87.50, -221.35, 0.9, {"Forklift"}, 90, bind(Job.requireVehicle, self))
+	self.m_VehicleSpawner = VehicleSpawner:new(88.162, -250.316, 0.85, {"Forklift"}, 90, bind(Job.requireVehicle, self))
 	self.m_VehicleSpawner.m_Hook:register(bind(self.onVehicleSpawn,self))
 	self.m_VehicleSpawner:disable()
 
