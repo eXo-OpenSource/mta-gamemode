e6eb4a2e359f389e95aa8f32526391ab460d8889
Stumpy <danieltaucher@gmail.com>
2017-01-04 02:42:26 +0100

    Removed BankRobbery Debug Message

--- vrp/server/classes/Faction/Actions/BankRobbery.lua
+++ vrp/server/classes/Faction/Actions/BankRobbery.lua
@@ -701,7 +701,7 @@ function BankRobbery:Event_onDestinationMarkerHit(hitElement, matchingDimension)
 								end
 							end
 						end
-						outputChatBox(_("Es wurden %d$ in die Kasse gelegt!", hitElement, totalAmount), hitElement, 255, 255, 255)
+						--outputChatBox(_("Es wurden %d$ in die Kasse gelegt!", hitElement, totalAmount), hitElement, 255, 255, 255)
 						if self:getRemainingBagAmount() == 0 then
 							PlayerManager:getSingleton():breakingNews("Der Bankraub wurde erfolgreich abgeschlossen! Die Täter sind mit der Beute entkommen!")
 							self.m_RobFaction:giveKarmaToOnlineMembers(-10, "Banküberfall erfolgreich!")
