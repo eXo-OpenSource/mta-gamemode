1c2eaee70b7e2aa123a75ef9caf058dc6dcbdcd5
StiviiK <stefan.kuerzeder@gmail.com>
2017-01-04 18:53:51 +0100

    Disabled old DownloadGUI

--- vrp/client/classes/Core.lua
+++ vrp/client/classes/Core.lua
@@ -29,11 +29,13 @@ function Core:constructor()
 			end
 		)()
 	else
-		DownloadGUI:new()
-		local dgi = DownloadGUI:getSingleton()
-		Provider:getSingleton():requestFile("vrp.data", bind(DownloadGUI.onComplete, dgi), bind(DownloadGUI.onProgress, dgi))
-		setAmbientSoundEnabled( "gunfire", false )
-		showChat(true)
+		--[[
+			DownloadGUI:new()
+			local dgi = DownloadGUI:getSingleton()
+			Provider:getSingleton():requestFile("vrp.data", bind(DownloadGUI.onComplete, dgi), bind(DownloadGUI.onProgress, dgi))
+			setAmbientSoundEnabled( "gunfire", false )
+			showChat(true)
+		--]]
 	end
 end
 
