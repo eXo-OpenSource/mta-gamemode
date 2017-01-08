b85eef191e6c0dfe15bfb2a6370a6933518d6241
Stumpy <danieltaucher@gmail.com>
2017-01-08 01:04:37 +0100

    Addendum to last commit

--- vrp/client/constants.lua
+++ vrp/client/constants.lua
@@ -335,7 +335,7 @@ HelpTexts = {
 			Als Holzfäller musst du die Bäume am Hügel (gelb markiert auf der Karte)
 			fällen und sie zum Sägewerk fahren.
 			Sofern du genug Bäume gefällt hast schnappst du dir einen Flatbed und fährst in den blauen Marker, neben welchem sich ein Haufen von Bäumen angesammelt hat (deine gefällten Bäume).
-			Nun werden diese Bäume aufgeladen. Nachdem sie aufgeladen wurden musst du zum Sägewerk fahren (die rote Säge auf der Karte). Dort lieferst du sie ab.
+			Nun werden diese Bäume aufgeladen. Nachdem sie aufgeladen wurden musst du zum Sägewerk fahren (die rote Säge oder Punkt auf der Karte). Dort lieferst du sie ab.
 		]];
 		Mechanic = [[
 			Als Mechaniker musst du die Autos deiner Mitbürger reparieren. Du reparierst sie, indem du auf das gewünschte Auto klickst und "reparieren" wählst.
