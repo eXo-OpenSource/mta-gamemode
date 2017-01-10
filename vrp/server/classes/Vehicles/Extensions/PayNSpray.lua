6a1c65674ffdf4321e5412adddca87abd7b73a43
StiviiK <stefan.kuerzeder@gmail.com>
2017-01-10 10:03:57 +0100

    Improved Vehicle folder structure

---
+++ vrp/server/classes/Vehicles/Extensions/PayNSpray.lua
@@ -0,0 +1,116 @@
+PayNSpray = inherit(Object)
+
+function PayNSpray:constructor(x, y, z, garageId)
+	self.m_FixShape = createColSphere(x, y, z, 4)
+	self.m_Blip = Blip:new("PayNSpray.png", x, y, root, 600)
+	if garageId then
+		setGarageOpen(garageId, true)
+	elseif isElement(self.m_CustomDoor) then
+		self:setCustomGarageOpen(true)
+	end
+
+	self.m_Company = CompanyManager:getSingleton():getFromId(2) -- Mechanic and Tow Id
+
+	addEventHandler("onColShapeHit", self.m_FixShape,
+		function(hitElement, matchingDimension)
+			if getElementType(hitElement) == "player" and matchingDimension then
+				local vehicle = getPedOccupiedVehicle(hitElement)
+				if not vehicle or getPedOccupiedVehicleSeat(hitElement) ~= 0 then
+					return
+				end
+
+				if getElementHealth(vehicle) > 950 then
+					hitElement:sendError(_("Dein Fahrzeug hat keinen erheblichen Schaden!", hitElement))
+					return
+				end
+
+				local costs = math.floor((1000-getElementHealth(vehicle))*0.5) + math.floor((1000-getElementHealth(vehicle))*0.5*0.33)
+				if hitElement:getMoney() < costs then
+					hitElement:sendError(_("Du hast nicht genügend Geld!", hitElement))
+					return
+				end
+
+				if garageId then
+					setGarageOpen(garageId, false)
+				elseif isElement(self.m_CustomDoor) then
+					self:setCustomGarageOpen(false)
+				end
+				setElementFrozen(vehicle, true)
+
+				-- Give money to the Owner (TODO: Improve this -> complete Repair ~4.58$ (310% Vehicle Health) -> is it okay?)
+				self.m_Company.m_BankAccount:addMoney(math.floor(costs*0.5))
+
+				setTimer(
+					function()
+						vehicle:fix()
+						vehicle:setWheelStates(0, 0, 0, 0)
+						setElementFrozen(vehicle, false)
+						if garageId then
+							setGarageOpen(garageId, true)
+						elseif isElement(self.m_CustomDoor) then
+							self:setCustomGarageOpen(true)
+						end
+
+						hitElement:takeMoney(costs, "Pay'N'Spray")
+						hitElement:sendShortMessage(_("Die Reperatur kostete %d$", hitElement, costs))
+					end,
+					3000,
+					1
+				)
+			end
+		end
+	)
+end
+
+function PayNSpray:createCustomDoor(model, pos, rot)
+	self.m_CustomDoor = createObject(model, pos, rot)
+	self.m_CustomDoor.open = true
+	self.m_CustomDoor.moving = false
+end
+
+function PayNSpray:setCustomGarageOpen(state)
+	if not self.m_CustomDoor.moving then
+		local pos = self.m_CustomDoor:getPosition()
+		if state == true then
+			if not self.m_CustomDoor.open then
+				pos.z = pos.z+1.7
+				self.m_CustomDoor:move(2000, pos, Vector3(0, 90, 0))
+				self.m_CustomDoor.moving = true
+				setTimer(function()
+					self.m_CustomDoor.open = true
+					self.m_CustomDoor.moving = false
+				end, 2500, 1)
+			end
+		else
+			if self.m_CustomDoor.open then
+				pos.z = pos.z-1.7
+				self.m_CustomDoor:move(2000, pos, Vector3(0, -90, 0))
+				self.m_CustomDoor.moving = true
+				setTimer(function()
+					self.m_CustomDoor.open = false
+					self.m_CustomDoor.moving = false
+				end, 2500, 1)
+			end
+		end
+	end
+end
+
+function PayNSpray:destructor()
+	destroyElement(self.m_FixShape)
+	delete(self.m_Blip)
+end
+
+function PayNSpray.initializeAll()
+	-- Todo
+	PayNSpray:new(2063.2, -1831.3, 13.5, 8) -- LS Idlewood
+	PayNSpray:new(487.4, -1742.8, 11.1, 12) -- LS Santa Maria Beach
+	PayNSpray:new(1025.1, -1022, 32.1, 11) -- LS Temple
+	PayNSpray:new(1976.60, 2162.41, 9.57, 36) -- LV City
+	PayNSpray:new(-99.77, 1118.37, 18.29, 41) -- Fort Carson
+	PayNSpray:new(-1904.47, 289.47, 40, 19) -- SF Wang Cars{ x = -1904.900, y = 286.494, z = 40.456 }
+	PayNSpray:new(-2425.84, 1020.08, 50.4, 27) -- SF Juniper Hill
+	PayNSpray:new(720.26, -455.14, 16.34, 47) -- Dillimore
+
+	local noobSpawn = PayNSpray:new(1444.860, -1790.127, 13.250)
+	noobSpawn:createCustomDoor(13028, Vector3(1445.5998,-1781.0005,16.1), Vector3(180,-90,90))
+end
---
+++ vrp/server/classes/Vehicles/Super/PermanentVehicle.lua
@@ -0,0 +1,271 @@
+-- ****************************************************************************
+-- *
+-- * PROJECT: vRoleplay
+-- * FILE: server/classes/PermanentVehicle.lua
+-- * PURPOSE: Vehicle class
+-- *
+-- ****************************************************************************
+PermanentVehicle = inherit(Vehicle)
+
+function PermanentVehicle:constructor(Id, owner, keys, color, color2, health, positionType, tunings, mileage, fuel, lightColor, trunkId, texture, horn, neon, special)
+	self.m_Id = Id
+	self.m_Owner = owner
+
+	self:setCurrentPositionAsSpawn(positionType)
+
+	setElementData(self, "OwnerName", Account.getNameFromId(owner) or "None") -- Todo: *hide*
+	self.m_Keys = keys or {}
+	self.m_PositionType = positionType or VehiclePositionType.World
+
+	if trunkId == 0 or trunkId == nil then
+		trunkId = Trunk.create()
+	end
+
+	if self.m_PositionType ~= VehiclePositionType.World then
+		-- Move to unused dimension | Todo: That's probably a bad solution
+		setElementDimension(self, PRIVATE_DIMENSION_SERVER)
+	end
+
+	self.m_Trunk = Trunk.load(trunkId)
+	self.m_TrunkId = trunkId
+
+	if health and health <= 300 then
+		health = 300
+  	end
+
+	self:setHealth(health or 1000)
+	self:setFuel(fuel or 100)
+	self:setLocked(true)
+	self:setMileage(mileage)
+	self:tuneVehicle(color, color2, tunings, texture, horn, neon, special)
+
+end
+
+function PermanentVehicle:destructor()
+
+end
+
+function PermanentVehicle.create(owner, model, posX, posY, posZ, rotation)
+  rotation = tonumber(rotation) or 0
+  if type(owner) == "userdata" then
+    owner = owner:getId()
+  end
+  if sql:queryExec("INSERT INTO ??_vehicles (Owner, Model, PosX, PosY, PosZ, Rotation, Health, Color) VALUES(?, ?, ?, ?, ?, ?, 1000, 0)", sql:getPrefix(), owner, model, posX, posY, posZ, rotation) then
+    local vehicle = createVehicle(model, posX, posY, posZ, 0, 0, rotation)
+    enew(vehicle, PermanentVehicle, sql:lastInsertId(), owner, nil, nil, 1000)
+    VehicleManager:getSingleton():addRef(vehicle)
+    return vehicle
+  end
+  return false
+end
+
+function PermanentVehicle:purge()
+  if sql:queryExec("DELETE FROM ??_vehicles WHERE Id = ?", sql:getPrefix(), self.m_Id) then
+    VehicleManager:getSingleton():removeRef(self)
+    destroyElement(self)
+    return true
+  end
+  return false
+end
+
+function PermanentVehicle:save()
+  local health = getElementHealth(self)
+  local r, g, b, r2, g2, b2 = getVehicleColor(self, true)
+  local color = setBytesInInt32(255, r, g, b) -- Format: argb
+  local color2 = setBytesInInt32(255, r2, g2, b2) -- Format: argb
+  local rLight, gLight, bLight = getVehicleHeadLightColor(self)
+  local lightColor = setBytesInInt32(255, rLight, gLight, bLight)
+  local tunings = getVehicleUpgrades(self) or {}
+  if self.m_Trunk then self.m_Trunk:save() end
+  return sql:queryExec("UPDATE ??_vehicles SET Owner = ?, PosX = ?, PosY = ?, PosZ = ?, Rotation = ?, Health = ?, Color = ?, Color2 = ?, `Keys` = ?, PositionType = ?, Tunings = ?, Mileage = ?, Fuel = ?, LightColor = ?, TrunkId = ?, TexturePath = ?, Horn = ?, Neon = ?, Special = ? WHERE Id = ?", sql:getPrefix(),
+    self.m_Owner, self.m_SpawnPos.x, self.m_SpawnPos.y, self.m_SpawnPos.z, self.m_SpawnRot, health, color, color2, toJSON(self.m_Keys), self.m_PositionType, toJSON(tunings), self:getMileage(), self:getFuel(), lightColor, self.m_TrunkId, self.m_Texture, self.m_CustomHorn, toJSON(self.m_Neon) or 0, self.m_Special or 0, self.m_Id)
+end
+
+function PermanentVehicle:getId()
+  return self.m_Id
+end
+
+function PermanentVehicle:setSpecial(special)
+  self.m_Special = special
+  self:setData("Special", special, true)
+  if special == VehicleSpecial.Soundvan then
+    if self:getModel() == 535 then
+      self.speakers = {}
+      self.speakers["Left"] = createObject(2229, 0, 0, 0)
+      self.speakers["Right"] = createObject(2229, 0, 0, 0)
+      self.speakers["Middle"] = createObject(1841, 0, 0, 0)
+      self.speakers["Middle"]:setScale(1.5)
+
+      self.speakers["Left"]:attach(self, -0.3, -1.5, 0, -55, 0, 0)
+      self.speakers["Right"]:attach(self, 1, -1.5, 0, -55, 0, 0)
+      self.speakers["Middle"]:attach(self, 0, -0.8, 0.4, 0, 0, 90)
+
+      for index, element in pairs(self.speakers) do
+        element:setCollisionsEnabled(false)
+      end
+
+      local refreshSpeaker = function()
+        for index, element in pairs(self.speakers) do
+          if isElement(self) then
+            element:setDimension(self:getDimension())
+            element:setInterior(self:getInterior())
+            if self.m_SoundURL then
+              triggerClientEvent("soundvanChangeURLClient", source, self.m_SoundURL)
+            end
+          else
+            element:destroy()
+            if self.m_SoundURL then
+              triggerClientEvent("soundvanStopSoundClient", self, url)
+            end
+          end
+        end
+      end
+
+      refreshSpeaker()
+
+      addEventHandler("onElementDimensionChange", self, refreshSpeaker)
+      addEventHandler("onElementInteriorChange", self, refreshSpeaker)
+      addEventHandler("onVehicleExplode", self, refreshSpeaker)
+      addEventHandler("onVehicleRespawn", self, refreshSpeaker)
+      addEventHandler("onElementDestroy", self, refreshSpeaker)
+    end
+  end
+end
+
+
+
+function PermanentVehicle:getTrunk()
+  if self.m_Trunk then return self.m_Trunk end
+  return false
+end
+
+function PermanentVehicle:isPermanent()
+  return true
+end
+
+function PermanentVehicle:getKeyNameList()
+  local names = {}
+  for k, v in ipairs(self.m_Keys) do
+    local name = Account.getNameFromId(v)
+    if name then
+      names[v] = name
+    end
+  end
+  return names
+end
+
+function PermanentVehicle:addKey(player)
+  if type(player) == "userdata" then
+    player = player:getId()
+  end
+  table.insert(self.m_Keys, player)
+end
+
+function PermanentVehicle:removeKey(player)
+  if type(player) == "userdata" then
+    player = player:getId()
+  end
+  local index = table.find(self.m_Keys, player)
+  if not index then
+    return false
+  end
+  table.remove(self.m_Keys, index)
+  return true
+end
+
+function PermanentVehicle:isInGarage()
+  return self.m_PositionType == VehiclePositionType.Garage
+end
+
+function PermanentVehicle:setInGarage(state)
+  self.m_PositionType = VehiclePositionType.Garage
+end
+
+function PermanentVehicle:isInHangar()
+  return self.m_PositionType == VehiclePositionType.Hangar
+end
+
+function PermanentVehicle:setInHangar(state)
+  self.m_PositionType = VehiclePositionType.Hangar
+end
+
+function PermanentVehicle:getPositionType()
+  return self.m_PositionType
+end
+
+function PermanentVehicle:setPositionType(type)
+  self.m_PositionType = type
+end
+
+function PermanentVehicle:respawn(garageOnly)
+  -- Set inGarage flag and teleport to private dimension
+  self.m_LastUseTime = math.huge
+  local vehicleType = self:getVehicleType()
+
+  -- Add to active garage session if there is one
+  local owner = Player.getFromId(self.m_Owner)
+  if owner and isElement(owner) then
+    -- Is the vehicle allowed to spawn in the garage
+    if vehicleType ~= VehicleType.Plane and vehicleType ~= VehicleType.Helicopter and vehicleType ~= VehicleType.Boat then
+      -- Does the player have a garage
+      if owner:getGarageType() > 0 then
+        -- Is there a slot available?
+        local maxSlots = VehicleGarages:getSingleton():getMaxSlots(owner:getGarageType())
+        local playerVehicles = VehicleManager:getSingleton():getPlayerVehicles(owner)
+        local numVehiclesInGarage = 0
+        for k, v in pairs(playerVehicles) do
+          if v:isInGarage() then numVehiclesInGarage = numVehiclesInGarage + 1 end
+        end
+
+        if maxSlots > numVehiclesInGarage then
+			self:setInGarage(true)
+			self:setDimension(PRIVATE_DIMENSION_SERVER)
+			fixVehicle(self)
+			setVehicleOverrideLights(self, 1)
+			self:setEngineState(false)
+			self:setSirensOn(false)
+         	local garageSession = owner.m_GarageSession
+          	if garageSession then
+            	garageSession:addVehicle(self)
+          	end
+
+          	owner:sendShortMessage(_("Dein Fahrzeug (%s) wurde in deiner Garage respawnt", owner, self:getName()))
+          	return true
+        end
+      end
+    end
+  end
+
+  if garageOnly then
+	if owner then
+		owner:sendShortMessage(_("Du hast keinen Platz in deiner Garage!", owner))
+	end
+	return false
+
+  	else
+		if self:respawnOnSpawnPosition() then
+			return true
+		else
+		-- Respawn at mechanic base
+			if vehicleType ~= VehicleType.Boat and vehicleType ~= VehicleType.Plane and vehicleType ~= VehicleType.Helicopter then
+				CompanyManager:getSingleton():getFromId(2):respawnVehicle(self)
+				if owner and isElement(owner) then
+				owner:sendShortMessage(_("Dein Fahrzeug (%s) wurde in der Mechaniker-Base respawnt", owner, self:getName()))
+				end
+				return true
+			end
+
+			-- Respawn at Harbor
+			if vehicleType == VehicleType.Boat or (vehicleType == VehicleType.Plane and self:getModel() == 460) or (vehicleType == VehicleType.Helicopter and self:getModel() == 447) then
+				VehicleHarbor:getSingleton():respawnVehicle(self)
+				if owner and isElement(owner) then
+				owner:sendShortMessage(_("Dein Fahrzeug (%s) wurde im Industrie-Hafen (Logistik-Job) respawnt", owner, self:getName()))
+				end
+				return true
+			end
+		end
+	end
+	return false
+
+end
+
