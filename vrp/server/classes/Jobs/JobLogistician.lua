b2c6f7e6dd1c39414b431d59a36f84fb786c7c20
Stumpy <danieltaucher@gmail.com>
2017-01-10 02:26:07 +0100

    Fixed Logistican Job Crane Bug

--- vrp/server/classes/Jobs/JobLogistician.lua
+++ vrp/server/classes/Jobs/JobLogistician.lua
@@ -15,8 +15,8 @@ function JobLogistician:constructor()
 	self.m_Crane1 = Crane:new(2387.30, -2492.40, 19.6, 2387.30, -2625.60, 19.6)
 	self.m_Crane2 = Crane:new(-219.70, -269.30, 7.30, -219.70, -200.30, 7.30)
 
-	self.m_Marker1 = self:createCraneMarker(Crane1, Vector3(2386.92, -2494.24, 13), Vector3(2387.60, -2490.87, 14.1), 0)
-	self.m_Marker2 = self:createCraneMarker(Crane2, Vector3(-219.35, -268.77, 0.6), Vector3(-219.70, -270.80, 2), 180)
+	self.m_Marker1 = self:createCraneMarker(self.m_Crane1, Vector3(2386.92, -2494.24, 13), Vector3(2387.60, -2490.87, 14.1), 0)
+	self.m_Marker2 = self:createCraneMarker(self.m_Crane2, Vector3(-219.35, -268.77, 0.6), Vector3(-219.70, -270.80, 2), 180)
 
 	self.m_VehicleSpawner1 = VehicleSpawner:new(2405.45, -2445.40, 12.6, {"DFT-30"}, 230, bind(Job.requireVehicle, self))
 	self.m_VehicleSpawner1.m_Hook:register(bind(self.onVehicleSpawn,self))
