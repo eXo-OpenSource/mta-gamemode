56e0b19d4b242c808a11eba1c3f07635f85670f7
Stumpy <danieltaucher@gmail.com>
2017-01-19 00:30:01 +0100

    Probably Fixed TAB-Playingtime Issue

--- vrp/server/classes/AntiCheat.lua
+++ vrp/server/classes/AntiCheat.lua
@@ -11,11 +11,13 @@ function AntiCheat:constructor()
 	-- Disable clientside synced element data setting entirely (even though we don't store anything important via elementdatas)
 	addEventHandler("onElementDataChange", root,
 		function(name, oldValue)
+			if name == "playingTime" then return end
+
 			-- Serverside changes are okay
 			if not client then
 				return
 			end
-			
+
 			setElementData(source, name, oldValue)
 		end
 	)
@@ -24,6 +26,6 @@ end
 function AntiCheat:report(player, name, severity)
 	assert(type(player) == "userdata" and type(name) == "string" and type(severity) == "number", "Bad argument @ AntiCheat.report")
 	outputServerLog(("AntiCheat:report(%s, %s, %i)"):format(player:getName(), name, severity))
-	
+
 	sql:queryExec("INSERT INTO ??_cheatlog (UserId, Name, Severity) VALUES(?, ?, ?)", sql:getPrefix(), player:getId(), name, severity)
 end
