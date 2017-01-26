68653fa8aa993d236a0a076c6b3e567242d94319
Stumpy <danieltaucher@gmail.com>
2017-01-26 01:52:30 +0100

    Added Wasted Screen at Deathmatch

--- vrp/server/classes/Gameplay/Deathmatch/DeathmatchRoom.lua
+++ vrp/server/classes/Gameplay/Deathmatch/DeathmatchRoom.lua
@@ -115,7 +115,9 @@ end
 function DeathmatchRoom:respawnPlayer(player, dead, killer, weapon)
 	local pos = Randomizer:getRandomTableValue(self.m_MapData["spawns"])
 	if dead then
+		player:triggerEvent("deathmatchStartDeathScreen", killer or player)
 		if killer then
+
 			self:increaseKill(killer, weapon)
 			self:increaseDead(player, weapon)
 		end
