e971102bd83d7edf4701b19f382b49c81dbdcd56
gatno <danieltaucher@gmail.com>
2017-01-27 17:53:39 +0100

    Fixed VehicleEnter Warning

--- vrp/server/classes/Vehicles/Super/FactionVehicle.lua
+++ vrp/server/classes/Vehicles/Super/FactionVehicle.lua
@@ -123,7 +123,7 @@ function FactionVehicle:onStartEnter(player, seat)
 end
 
 function FactionVehicle:onEnter(player)
-	if player:getFaction() and player:getFaction() == source.m_Faction then
+	if player.getFaction and player:getFaction() and player:getFaction() == source.m_Faction then
 
 	end
 end
