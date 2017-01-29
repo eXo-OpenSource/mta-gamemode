09a37037bd2398ea464da7aa3235216f27e50e03
Stumpy <danieltaucher@gmail.com>
2017-01-29 22:54:31 +0100

    Fixed Rescue Vehicle Key Function

--- vrp/server/classes/Vehicles/Super/FactionVehicle.lua
+++ vrp/server/classes/Vehicles/Super/FactionVehicle.lua
@@ -161,7 +161,7 @@ end
 function FactionVehicle:hasKey(player)
   if self:isPermanent() then
     if self.m_Faction:isStateFaction() or self.m_Faction:isRescueFaction() then
-		if player:getFaction() and player:getFaction():isStateFaction() and player:isFactionDuty() then
+		if player:getFaction() and player:isFactionDuty() then
 			return true
 		end
 	elseif player:getFaction() == self.m_Faction then
