0a2a827670d389f9298e343429cb4711dd1e6992
gatno <danieltaucher@gmail.com>
2017-02-09 15:46:10 +0100

    Added new Kart-Map (8-Track)

--- vrp/server/classes/Gameplay/Race/Kart.lua
+++ vrp/server/classes/Gameplay/Race/Kart.lua
@@ -12,7 +12,8 @@ Kart.Maps = {
 	"files/maps/Kart/Kartbahn.map",
 	"files/maps/Kart/Kartbahn2.map",
 	"files/maps/Kart/Karthalle.map",
-	"files/maps/Kart/Funny Tubes.map",
+	"files/maps/Kart/8-Track.map",
+	--"files/maps/Kart/Funny Tubes.map",
 	--"files/maps/Kart/EliteKartMap.map",
 }
 
