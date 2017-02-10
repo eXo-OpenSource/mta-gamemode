5f8cb88bc5f545cbd80ccd8c9e0a70f474f22e3b
gatno <danieltaucher@gmail.com>
2017-02-10 16:42:03 +0100

    Added some new Help Texts

--- vrp/client/constants.lua
+++ vrp/client/constants.lua
@@ -212,6 +212,14 @@ HelpTextTitles = {
 		LoginRegister = "Login/Registration";
 		Team = "Team";
 	};
+	Freizeit = {
+		Kart = "Kartstrecke";
+		Angeln = "Angeln";
+		Boxhalle = "Boxhalle";
+		Bars = "Bars";
+		Pferdewetten = "Pferdewetten";
+		Minispiele = "Minispiele";
+	};
 	Jobs = {
 		BusDriver = "Job: Busfahrer";
 		Farmer = "Job: Bauer";
@@ -226,11 +234,6 @@ HelpTextTitles = {
 		HeliTransport = "Job: Helikopter-Pilot";
 		ForkLift = "Job: Gabelstapler-Fahrer";
 		TreasureSeeker = "Job: Schatz Sucher";
-	};
-	Events = {
-		Deathmatch = "Event: Deathmatch";
-		DMRace = "Event: DM-Race";
-		StreetRace = "Event: Street Race";
 	};
 	Gameplay = {
 		Cars = "Fahrzeuge";
@@ -249,6 +252,11 @@ HelpTextTitles = {
 		Bankrob = "Aktionen: Bank-Überfall";
 		StateWeaponTruck = "Aktionen: Staats Waffen Truck";
 
+	};
+	Events = {
+		Deathmatch = "Event: Deathmatch";
+		DMRace = "Event: DM-Race";
+		StreetRace = "Event: Street Race";
 	};
 	Credits = {
 		OldVRPTeam = "vRP-Team";
@@ -313,6 +321,53 @@ HelpTexts = {
 			Informationen zum damaligen vRP-Team und Unterstützer unter "vRP-Team".
 		]];
 
+	};
+	Freizeit = {
+		Kart = [[
+		Die Kartstrecke befindet sich in Montgomery (Reifen Blip auf der Karte). Du kannst dort auf täglich wechselnden
+		Strecken dein Fahrkönnen unter Beweis stellen.
+		Natürlich gibt es ein Highscore System für die schnellsten Runden.
+
+		In den nächsten Updates wird zudem ein Ghostmode hinzugefügt um rauszufinden wo du deine Zeit liegen lässt.
+		]];
+		Angeln = [[
+		Am Pier in Los Santos (am Riesenrad) gibt es mehrere Angeln um Meeresbewohner oder andere Gegenstände aus dem
+		Wasser zu angeln. Je besser du auswirfst und einholst, desto wertvoller deine Beute.
+
+		Verkaufe diese einfach an Angeler Lutz der direkt daneben seinen Stand aufgebaut hat!
+		]];
+		Boxhalle = [[
+		Im Fitness-Center Los Santos kannst du andere Spieler zum Boxkampf herausfordern! Dabei könnt ihr einfach aus
+		Spaß kämpfen oder aber einen Geldeinsatz vereinbaren.
+		Du findest das Fitness-Center nahe der Green Bottle bar!
+
+		Möge der stärkste Kämpfer gewinnen!
+		]];
+		Bars = [[
+		Bars können von privaten Firma (Können in der Stadthalle gegründet werden) gekauft werden.
+		Diese bieten den Angestellten der Firma viele Funktionen wie z.B die Verwaltung der Kasse
+		(in der die Einnahmen gehen),das Abspielen von Musik oder das Engagieren von
+		professionellen Tänzerinnen.
+
+		Der perfekten Party steht damit nichts im Wege!
+		]];
+		Pferdewetten = [[
+		Täglich um 20:00 Uhr findet das eXo-Pferderennen statt. Du kannst im Wettlokal (Pferd auf der Karte)
+		einen gewünschten Geldbetrag auf das Pferd deiner Wahl setzen.
+
+		Mit etwas Glück kannst du damit deinen Einsatz verdreifachen!
+		]];
+		Minispiele = [[
+		Auf eXo-Reallife gibt es zahlreiche Minispiele die du teilweise alleine, teilweise mit anderen Usern
+		spielen kannst.
+		Klicke einen Spieler an und wähle "Spielen" im Klickmenü, dort gibt es eine Auswahl
+		an Spielen zu denen du den Spieler herausfordern kannst.
+
+		Unter anderem mit von der Partie sind Schach, Pong und Schere, Stein, Papier
+
+		Weitere Spiele findest du im Casino. (Spielkarten Symbol auf der Karte)
+		Dort findest du z.B. 2 Cars, Go-Jump oder SideSwipe.
+		]];
 	};
 	Jobs = {
 		BusDriver = [[
