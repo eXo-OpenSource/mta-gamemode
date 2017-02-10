699f93bd6805c38f4f739ddb00cec552aa7350af
Stumpy <danieltaucher@gmail.com>
2017-02-11 00:46:54 +0100

    Fixed PayNSpray

--- vrp/server/classes/Vehicles/Extensions/PayNSpray.lua
+++ vrp/server/classes/Vehicles/Extensions/PayNSpray.lua
@@ -19,7 +19,7 @@ function PayNSpray:constructor(x, y, z, garageId)
 					return
 				end
 
-				if getElementHealth(vehicle) < 999 then
+				if vehicle:getHealt() >= 1000 then
 					hitElement:sendError(_("Dein Fahrzeug hat keinen erheblichen Schaden!", hitElement))
 					return
 				end
