b2d53b88913f0b1a69037d85463486030f18f4f9
Stumpy <danieltaucher@gmail.com>
2017-02-11 16:01:01 +0100

    Addendum to last commit

--- [tools]/camera_tool/server.lua
+++ [tools]/camera_tool/server.lua
@@ -1,7 +1,9 @@
 function camtool(player, cmd, pw)
 	if pw and pw == "eXoTrailerTeam" then
-		outputChatBox("Camera-Tool Access erlaubt!", player, 255, 0, 0)
+		outputChatBox("Camera-Tool / Freecam Access erlaubt!", player, 255, 0, 0)
 		triggerClientEvent(player, "onAllowPlayer", player)
+		local x, y, z = getElementPosition(player)
+		triggerClientEvent(player,"doSetFreecamEnabled", player, x, y, z)
 	else
 		outputChatBox("Falsches/Kein Passwort!", player, 255, 0, 0)
 	end
