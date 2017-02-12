9d601023b34b21dcbf8ad00d8feb7e605f25af98
Stumpy <danieltaucher@gmail.com>
2017-02-12 19:50:01 +0100

    Fixed Paynspray issue

--- vrp/server/classes/Vehicles/Extensions/PayNSpray.lua
+++ vrp/server/classes/Vehicles/Extensions/PayNSpray.lua
@@ -19,7 +19,7 @@ function PayNSpray:constructor(x, y, z, garageId)
 					return
 				end
 
-				if not vehicle:getHealth() < 1000 then
+				if vehicle:getHealth() > 999 then
 					hitElement:sendError(_("Dein Fahrzeug hat keinen erheblichen Schaden!", hitElement))
 					return
 				end
