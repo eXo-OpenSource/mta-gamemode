5dbef4afc6ee2f8ead9c78f6a24f731b2eeb9ad3
StiviK <stefan.kuerzeder@gmail.com>
2017-02-15 01:26:17 +0100

    Addendum to Doge EasterEgg/Achievement

--- vrp/client/classes/EasterEgg.lua
+++ vrp/client/classes/EasterEgg.lua
@@ -1,7 +1,23 @@
 EasterEgg = inherit(Singleton)
 
 function EasterEgg:constructor()
+	-- Doge EasterEgg
 	self.m_DogeQRFrame = createObject(2257, 1102.8000488281, -841.70001220703, 108.19999694824, 345, 0, 7.25);
 	setObjectScale(self.m_DogeQRFrame, 1.5);
 	TextureReplace:constructor("cj_painting15", "files/images/Other/such_qr_wow.png", false, 1320, 1320, self.m_DogeQRFrame)
+
+	-- EventHandler
+	addEventHandler("onClientClick", root, bind(self.onClick, self))
+end
+
+function EasterEgg:destructor()
+	self.m_DogeQRFrame:destroy()
+end
+
+function EasterEgg:onClick(button, state, absX, absY, worldX, worldY, worldZ, element)
+	if state then
+		if element == self.m_DogeQRFrame then
+			localPlayer:giveAchievement(77)
+		end
+	end
 end
