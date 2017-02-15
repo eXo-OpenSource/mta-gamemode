c0178d443fb31d9b7fce56c8a4a8c411d62742a5
HorrorClown <admin@pewx.de>
2017-02-15 21:29:44 +0100

    Updated slenderman

--- vrp/client/classes/EasterEggs/Slenderman.lua
+++ vrp/client/classes/EasterEggs/Slenderman.lua
@@ -3,8 +3,9 @@ EasterEgg.Slenderman = inherit(Object)
 
 function EasterEgg.Slenderman:constructor(position, rotation)
 	-- load models
-	__, self.m_TXD = CustomModelManager:getSingleton():loadImportTXD("files/models/scary.txd", 230)
+	local _, txd = CustomModelManager:getSingleton():loadImportTXD("files/models/scary.txd", 230)
     CustomModelManager:getSingleton():loadImportDFF("files/models/scary.dff", 230)
+	self.m_TXD = txd
 
 	-- create ped + sound
 	self.m_StartPosition = position
@@ -32,6 +33,7 @@ function EasterEgg.Slenderman:destructor()
 	self.m_TXD:destroy()
 	self.m_Ped:destroy()
 	CustomModelManager:getSingleton():restoreModel(230)
+	setTimer(fadeCamera, 1500, 1, true)
 end
 
 function EasterEgg.Slenderman:move()
@@ -39,8 +41,13 @@ function EasterEgg.Slenderman:move()
 	self.m_Ped:setPosition(interpolateBetween(self.m_StartPosition, localPlayer.position, progress, "InQuad"))
 	self.m_Ped:setRotation(Vector3(0, 0, findRotation(self.m_Ped.position.x, self.m_Ped.position.y, localPlayer.position.x, localPlayer.position.y)))
 
-	if progress >= 0.95 and not self.m_CameraFaded then
-		fadeCamera(false, 0.3)
-		self.m_CameraFaded = true
+	if not self.m_CameraFaded then
+		local offsetX, offsetY = math.random(1,15), math.random(1,15)
+		dxDrawImage(-offsetX, -offsetY, screenWidth+15, screenHeight+15, "files/images/Other/slender.jpg", 0, 0, 0, tocolor(255, 255, 255, 200*progress), true)
+
+		if progress >= 0.95 then
+			fadeCamera(false, 0.3)
+			self.m_CameraFaded = true
+		end
 	end
 end
