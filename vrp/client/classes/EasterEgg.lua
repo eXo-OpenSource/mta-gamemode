1e5d47f380d7d069c8ac32758e054ebb4c799a90
StiviK <stefan.kuerzeder@gmail.com>
2017-02-15 01:31:12 +0100

    Addendum to last commit

--- vrp/client/classes/EasterEgg.lua
+++ vrp/client/classes/EasterEgg.lua
@@ -3,6 +3,28 @@ EasterEgg = inherit(Singleton)
 function EasterEgg:constructor()
 	-- Doge EasterEgg
 	self.m_DogeQRFrame = createObject(2257, 1102.8000488281, -841.70001220703, 108.19999694824, 345, 0, 7.25);
+	self.m_DogeString  =
+	[[
+		░░░░░░░░░▄░░░░░░░░░░░░░░▄░░░░ wow
+		░░░░░░░░▌▒█░░░░░░░░░░░▄▀▒▌░░░
+		░░░░░░░░▌▒▒█░░░░░░░░▄▀▒▒▒▐░░░ such eXo (iLife :P)
+		░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐░░░
+		░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐░░░
+		░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌░░░
+		░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌░░ so MTA
+		░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐░░
+		░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌░
+		░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌░
+		▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐░ much driving
+		▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌
+		▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐░
+		░▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌░
+		░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐░░
+		░░▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌░░
+		░░░░▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀░░░ very impressive
+		░░░░░░▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀░░░░░
+		░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▀▀░░░░░░░░
+	]]
 	setObjectScale(self.m_DogeQRFrame, 1.5);
 	TextureReplace:constructor("cj_painting15", "files/images/Other/such_qr_wow.png", false, 1320, 1320, self.m_DogeQRFrame)
 
@@ -17,6 +39,7 @@ end
 function EasterEgg:onClick(button, state, absX, absY, worldX, worldY, worldZ, element)
 	if state then
 		if element == self.m_DogeQRFrame then
+			outputConsole(self.m_DogeString)
 			localPlayer:giveAchievement(77)
 		end
 	end
