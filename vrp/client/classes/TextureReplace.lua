44ad9c30c0de3472cfd8b9603947bec0a16ec0f7
StiviK <stefan.kuerzeder@gmail.com>
2017-02-17 18:35:31 +0100

    Added Texture streamer to TextureReplace

--- vrp/client/classes/TextureReplace.lua
+++ vrp/client/classes/TextureReplace.lua
@@ -2,35 +2,28 @@ TextureReplace = inherit(Object)
 TextureReplace.ServerElements = {}
 
 function TextureReplace:constructor(textureName, path, isRenderTarget, width, height, targetElement)
-	if not isRenderTarget then
-		self.m_Texture = dxCreateTexture(path)
-	else
-		self.m_Texture = dxCreateRenderTarget(width, height, true)
+	self.m_TextureName = textureName
+	self.m_TexturePath = path
+	self.m_IsRenderTarget = isRenderTarget
+	self.m_Width = width
+	self.m_Height = height
+	self.m_Element = targetElement
 
-		if path then
-			dxSetRenderTarget(texture)
-				dxDrawImage(0, 0, width, height, path)
-			dxSetRenderTarget(nil)
+	if not self.m_Element then
+		self:loadShader()
+	else
+		if isElementStreamedIn(self.m_Element) then
+			self:loadShader()
 		end
 	end
 
-	self.m_Shader = dxCreateShader("files/shader/texreplace.fx")
-	if not self.m_Shader then
-		outputDebugString("Loading the shader failed")
-		return
-	end
-
-	if not self.m_Texture then
-		outputDebugString("Loading the texture failed! "..textureName)
-		return
-	end
-
-	dxSetShaderValue(self.m_Shader, "gTexture", self.m_Texture)
-	if targetElement then
-		engineApplyShaderToWorldTexture(self.m_Shader, textureName, targetElement)
-		addEventHandler("onClientElementDestroy", targetElement, function () delete(self) end)
-	else
-		engineApplyShaderToWorldTexture(self.m_Shader, textureName)
+	if self.m_Element then
+		self.m_OnElementDestory = bind(delete, self)
+		self.m_OnElementStreamOut = bind(self.onElementStreamOut, self)
+		self.m_OnElementStreamIn = bind(self.onElementStreamIn, self)
+		addEventHandler("onClientElementDestroy", self.m_Element, self.m_OnElementDestory)
+		addEventHandler("onClientElementStreamOut", self.m_Element, self.m_OnElementStreamOut)
+		addEventHandler("onClientElementStreamIn", self.m_Element, self.m_OnElementStreamIn)
 	end
 end
 
@@ -39,12 +32,71 @@ function TextureReplace:destructor()
 	destroyElement(self.m_Shader)
 end
 
+function TextureReplace:onElementStreamIn()
+	outputConsole(("Element %s streamed in, creating texture..."):format(tostring(self.m_Element)))
+	if not self:loadShader() then
+		outputConsole(("Loading the texture of element %s failed!"):format(tostring(self.m_Element)))
+	end
+end
+
+function TextureReplace:onElementStreamOut()
+	outputConsole(("Element %s streamed out, destroying texture..."):format(tostring(self.m_Element)))
+	if not self:unloadShader() then
+		outputConsole(("Unloading the texture of element %s failed!"):format(tostring(self.m_Element)))
+	end
+end
+
 function TextureReplace:getTexture()
 	return self.m_Texture
 end
 
 function TextureReplace:getShader()
 	return self.m_Shader
+end
+
+function TextureReplace:loadShader()
+	if self.m_Shader and isElement(self.m_Shader) then return false end
+	if self.m_Texture and isElement(self.m_Shader) then return false end
+
+	if not self.m_IsRenderTarget then
+		self.m_Texture = dxCreateTexture(self.m_TexturePath)
+	else
+		self.m_Texture = dxCreateRenderTarget(self.m_Width, self.m_Height, true)
+
+		if self.m_TexturePath then
+			dxSetRenderTarget(self.m_Texture)
+				dxDrawImage(0, 0, width, height, path)
+			dxSetRenderTarget(nil)
+		end
+	end
+
+	self.m_Shader = dxCreateShader("files/shader/texreplace.fx")
+	if not self.m_Shader then
+		outputDebugString("Loading the shader failed")
+		return false
+	end
+
+	if not self.m_Texture then
+		outputDebugString("Loading the texture failed! "..textureName)
+		return false
+	end
+
+	dxSetShaderValue(self.m_Shader, "gTexture", self.m_Texture)
+	if self.m_Element then
+		return engineApplyShaderToWorldTexture(self.m_Shader, self.m_TextureName, self.m_Element)
+	else
+		return engineApplyShaderToWorldTexture(self.m_Shader, self.m_TextureName)
+	end
+end
+
+function TextureReplace:unloadShader()
+	if not self.m_Shader or not isElement(self.m_Shader) then return false end
+	if not self.m_Texture or not isElement(self.m_Texture) then return false end
+
+	local a = destroyElement(self.m_Texture)
+	local b = destroyElement(self.m_Shader)
+
+	return a and b
 end
 
 -- Events
