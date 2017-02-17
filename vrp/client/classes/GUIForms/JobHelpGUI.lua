9d1d96b283d9abffe78e49225bbb26604e50e4c9
gatno <danieltaucher@gmail.com>
2017-02-17 09:57:37 +0100

    Fixed JobHelpGUI

--- vrp/client/classes/GUIForms/JobHelpGUI.lua
+++ vrp/client/classes/GUIForms/JobHelpGUI.lua
@@ -7,9 +7,9 @@
 -- ****************************************************************************
 JobHelpGUI = inherit(GUIForm)
 inherit(Singleton, JobHelpGUI)
-local jobNames = 
+local jobNames =
 {
-	{"Trashman",1
+	{"Trashman",1},
 	{"Roadsweeper",0},
 	{"Lumberjack",3},
 	{"Farmer",4},
@@ -23,14 +23,14 @@ local jobNames =
 function JobHelpGUI:constructor()
 	GUIForm.constructor(self, screenWidth/2-300, screenHeight/2-230, 600, 460)
 
-	self.m_Window = GUIWindow:new(0, 0, self.m_Width, self.m_Height, "JobGUI", true, true, self)
+	self.m_Window = GUIWindow:new(0, 0, self.m_Width, self.m_Height, "Job Info", true, true, self)
 	self.m_InfoLabel = GUILabel:new(30, 180, 540, 200, "Hier eine Liste von Jobs!", self)
 	self.m_InfoLabel:setFont(VRPFont(24))
-	
+
 	self.m_JobList = GUIGridList:new(5, 35, self.m_Width-10, self.m_Height-60, self)
 	self.m_JobList:addColumn(_"Jobs", 1)
 	local pos
-	for index, job in pairs(JobManager:getSingleton().m_Jobs) do 
+	for index, job in pairs(JobManager:getSingleton().m_Jobs) do
 		pos = job.m_Ped:getPosition()
 		item = self.m_JobList:addItem(jobNames[index][1].." - Level "..jobNames[index][2])
 		item.onLeftDoubleClick = function () self:showJob( pos, job.m_Ped ) end
@@ -43,7 +43,7 @@ end
 
 
 function JobHelpGUI:showJob( pos, ped )
-	if self.m_JobBlip then 
+	if self.m_JobBlip then
 		delete(self.m_JobBlip)
 	end
 	self.m_JobBlip = Blip:new("Marker.png", pos.x, pos.y,9999)
