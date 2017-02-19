1e7582515b398cc4a21da987c75a42eb0f43b505
Jusonex <jusonex96@gmail.com>
2017-02-19 14:52:09 +0100

    Improve GUIGridlist:clear performance significantly Also increase scoreboard refresh rate

--- vrp/client/classes/GUI/GUIGridList.lua
+++ vrp/client/classes/GUI/GUIGridList.lua
@@ -102,8 +102,8 @@ end
 
 function GUIGridList:clear()
 	self.m_SelectedItem = nil
-	delete(self.m_ScrollArea)
-	self.m_ScrollArea = GUIScrollableArea:new(0, self.m_ItemHeight, self.m_Width, self.m_Height-self.m_ItemHeight, self.m_Width, 1, true, false, self, self.m_ItemHeight)
+
+	self.m_ScrollArea:clearChildren()
 end
 
 function GUIGridList:onInternalSelectItem(item)
