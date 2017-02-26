573d4a3c735506028c9838c2c39f551d1d7406db
StiviK <stefan.kuerzeder@gmail.com>
2017-02-26 17:13:17 +0100

    Added Donut Model

--- vrp/client/classes/CustomModelManager.lua
+++ vrp/client/classes/CustomModelManager.lua
@@ -75,6 +75,10 @@ function CustomModelManager:constructor()
 	self:loadImportCOL("files/models/bed.col", 1879)
 	self:loadImportDFF("files/models/bed.dff", 1879)
 
+	self:loadImportTXD("files/models/donut.txd", 1915)
+	self:loadImportCOL("files/models/donut.col", 1915)
+	self:loadImportDFF("files/models/donut.dff", 1915)
+
 	-- Tardis EasterEgg
 	--self:loadImportTXD("files/models/tardis.txd", 1337)
 	--self:loadImportDFF("files/models/tardis.dff", 1337)
