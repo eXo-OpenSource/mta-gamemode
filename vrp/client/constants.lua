328deb1fd0a96318918d162138d54fd6cfcdb029
gatno <danieltaucher@gmail.com>
2017-03-01 18:08:39 +0100

    Updated Gravel Job Description

--- vrp/client/constants.lua
+++ vrp/client/constants.lua
@@ -452,14 +452,13 @@ HelpTexts = {
 			Drücke 'Leertaste' um den "Schatz" aufzunehmen.
 		]];
 		Gravel = [[
-			Dieser Job ist in 3 Abreitsschritte aufgeteilt die auch verschiedene Spieler zugleich ausführen können.
-			1.) Abbau mit Spitzhacke
-			Baue die hellen Felsen direkt neben dem Dozer-Spawn mit der Spitzhacke ab. (Klicken zum abbauen)
-			2.) Einlagern
-			Lagere die abgebauten Gesteinsbrocken ein indem du diese mit dem Dozer in die Auffangbehälter schiebst.
-			3.) Transport
-			Transportiere die eingelagerten Gesteinsbrocken mit dem Dumper aus der Kiesgrube. Zum Beladen fahre einfach
-			unter ein Förderband.
+			Der Job ist in 3 Arbeitsschritte aufgeteilt die verschiedene Spieler zu gleich ausführen können.
+
+			1.) Abbau mit Spitzhacke: Baue die hellen Felsen direkt neben dem Dozer-Spawn mit der Spitzhacke ab. (Klicken zum Abbauen)
+
+			2.) Einlagern: Lagere die abgebauten Gesteinsbrocken ein indem du diese mit dem Dozer in die Behälter schiebst.
+
+			3.) Transport: Transportiere die eingelagerten Gesteinsbrocken mit dem Dumper aus der Kiesgrube. Zum Beladen fahre einfach unter ein Förderband.
 		]];
 
 	};
