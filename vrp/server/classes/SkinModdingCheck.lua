8ee3a34430b57e03b92cbf2c53b922542764ddee
Strobe <devcxo@gmail.com>
2017-03-03 15:38:56 +0100

    Adjusted tolerance of SkinModding

--- vrp/server/classes/SkinModdingCheck.lua
+++ vrp/server/classes/SkinModdingCheck.lua
@@ -1,7 +1,7 @@
 SkinModdingCheck = inherit( Singleton )
 SkinModdingCheck.MAX_DIFFER_X = 0.2
 SkinModdingCheck.MAX_DIFFER_Y = 0.2
-SkinModdingCheck.MAX_DIFFER_Z = 0.1
+SkinModdingCheck.MAX_DIFFER_Z = 0.4
 
 function SkinModdingCheck:constructor()
 	addEventHandler ( "onPlayerModInfo", getRootElement(), bind(self.handleOnPlayerModInfo, self))
