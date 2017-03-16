2051a7557a708ea53e0b94810c6c7d666901085c
Stumpy <danieltaucher@gmail.com>
2017-03-16 19:48:07 +0100

    Addendum to last commit - Fixed Typo

--- vrp/server/classes/ModdingCheck.lua
+++ vrp/server/classes/ModdingCheck.lua
@@ -26,27 +26,27 @@ function ModdingCheck:handleOnPlayerModInfo ( filename, modList )
     for idx,item in ipairs(modList) do
 		if item.id >= 0 and item.id <= 310 then -- Skins
 			if item.sizeX then
-				differenceX =  math.abs(item.originalSizeX - item.sizeX)
-				differenceY = math.abs( item.originalSizeY - item.sizeY)
-				differenceZ = math.abs( item.originalSizeZ - item.sizeZ)
+				differenceX = math.abs(item.originalSizeX - item.sizeX)
+				differenceY = math.abs(item.originalSizeY - item.sizeY)
+				differenceZ = math.abs(item.originalSizeZ - item.sizeZ)
 				if differenceX >= ModdingCheck.SKIN_MAX_DIFFER_X or differenceY >= ModdingCheck.SKIN_MAX_DIFFER_Y or differenceZ >= ModdingCheck.SKIN_MAX_DIFFER_Z then
 					tNames[#tNames+1] = item.id.." - "..item.name
 				end
 			end
         elseif item.id >= 400 and item.id <= 611 then -- Vehicles
 			if item.sizeX then
-				differenceX =  math.abs(item.originalSizeX - item.sizeX)
-				differenceY = math.abs( item.originalSizeY - item.sizeY)
-				differenceZ = math.abs( item.originalSizeZ - item.sizeZ)
+				differenceX = math.abs(item.originalSizeX - item.sizeX)
+				differenceY = math.abs(item.originalSizeY - item.sizeY)
+				differenceZ = math.abs(item.originalSizeZ - item.sizeZ)
 				if differenceX >= ModdingCheck.VEH_MAX_DIFFER_X or differenceY >= ModdingCheck.VEH_MAX_DIFFER_Y or differenceZ >= ModdingCheck.VEH_MAX_DIFFER_Z then
 					tNames[#tNames+1] = item.id.." - "..item.name
 				end
 			end
 		else
 			if item.sizeX then
-				differenceX =  math.abs(item.originalSizeX - item.sizeX)
-				differenceY = math.abs( item.originalSizeY - item.sizeY)
-				differenceZ = math.abs( item.originalSizeZ - item.sizeZ)
+				differenceX = math.abs(item.originalSizeX - item.sizeX)
+				differenceY = math.abs(item.originalSizeY - item.sizeY)
+				differenceZ = math.abs(item.originalSizeZ - item.sizeZ)
 				if differenceX >= ModdingCheck.OTHER_MAX_DIFFER_X or differenceY >= ModdingCheck.OTHER_MAX_DIFFER_Y or differenceZ >= ModdingCheck.OTHER_MAX_DIFFER_Z then
 					tNames[#tNames+1] = item.id.." - "..item.name
 				end
