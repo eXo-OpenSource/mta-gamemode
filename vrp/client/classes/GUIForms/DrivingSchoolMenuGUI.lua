3d3fb596594df0019d96f0264cf10a8561939924
HorrorClown <admin@pewx.de>
2017-03-20 21:30:26 +0100

    Fixed driving school gui

--- vrp/client/classes/GUIForms/DrivingSchoolMenuGUI.lua
+++ vrp/client/classes/GUIForms/DrivingSchoolMenuGUI.lua
@@ -16,20 +16,20 @@ function DrivingSchoolMenuGUI:constructor(count, instructors)
 	GUIForm.constructor(self, screenWidth/2-(300/2), screenHeight/2-(160/2), 300, 250)
 	self.m_Window = GUIWindow:new(0, 0, self.m_Width, self.m_Height, _"eXo Fahrschule", true, true, self):deleteOnClose(true)
 
-	GUILabel:new(10, 40, self.m_Width-20, 25, _("Es sind aktuell %d Fahrlehrer online!",self.m_InstructorCount), self):setAlignX("center")
+	GUILabel:new(10, 40, self.m_Width-20, 25, _("Es %s aktuell %d Fahrlehrer online!", self.m_InstructorCount > 0 and "sind" or "ist", self.m_InstructorCount), self):setAlignX("center")
 	self.m_CallInstructorButton = GUIButton:new(30, 70, self.m_Width-60, 35,_"Fahrlehrer rufen", self)
 	self.m_CallInstructorButton:setBackgroundColor(Color.Green):setFont(VRPFont(28)):setFontSize(1)
 	self.m_CallInstructorButton.onLeftClick = bind(self.callInstructor,self)
 
-	local instructorString = ""
+	local instructorsGrid = GUIGridList:new(10, 115, 280, 125, self.m_Window)
+	instructorsGrid:addColumn(_"Spieler", .6)
+	instructorsGrid:addColumn(_"Im Dienst", .4)
+
 	if self.m_InstructorCount > 0 then
-		instructorString = _"Diese Fahrlehrer sind online:\n"
 		for name, duty in pairs(instructors) do
-			instructorString = instructorString..name.." "..duty.."\n"
+			instructorsGrid:addItem(name, duty)
 		end
 	end
-
-	GUILabel:new(30, 115, self.m_Width-60, 20, instructorString, self):setMultiline(true)
 end
 
 addEventHandler("showDrivingSchoolMenu", root,
