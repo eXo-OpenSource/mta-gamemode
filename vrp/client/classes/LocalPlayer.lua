64daec80bb33c98bdffcdd2f0d48ddfc0fbdee01
Strobe <devcxo@gmail.com>
2017-03-27 12:05:08 +0200

    Addendum #4ac647b

--- vrp/client/classes/LocalPlayer.lua
+++ vrp/client/classes/LocalPlayer.lua
@@ -88,12 +88,14 @@ function LocalPlayer:checkWeaponAim()
 				localPlayer.m_HasScopedIn = true 
 				localPlayer.m_IsFading = true 
 				fadeCamera(false,0.5,0,0,0)
+				setCameraShakeLevel(240)
 				setTimer(self.m_FadeOut, 500,1)
 			end
 		else 
 			localPlayer.m_HasScopedIn = false
 			if localPlayer.m_IsFading then 
 				fadeCamera(true,0.5)
+				setCameraShakeLevel(0)
 				localPlayer.m_IsFading = false
 			end
 		end
@@ -101,6 +103,7 @@ function LocalPlayer:checkWeaponAim()
 		localPlayer.m_HasScopedIn = false
 		if localPlayer.m_IsFading then 
 			fadeCamera(true,0.5)
+			setTimer(setCameraShakeLevel, 1000, 1, 0)
 			localPlayer.m_IsFading = false
 		end
 	end
@@ -112,6 +115,7 @@ end
 function LocalPlayer:fadeOutScope() 
 	if localPlayer.m_IsFading then 
 		fadeCamera(true,1)
+		setTimer(setCameraShakeLevel, 1000, 1, 0)
 		localPlayer.m_IsFading = false
 	end
 end
