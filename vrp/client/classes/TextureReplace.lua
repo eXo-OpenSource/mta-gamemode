55082a98dcecd89ee0f56bb2e2b94e48d7641533
Strobe <devcxo@gmail.com>
2017-03-29 20:39:27 +0200

    Revert on #5ea70bc

--- vrp/client/classes/TextureReplace.lua
+++ vrp/client/classes/TextureReplace.lua
@@ -50,7 +50,7 @@ function TextureReplace:destructor()
 		if self.m_IsRenderTarget then
 			destroyElement(self.m_Texture)
 		else
-			TextureReplace.unloadCache(self.m_TexturePath, self.m_IsRawPixels, self.m_URLPath,  self.m_Element)
+			TextureReplace.unloadCache(self.m_TexturePath, self.m_IsRawPixels, self.m_URLPath)
 		end
 	end
 	if self.m_Shader and isElement(self.m_Shader) then
@@ -155,16 +155,16 @@ function TextureReplace:unloadShader()
 		end
 	end
 	--local a = destroyElement(self.m_Texture)
-	local a = TextureReplace.unloadCache(self.m_TexturePath, self.m_IsRawPixels, self.m_URLPath, self.m_Element)
+	local a = TextureReplace.unloadCache(self.m_TexturePath, self.m_IsRawPixels, self.m_URLPath)
 	local b = destroyElement(self.m_Shader)
 	
 	return a and b and c
 end
 
-function TextureReplace.getCachedTexture(path, bIsRawPixels, url, elem)
+function TextureReplace.getCachedTexture(path, bIsRawPixels, url)
 	local index = md5(path):sub(1, 8)
 	if bIsRawPixels then 
-		index = md5(url..""..tostring(elem)):sub(1,8)
+		index = url
 	end
 	if not TextureReplace.Cache[index] then
 		--outputConsole("creating texture "..path)
@@ -189,10 +189,10 @@ function TextureReplace.getCachedTexture(path, bIsRawPixels, url, elem)
 	return TextureReplace.Cache[index].texture
 end
 
-function TextureReplace.unloadCache(path, isRawPixels, url, elem)
+function TextureReplace.unloadCache(path, isRawPixels, url)
 	local index = md5(path):sub(1, 8)
 	if isRawPixels then 
-		index = md5(url..""..tostring(elem)):sub(1, 8)
+		index = url
 	end
 	if not TextureReplace.Cache[index] then return false end
 	TextureReplace.Cache[index].counter = TextureReplace.Cache[index].counter - 1
