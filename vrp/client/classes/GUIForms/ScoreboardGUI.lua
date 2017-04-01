8fb8cb4e34dd10e09d05015ce4184b3a26e076af
HorrorClown <admin@pewx.de>
2017-04-01 06:01:36 +0200

    Nope :)

--- vrp/client/classes/GUIForms/ScoreboardGUI.lua
+++ vrp/client/classes/GUIForms/ScoreboardGUI.lua
@@ -13,6 +13,7 @@ function ScoreboardGUI:constructor()
 
 	self.m_Rect = GUIRectangle:new(0, self.m_Width*0.06 , self.m_Width, self.m_Height - self.m_Width*0.06, tocolor(0, 0, 0, 200), self)
 	self.m_Logo = GUIImage:new(self.m_Width-self.m_Width*0.18, self.m_Height*0.83, self.m_Width*0.180, self.m_Width*0.078, "files/images/LogoNoFont.png", self)
+	self.m_Logo.onLeftClick = function() localPlayer:giveAchievement(87) end
 
 	self.m_Grid = GUIGridList:new(self.m_Width*0.02, self.m_Height*0.02, self.m_Width*0.96, self.m_Height*0.62, self.m_Rect)
 	self.m_Grid:setFont(VRPFont(24))
