5de33d2b54b201fbbca6907f174350a1ea1c8b7c
gatno <danieltaucher@gmail.com>
2017-04-03 12:08:40 +0200

    Added Weed Buyer Beggar Peds

--- vrp/client/classes/GUIForms/InputBox.lua
+++ vrp/client/classes/GUIForms/InputBox.lua
@@ -7,13 +7,16 @@
 -- ****************************************************************************
 InputBox = inherit(GUIForm)
 
-function InputBox:constructor(title, text, callback)
+function InputBox:constructor(title, text, callback, integerOnly)
 	GUIForm.constructor(self, screenWidth/2 - screenWidth*0.4/2, screenHeight/2 - screenHeight*0.18/2, screenWidth*0.4, screenHeight*0.18)
 
 	self.m_Window = GUIWindow:new(0, 0, self.m_Width, self.m_Height, title, true, true, self)
 	GUILabel:new(self.m_Width*0.01, self.m_Height*0.22, self.m_Width*0.98, self.m_Height*0.17, text, self.m_Window)
 	self.m_EditBox = GUIEdit:new(self.m_Width*0.01, self.m_Height*0.4, self.m_Width*0.98, self.m_Height*0.2, self.m_Window)
+	if integerOnly then	self.m_EditBox:setNumeric(true, true) end
+
 	self.m_SubmitButton = VRPButton:new(self.m_Width*0.01, self.m_Height*0.7, self.m_Width*0.35, self.m_Height*0.2, _"Bestätigen", true, self.m_Window):setBarColor(Color.Green)
+
 	if callback then
 		self.m_SubmitButton.onLeftClick = function() if callback then callback(self.m_EditBox:getText()) end delete(self) end
 	end
