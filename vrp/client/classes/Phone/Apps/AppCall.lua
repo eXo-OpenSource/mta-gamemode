1f125d67ddc8f4c97ba5bb73ca4bc3c9e0b1b838
HorrorClown <admin@pewx.de>
2017-04-09 23:40:41 +0200

    Changed phone char delete icon

--- vrp/client/classes/Phone/Apps/AppCall.lua
+++ vrp/client/classes/Phone/Apps/AppCall.lua
@@ -90,10 +90,7 @@ function MainActivity:constructor(app)
 	self.m_Label = GUILabel:new(10, 10, 200, 50, _"Telefon", self.m_Tabs["Keyboard"]) -- 3
 	self.m_Edit = GUIEdit:new(10, 60, 200, 40, self.m_Tabs["Keyboard"])
 	self.m_Edit:setCaption(_"Telefonnummer")
-	self.m_ButtonDelete = GUIButton:new(215, 60, 40, 40, FontAwesomeSymbols.Back, self.m_Tabs["Keyboard"])
-		:setFont(FontAwesome(30))
-		:setFontSize(1)
-		:setBackgroundColor(Color.Red)
+	self.m_ButtonDelete = GUIButton:new(215, 60, 40, 40, "⌫", self.m_Tabs["Keyboard"]):setBackgroundColor(Color.Red)
 	self.m_ButtonDelete.onLeftClick = function() self.m_Edit:setText(self.m_Edit:getText():sub(1, #self.m_Edit:getText() - 1)) end
 
 	self.m_ButtonCallNumpad = GUIButton:new(self.m_Width-110, 370, 100, 30, _"Anrufen", self.m_Tabs["Keyboard"]):setBackgroundColor(Color.Green)
