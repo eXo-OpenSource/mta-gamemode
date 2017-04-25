657ddd4ad16721c0acc61c24dff207a9e8243b4a
Stumpy <danieltaucher@gmail.com>
2017-04-26 01:06:24 +0200

    Updated Speedcam Mouse-Menu Added Icons Added Owner

--- vrp/client/classes/GUIForms/MouseMenu/SpeedCamMouseMenu.lua
+++ vrp/client/classes/GUIForms/MouseMenu/SpeedCamMouseMenu.lua
@@ -9,17 +9,17 @@ SpeedCamMouseMenu = inherit(GUIMouseMenu)
 
 function SpeedCamMouseMenu:constructor(posX, posY, element)
 	GUIMouseMenu.constructor(self, posX, posY, 300, 1) -- height doesn't matter as it will be set automatically
+	self:addItem(_("Aufgestellt von: %s", getElementData(element, "owner"))):setIcon(FontAwesomeSymbols.Player)
 
 	if localPlayer:getFaction() and localPlayer:getFaction():isStateFaction() and localPlayer:getPublicSync("Faction:Duty") == true then
-		self:addItem(_("Einnahmen: %d$", getElementData(element, "earning")))
-
+		self:addItem(_("Einnahmen: %d$", getElementData(element, "earning"))):setIcon(FontAwesomeSymbols.Money)
 		self:addItem(_"Abbauen",
 			function()
 				if self:getElement() then
 					triggerServerEvent("worldItemCollect", self:getElement())
 				end
 			end
-		)
+		):setIcon(FontAwesomeSymbols.Trash)
 	end
 end
 
