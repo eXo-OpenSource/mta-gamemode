2742002499b30ec02961a4805d491b7491cbba1e
Stumpy <danieltaucher@gmail.com>
2017-04-28 00:22:59 +0200

    Fixed Vehicle Custom Texture Price

--- vrp/client/classes/GUIForms/VehicleCustomTextureGUI.lua
+++ vrp/client/classes/GUIForms/VehicleCustomTextureGUI.lua
@@ -131,7 +131,7 @@ function VehicleCustomTextureGUI:initTextures(textures)
 		item.Id = row["Id"]
         item.onLeftClick = bind(self.Texture_Click, self)
 		item.onLeftDoubleClick = function()
-			QuestionBox:new(_("Möchtest du die Textur wirklich für 15.000$ kaufen?"),
+			QuestionBox:new(_("Möchtest du die Textur wirklich für 75.000$ kaufen?"),
 				function()
 					triggerServerEvent("vehicleCustomTextureBuy", self.m_Vehicle, item.Id, item.Url, self.m_Tuning:getTuning("Color1"), self.m_Tuning:getTuning("Color2"))
 				end
