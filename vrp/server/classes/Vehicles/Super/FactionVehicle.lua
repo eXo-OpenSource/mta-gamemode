f499a1f8850ecb98ec88d3d823a0cb9a2c860c8c
Strobe <devcxo@gmail.com>
2017-04-28 20:04:50 +0200

    Added ELS for Rescue/Fire-Department!

--- vrp/server/classes/Vehicles/Super/FactionVehicle.lua
+++ vrp/server/classes/Vehicles/Super/FactionVehicle.lua
@@ -72,6 +72,12 @@ function FactionVehicle:constructor(Id, faction, color, health, posionType, tuni
 			self.m_VehELSObj = ELSSystem:new(self)
 		end
 	end
+	
+	if faction:isRescueFaction() then
+		if self:getVehicleType() == VehicleType.Automobile then
+			self.m_VehELSObj = ELSSystem:new(self)
+		end
+	end
 	if handlingFaktor and handlingFaktor ~= "" then
 		local handling = getOriginalHandling(getElementModel(self))
 		local tHandlingTable = split(handlingFaktor, ";")
