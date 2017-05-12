8785dbba970dd55fc265a1de58034558be584e65
Strobe <devcxo@gmail.com>
2017-05-12 04:34:09 +0200

    Included Weapon_Attach in meta.xml

--- [deps]/weapon_attach/C_attach.lua
+++ [deps]/weapon_attach/C_attach.lua
@@ -3,3 +3,4 @@ function removeWeaponOnSwitch(prevSlot, newSlot)
 	triggerServerEvent("createWepObject", localPlayer, localPlayer, getPedWeapon(localPlayer, prevSlot), 1, getSlotFromWeapon(getPedWeapon(localPlayer, prevSlot)))
 end
 addEventHandler("onClientPlayerWeaponSwitch", getRootElement(), removeWeaponOnSwitch)
+
