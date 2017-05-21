df052c6b84811fd74f1bbd24d0bc248dbcff42a8
Strobe <devcxo@gmail.com>
2017-05-21 03:48:48 +0200

    Fixed camera-bug

--- vrp/server/classes/Business/CompanyManager.lua
+++ vrp/server/classes/Business/CompanyManager.lua
@@ -374,8 +374,9 @@ function CompanyManager:Event_toggleDuty()
 			client:sendInfo(_("Du bist nun im Dienst deines Unternehmens!", client))
 			client:setPublicSync("Company:Duty",true)
             takeAllWeapons(client)
-			giveWeapon(client, 43, 50) -- Camera
-
+			if company.m_Id == 3 then
+				giveWeapon(client, 43, 50) -- Camera
+			end
             if company.start then
                 company:start(client)
             end
