4931eec979c7485b582a68e0810d7e1f00a5b80d
MasterM <masterm4yt@gmail.com>
2017-06-04 00:24:00 +0200

    sqashmerge objectplacer into master

--- vrp/client/classes/ObjectPlacer.lua
+++ vrp/client/classes/ObjectPlacer.lua
@@ -8,76 +8,114 @@
 ObjectPlacer = inherit(Object)
 
 function ObjectPlacer:constructor(model, callback)
-    showCursor(true)
+	showCursor(true)
 
-    self.m_Object = createObject(model, localPlayer:getPosition())
-    self.m_Object:setCollisionsEnabled(false)
-    self.m_Callback = callback
+	self.m_Object = createObject(model, localPlayer:getPosition())
+	self.m_Object:setCollisionsEnabled(false)
+	self.m_Callback = callback
 
-    self.m_CursorMove = bind(self.Event_CursorMove, self)
-    addEventHandler("onClientCursorMove", root, self.m_CursorMove)
+	self.m_CursorMove = bind(self.Event_CursorMove, self)
+	addEventHandler("onClientCursorMove", root, self.m_CursorMove)
 
-    self.m_MouseWheel = bind(self.Event_MouseWheel, self)
-    bindKey("mouse_wheel_down", "down", self.m_MouseWheel)
-    bindKey("mouse_wheel_up", "down", self.m_MouseWheel)
+	self.m_MouseWheel = bind(self.Event_MouseWheel, self)
+	bindKey("mouse_wheel_down", "down", self.m_MouseWheel)
+	bindKey("mouse_wheel_up", "down", self.m_MouseWheel)
 
-    self.m_Click = bind(self.Event_Click, self)
-    addEventHandler("onClientClick", root, self.m_Click)
-
+	self.m_Click = bind(self.Event_Click, self)
+	addEventHandler("onClientClick", root, self.m_Click)
+	
+	--do not indent as it will destroy the short message design
+	self.m_ShortMessage = ShortMessage:new(_[[Bewege das Objekt mit deiner Maus
+Weitere Funktionen:
+  [Mausrad] - Objekt drehen (10°)
+  [Shift] - Objekt schneller drehen (45°)
+  [Alt] - Objekt langsamer drehen (5°)
+Linksklick zum Platzieren
+Rechtsklick zum Abbrechen]], "Objektplatzierung", Color.DarkBlue, 9999999)
 
 end
 
 function ObjectPlacer:destructor()
-    if self.m_Object and isElement(self.m_Object) then
-        self.m_Object:destroy()
-    end
+	if self.m_Object and isElement(self.m_Object) then
+		self.m_Object:destroy()
+	end
 
-    unbindKey("mouse_wheel_down", "down", self.m_MouseWheel)
-    unbindKey("mouse_wheel_up", "down", self.m_MouseWheel)
-    removeEventHandler("onClientCursorMove", root, self.m_CursorMove)
-    removeEventHandler("onClientClick", root, self.m_Click)
+	delete(self.m_ShortMessage)
+	unbindKey("mouse_wheel_down", "down", self.m_MouseWheel)
+	unbindKey("mouse_wheel_up", "down", self.m_MouseWheel)
+	removeEventHandler("onClientCursorMove", root, self.m_CursorMove)
+	removeEventHandler("onClientClick", root, self.m_Click)
 end
 
 function ObjectPlacer:Event_CursorMove(cursorX, cursorY, absX, absY, worldX, worldY, worldZ)
-    local camX, camY, camZ = getCameraMatrix()
-    local surfaceFound, surfaceX, surfaceY, surfaceZ, element, nx, ny, nz, materialID = processLineOfSight(camX, camY, camZ, worldX, worldY, worldZ, true,
-        true, true, true, true, true, false, true, self.m_Object)
-
+	local camX, camY, camZ = getCameraMatrix()
+	local surfaceFound, surfaceX, surfaceY, surfaceZ, element, nx, ny, nz, materialID = processLineOfSight(camX, camY, camZ, worldX, worldY, worldZ, true,
+		true, true, true, true, true, false, true, localPlayer, false, true)
 	if surfaceFound and materialID ~= 51 and materialID ~= 53 then -- probably add more unexpeted materialIDs
-		local surfaceZ = getGroundPosition(surfaceX, surfaceY, surfaceZ)
-        self.m_Object:setPosition(surfaceX, surfaceY, surfaceZ + self.m_Object:getDistanceFromCentreOfMassToBaseOfModel())
-    end
+		local groundZ = getGroundPosition(surfaceX, surfaceY, surfaceZ + 2)
+		if groundZ > surfaceZ then return end -- just stop the object if if would spawn underground
+
+		self.m_Object:setPosition(surfaceX, surfaceY, surfaceZ + self.m_Object:getDistanceFromCentreOfMassToBaseOfModel())
+		self.m_HitElement = element
+		
+		--[[dxDrawLine3D(surfaceX + 1, surfaceY, surfaceZ, surfaceX - 1, surfaceY, surfaceZ, tocolor(200, 0, 0), 2)
+		dxDrawLine3D(surfaceX, surfaceY + 1, surfaceZ, surfaceX, surfaceY - 1, surfaceZ, tocolor(200, 0, 0), 2)
+		dxDrawLine3D(surfaceX, surfaceY, surfaceZ + 1, surfaceX, surfaceY, surfaceZ - 1, tocolor(200, 0, 0), 2)]]
+	end
 end
 
 function ObjectPlacer:Event_MouseWheel(button, state)
-    if button == "mouse_wheel_down" then
-        self.m_Object:setRotation(0, 0, self.m_Object:getRotation().z + 10)
-    else
-        self.m_Object:setRotation(0, 0, self.m_Object:getRotation().z - 10)
-    end
+	local offset = 10
+	if getKeyState("lshift") or getKeyState("rshift") then offset = 45 end
+	if getKeyState("lalt") then offset = 5 end
+	if button == "mouse_wheel_down" and state == "down" then
+		self.m_Object:setRotation(0, 0, self.m_Object:getRotation().z + offset)
+	else
+		self.m_Object:setRotation(0, 0, self.m_Object:getRotation().z - offset)
+	end
 end
 
-function ObjectPlacer:Event_Click()
-    if self.m_Callback then
-        self.m_Callback(self.m_Object:getPosition(), self.m_Object:getRotation().z)
-    end
-
-    -- Self-destruct
-    delete(self)
+function ObjectPlacer:Event_Click(btn, state)
+	if state ~= "up" then return false end
+	
+	if btn == "left" then
+		if self.m_HitElement and isElement(self.m_HitElement) and (self.m_HitElement:getType() == "player" or self.m_HitElement:getType() == "ped") then
+			return ErrorBox:new(_"Du kannst Objekte nicht an Spielern platzieren.")
+		end
+		if self.m_HitElement and isElement(self.m_HitElement) and self.m_HitElement:getType() == "vehicle" then
+			return ErrorBox:new(_"Du kannst Objekte nicht an Fahrzeugen platzieren.")
+		end
+		if self.m_Callback then
+			iprint(self.m_HitElement)
+			self.m_Callback(self.m_Object:getPosition(), self.m_Object:getRotation().z)
+		end
+	else
+		self.m_Callback(false)
+	end
+	-- Self-destruct
+	delete(self)
 end
 
 addEvent("objectPlacerStart", true)
 addEventHandler("objectPlacerStart", root,
-    function(model, callbackEvent)
-        Inventory:getSingleton():hide()
-        setTimer(
-        function(model,callbackEvent)
-            local objectPlacer = ObjectPlacer:new(model,
-                function(position, rotation)
-                    triggerServerEvent(callbackEvent, localPlayer, position.x, position.y, position.z, rotation)
-                end
-            )
-        end,250,1,model,callbackEvent)
-
-    end
+	function(model, callbackEvent)
+		Inventory:getSingleton():hide()
+		setTimer(
+		function(model,callbackEvent)
+			local objectPlacer = ObjectPlacer:new(model,
+				function(position, rotation)
+					if position then
+						triggerServerEvent(callbackEvent, localPlayer, position.x, position.y, position.z, rotation)
+					else
+						triggerServerEvent(callbackEvent, localPlayer, false)
+					end
+					setTimer(
+						function()
+							Inventory:getSingleton():show()
+						end, 
+					50, 1)
+				end
+			)
+		end,50,1,model,callbackEvent)
+	end
 )
