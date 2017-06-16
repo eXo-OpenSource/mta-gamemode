65a9241b05ea901a2b37dbb9457c65901dbd4c08
HorrorClown <admin@pewx.de>
2017-06-16 14:58:30 +0200

    Updated the most hated achievement

--- vrp/client/classes/EasterEggs/PewPew.lua
+++ vrp/client/classes/EasterEggs/PewPew.lua
@@ -18,10 +18,12 @@ end
 function EasterEgg.PewPew:destructor()
 end
 
-local rndText = {"Pew Pew!", "PEW", "PewX", "#PewPorn.com", "<3", "50", "CATS", "meow"}
+local rndText = {"Pew Pew!", "PEW", "PewX", "#PewPorn.com", "<3", "50", "CATS", "meow", "OMG", "PR0N", "grills"}
 local colors = {Color.DarkLightBlue, Color.Red, Color.Blue, Color.Green, Color.Orange, Color.Brown, Color.White}
 function EasterEgg.PewPew:startRender()
-	if isTimer(self.m_Timer) then killTimer(self.m_Timer) end
+	playSound("http://pewx.de/res/sounds/tactical_nuke.mp3")
+	playSound("http://pewx.de/res/sounds/SKRILLEX_Scary.mp3")
+	playSound("http://pewx.de/res/sounds/OMG_TRICKSHOT_CHILD.mp3")
 
 	self.m_OffsetX = -452
 	self.m_Wank = -10
@@ -29,7 +31,7 @@ function EasterEgg.PewPew:startRender()
 	self.m_TextMoveOut = false
 	self.m_Texts = {}
 
-	for i = 1, 50 do
+	for i = 1, 100 do
 		local text = rndText[math.random(1, #rndText)]
 		table.insert(self.m_Texts, {text = text, x = math.random(0, screenWidth), y = math.random(0, screenHeight), w = dxGetTextWidth(text, 3), h = dxGetFontHeight(3), color = colors[math.random(1, #colors)], moveDown = math.random(0,1) == 1, moveRight = math.random(0,1) == 1})
 	end
@@ -47,19 +49,27 @@ function EasterEgg.PewPew:startRender()
 	self.m_Animation2:startAnimation(2000, "SineCurve", 20)
 	self.m_Animation3:startAnimation(3000, "OutQuad", 0, 0, 0)
 
-	self.m_Timer = setTimer(
+	setTimer(
 		function()
 			self.m_Wank = -10
 			self.m_Animation2:startAnimation(2000, "SineCurve", 20)
 		end, 2000, 4
 	)
 
-	setTimer(function() self.m_TextMoveOut = true end, 5000, 1)
+	setTimer(
+		function()
+			self.m_TextMoveOut = true
+			playSound("http://pewx.de/res/sounds/SPOOKY.mp3")
+			playSound("http://pewx.de/res/sounds/AIRPORN.mp3")
+			playSound("http://pewx.de/res/sounds/wow.mp3")
+		end, 20000, 1
+	)
 
 	setTimer(
 		function()
+			playSound("http://pewx.de/res/sounds/DAMN_SON_WHERED_YOU_FIND_THIS.mp3")
 			removeEventHandler("onClientRender", root, self.m_RenderPewPewAchievement)
-		end, 8000, 1
+		end, 23000, 1
 	)
 end
 
@@ -71,8 +81,9 @@ function EasterEgg.PewPew:render()
 		dxDrawImage((screenWidth/2 - self.m_SizeX/2)*v[1], (screenHeight/2 - self.m_SizeY/2)*v[2], self.m_SizeX, self.m_SizeY, "files/images/Other/PewPew/head.png", self.m_Wank, 0, 0, tocolor(255, 255, 255, self.m_Alpha))
 	end
 
-	dxDrawImage(self.m_OffsetX, screenHeight/2, 415, 452, "files/images/Other/PewPew/body.png")
-	dxDrawImage(self.m_OffsetX, screenHeight/2 + 10, 415, 452, "files/images/Other/PewPew/head.png", self.m_Wank)
+	local width, height = 415/1600*screenWidth, 452/1024*screenHeight
+	dxDrawImage(self.m_OffsetX, screenHeight - height, width, height, "files/images/Other/PewPew/body.png")
+	dxDrawImage(self.m_OffsetX, screenHeight - height + 10, width, height, "files/images/Other/PewPew/head.png", self.m_Wank)
 
 	for _, v in pairs(self.m_Texts) do
 		if not self.m_TextMoveOut then
@@ -98,5 +109,10 @@ function EasterEgg.PewPew:render()
 		if v.moveDown then v.y = v.y + 5 else v.y = v.y - 5 end
 
 		dxDrawText(v.text, v.x, v.y, v.w, v.h, v.color, 3)
+	end
+
+	if math.random(1, 10) == 5 then
+		dxDrawImage(screenWidth/2-25, screenHeight/2-25, 50, 50, "files/images/Other/PewPew/hit.png")
+		playSound("http://pewx.de/res/sounds/HITMARKER.mp3")
 	end
 end
