bef96270d903c3951f1cb8f9fe46f4c727d17486
StiviiK <stefan.kuerzeder@gmail.com>
2017-06-20 01:36:20 +0200

    Fixed TaskWaitForGettingTargetted errors (when targeting mapped peds)

--- vrp/server/classes/AI/Tasks/TaskWaitForGettingTargetted.lua
+++ vrp/server/classes/AI/Tasks/TaskWaitForGettingTargetted.lua
@@ -8,11 +8,22 @@
 TaskWaitForGettingTargetted = inherit(Task)
 
 function TaskWaitForGettingTargetted:constructor(actor)
-
+	self.m_onPlayerTarget = bind(self.Event_OnPlayerTarget, self)
+	addEventHandler("onPlayerTarget", actor, self.m_onPlayerTarget)
 end
 
 function TaskWaitForGettingTargetted:destructor()
+	removeEventHandler("onPlayerTarget", self.m_Actor, self.m_onPlayerTarget)
     setPedAnimation(self.m_Actor, nil)
+end
+
+function TaskWaitForGettingTargetted.Event_OnPlayerTarget()
+	if targetElement and getElementType(targetElement) == "ped" and getPedWeapon(source) ~= 0 and instanceof(targetElement, Actor) then
+		local task = targetElement:getTaskById(TaskWaitForGettingTargetted.getId())
+		if task then
+			setPedAnimation(targetElement, "ped", "handsup", -1, false)
+		end
+	end
 end
 
 function TaskWaitForGettingTargetted:getId()
@@ -22,18 +33,3 @@ end
 function TaskWaitForGettingTargetted:hasClientScript()
     return false
 end
-
-function TaskWaitForGettingTargetted:Actor_GettingTargetted(attacker)
-    setPedAnimation(self.m_Actor, "ped", "handsup", -1, false)
-end
-
-addEventHandler("onPlayerTarget", root,
-    function(targetElement)
-        if targetElement and getElementType(targetElement) == "ped" and getPedWeapon(source) ~= 0 and instanceof(targetElement, Actor) then -- Todo: Sometimes Error: classlib.lua:139 - Cannot get the superclass of this element
-            local task = targetElement:getTaskById(TaskWaitForGettingTargetted.getId())
-            if task then
-                task:Actor_GettingTargetted(source)
-            end
-        end
-    end
-)
