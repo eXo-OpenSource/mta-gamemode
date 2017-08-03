9af5956843959943464c79b92ff9105c7f70d37c
gatno <danieltaucher@gmail.com>
2017-08-03 10:00:31 +0200

    Updated EPT Seat Position

--- vrp/server/classes/Gameplay/Chair.lua
+++ vrp/server/classes/Gameplay/Chair.lua
@@ -31,7 +31,7 @@ Chair.Map = {
 	[2356] = {seats = 1, offsetPosition = Vector3(-.1, .7, .1), rotationOffset = 0},
 	[1721] = {seats = 1, offsetPosition = Vector3(0, .7, .1), rotationOffset = 0},
 	[2309] = {seats = 1, offsetPosition = Vector3(-0.05, .7, .1), rotationOffset = 0},
-	[1704] = {seats = 1, offsetPosition = Vector3(-0.05, .7, .1), rotationOffset = 0},
+	[1704] = {seats = 1, offsetPosition = Vector3(0.5, -0.6, .2), rotationOffset = 180},
 }
 
 function Chair:constructor()
