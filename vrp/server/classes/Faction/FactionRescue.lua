9f0122a60cd73aa42889393b49b58c715d7d39d1
Strobe <devcxo@gmail.com>
2017-08-08 04:34:15 +0200

    Fix Rescue-Safe; Add heliport-immo.map

--- vrp/server/classes/Faction/FactionRescue.lua
+++ vrp/server/classes/Faction/FactionRescue.lua
@@ -56,7 +56,7 @@ function FactionRescue:constructor()
 
 	nextframe( -- Todo workaround
 		function ()
-			local safe = createObject(2332, 1724.65, -1754.29, 13.55, 0, 0, 0)
+			local safe = createObject(2332, 1724.65, -1754.29, 15.55, 0, 0, 180)
 			FactionManager:getSingleton():getFromId(4):setSafe(safe)
 
 			FactionManager:getSingleton():createVehicleServiceMarker("Rescue", Vector3(1798, -1739.7, 5)) --Unity garage
