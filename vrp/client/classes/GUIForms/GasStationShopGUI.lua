ab55014b8c7da8061feaa721accc444a3d054a2f
HorrorClown <admin@pewx.de>
2017-08-20 18:41:19 +0200

    Finished gas station GUI

--- vrp/client/classes/GUIForms/GasStationShopGUI.lua
+++ vrp/client/classes/GUIForms/GasStationShopGUI.lua
@@ -11,7 +11,7 @@ inherit(Singleton, GasStationShopGUI)
 addRemoteEvents{"showGasStationShopGUI", "refreshGasStationShopGUI"}
 
 function GasStationShopGUI:constructor(callback, name)
-	GUIForm.constructor(self, screenWidth/2-270, screenHeight/2-230, 540, 460)
+	GUIForm.constructor(self, screenWidth/2-240, screenHeight/2-150, 480, 300)
 
 	self.m_TabPanel = GUITabPanel:new(0, 0, self.m_Width, self.m_Height, self)
 	self.m_CloseButton = GUIButton:new(self.m_Width-30, 0, 30, 30, FontAwesomeSymbols.Close, self):setFont(FontAwesome(20)):setBackgroundColor(Color.Clear):setBackgroundHoverColor(Color.Red):setHoverColor(Color.White):setFontSize(1)
@@ -20,6 +20,8 @@ function GasStationShopGUI:constructor(callback, name)
 	local tabFuelStation = self.m_TabPanel:addTab(_("Tankstelle"))
 	local tabItemShop = self.m_TabPanel:addTab(_("Shop"))
 
+
+	--- Fuelstation
 	GUILabel:new(5, 5, 200, 30, "Tankstelle:", tabFuelStation)
 	GUILabel:new(5, 35, 200, 30, "Liter:", tabFuelStation)
 	GUILabel:new(5, 65, 200, 30, "Preis:", tabFuelStation)
@@ -36,8 +38,8 @@ function GasStationShopGUI:constructor(callback, name)
 		self.m_Fuel:setText(fuel .. " L")
 		self.m_Price:setText(fuel * 2 .. " $")
 
-		self.m_Confirm = GUIButton:new(5, 120, 200, 25, "Bezahlen", tabFuelStation):setBackgroundColor(Color.Green)
-		self.m_Cancel = GUIButton:new(210, 120, 200, 25, "Abbrechen", tabFuelStation):setBackgroundColor(Color.Red)
+		self.m_Confirm = GUIButton:new(5, 240, 230, 25, "Bezahlen", tabFuelStation):setBackgroundColor(Color.Green)
+		self.m_Cancel = GUIButton:new(240, 240, 230, 25, "Abbrechen", tabFuelStation):setBackgroundColor(Color.Red)
 
 		self.m_Confirm.onLeftClick =
 			function()
@@ -54,39 +56,22 @@ function GasStationShopGUI:constructor(callback, name)
 			end
 	end
 
-	---
-
-	self.m_Preview = GUIImage:new(280, 5, 150, 150, false, tabItemShop)
-	self.m_LabelDescription = GUILabel:new(265, 160, 150, 20, "", tabItemShop)
-	self.m_LabelDescription:setFont(VRPFont(self.m_Height*0.07)):setMultiline(true)
-
-	self.m_Grid = GUIGridList:new(5, 5, 255, 420, tabItemShop)
+	--- Itemshop
+	self.m_Grid = GUIGridList:new(5, 5, 230, 260, tabItemShop)
 	self.m_Grid:addColumn(_"Name", 0.7)
 	self.m_Grid:addColumn(_"Preis", 0.3)
 
-	addEventHandler("refreshGasStationShopGUI", root, bind(GasStationShopGUI.refreshItemShopGUI, self))
+	self.m_Preview = GUIImage:new(300, 5, 120, 120, false, tabItemShop)
+	self.m_LabelDescription = GUILabel:new(240, 130, 240, 25, "", tabItemShop):setMultiline(true)
 
-	--[[GUIForm.constructor(self, screenWidth/2-screenWidth*0.3*0.5, screenHeight/2-screenHeight*0.4*0.5, screenWidth*0.3, screenHeight*0.4)
+	GUILabel:new(240, 230, 100, 25, "Anzahl:", tabItemShop)
+	self.m_EditAmount = GUIEdit:new(300, 230, 50, 25, tabItemShop):setNumeric(true, true):setMaxLength(3):setText("1")
 
-	self.m_Window = GUIWindow:new(0, 0, self.m_Width, self.m_Height, _"Shop", true, true, self)
-	self.m_Preview = GUIImage:new(self.m_Height*0.08, self.m_Height*0.12, self.m_Width*0.2, self.m_Width*0.2, false, self.m_Window)
-	self.m_LabelDescription = GUILabel:new(self.m_Width*0.02, self.m_Width*0.3, self.m_Width*0.45, self.m_Height-self.m_Width*0.76, "", self.m_Window) -- use width to align correctly
-	self.m_LabelDescription:setFont(VRPFont(self.m_Height*0.07)):setMultiline(true)
-
-	self.m_Grid = GUIGridList:new(self.m_Width*0.5, self.m_Height*0.12, self.m_Width*0.48, self.m_Height*0.7, self.m_Window)
-	self.m_Grid:addColumn(_"Name", 0.7)
-	self.m_Grid:addColumn(_"Preis", 0.3)
-
-	GUILabel:new(self.m_Width*0.08, self.m_Height*0.8, self.m_Width*0.2, self.m_Height*0.1, "Anzahl:", self.m_Window)
-	self.m_EditAmount = GUIEdit:new(self.m_Width*0.26, self.m_Height*0.8, self.m_Width*0.1, self.m_Height*0.1, self.m_Window)
-	self.m_EditAmount:setNumeric(true, true)
-	self.m_EditAmount:setText("1")
-
-	self.m_ButtonBuy = VRPButton:new(self.m_Width*0.65, self.m_Height*0.85, self.m_Width*0.33, self.m_Height*0.12, _"Kaufen", true, self.m_Window):setBarColor(Color.Green)
+	self.m_ButtonBuy = GUIButton:new(355, 230, 120, 25, _"Kaufen", tabItemShop):setBackgroundColor(Color.Green)
 	self.m_ButtonBuy.onLeftClick = bind(self.ButtonBuy_Click, self)
 
 	addEventHandler("refreshGasStationShopGUI", root, bind(GasStationShopGUI.refreshItemShopGUI, self))
-	self.m_CallBack = callback]]
+	self.m_CallBack = callback
 end
 
 function GasStationShopGUI:refreshItemShopGUI(shopId, items)
