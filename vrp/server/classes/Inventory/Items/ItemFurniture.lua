236d5905d277ef091f4bd57eac4b296e0446ae32
StiviiK <stefan.kuerzeder@gmail.com>
2017-08-21 13:47:03 +0200

    Fixed "unable to place" error

--- vrp/server/classes/Inventory/Items/ItemFurniture.lua
+++ vrp/server/classes/Inventory/Items/ItemFurniture.lua
@@ -8,16 +8,19 @@
 ItemFurniture = inherit(Item)
 
 function ItemFurniture:use(player, itemId, bag, place, itemName)
-	if player.m_CurrentHouse ~= nil and player.m_CurrentHouse:isValidToEnter(player) then
-		local result = self:startObjectPlacing(player,
-			function(item, position, rotation)
-				if item ~= self or not position then return end
-				player.m_CurrentHouse:createInsideFurniture(item, position, rotation)
-
-				player:getInventory():removeItemFromPlace(bag, place, 1)
-			end
-		)
-	else
-		player:sendError("No Access!")
+	local currentHouse = player.m_CurrentHouse
+	if currentHouse ~= nil then
+		if currentHouse:isValidToEnter(player) and currentHouse.m_HasGTAInterior == false then
+			local result = self:startObjectPlacing(player,
+				function(item, position, rotation)
+					if item ~= self or not position then return end
+					player.m_CurrentHouse:createInsideFurniture(item, position, rotation)
+					player:getInventory():removeItemFromPlace(bag, place, 1)
+					return
+				end
+			)
+		end
 	end
+
+	player:sendError(_("Du kannst hier kein(e) %s platzieren!", player, self:getName()))
 end
