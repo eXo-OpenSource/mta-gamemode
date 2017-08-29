da54eecefe9ba0403d2c3ab5b5212a6e7aa5596a
HorrorClown <admin@pewx.de>
2017-08-29 23:52:19 +0200

    Started super cute turtle race

---
+++ vrp/server/classes/Gameplay/Minigames/TurtleRace.lua
@@ -0,0 +1,15 @@
+-- ****************************************************************************
+-- *
+-- *  PROJECT:     vRoleplay
+-- *  FILE:        TODO
+-- *  PURPOSE:     TODO
+-- *
+-- ****************************************************************************
+TurtleRace = inherit(Singleton)
+
+function TurtleRace:constructor()
+
+end
+
+function TurtleRace:destructor()
+end
