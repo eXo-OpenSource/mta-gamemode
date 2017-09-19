b3d2eb8b006bf3122ccfc74f135d4efb91a1aabe
Strobe <devcxo@gmail.com>
2017-09-19 03:44:52 +0200

    Add on Countdown

--- vrp/client/classes/Gameplay/Ware/WareHUD.lua
+++ vrp/client/classes/Gameplay/Ware/WareHUD.lua
@@ -41,7 +41,7 @@ function WareHUD:render_bomb()
 	dxDrawText(timeLeft, bombStartX, bombStartY+bombHeight*1.1, bombStartX + bombWidth, (bombStartY + bombHeight*1.1), tocolor(200, 200*(1-prog), 0, 255), 1*scale, "default-bold", "center")
 	dxDrawImage(bombStartX, bombStartY, bombWidth, bombHeight, self:makePath("bomb.png"))
 	dxDrawImageSection(bombStartX+bombWidth+screenWidth*0.3, bombStartY+bombHeight*0.5-screenHeight*0.025, -1*screenWidth*0.3, math.floor(screenHeight*0.05), (self.m_FuseWidth*prog)*-1, 0, self.m_FuseWidth, self.m_FuseHeight, fuseTexture)
-	dxDrawImage((bombStartX+bombWidth + (screenWidth*0.3*(1-prog))) - ((screenWidth*0.015*scale - screenWidth*0.015)*0.5), bombStartY-((bombHeight*scale - bombHeight)*0.5), screenWidth*0.015*scale, bombHeight*scale, self:makePath("fire.png") , getTickCount(), -1*(screenWidth*0.015*scale)*0.1, 0, tocolor(255, 255, 255*prog, 255))
+	dxDrawImage((bombStartX+bombWidth + (screenWidth*0.3*(1-prog))) - ((screenWidth*0.015*scale - screenWidth*0.015)*0.5), bombStartY-((bombHeight*scale - bombHeight)*0.5), screenWidth*0.015*scale, bombHeight*scale, self:makePath("fire.png") , 0, 0, 0, tocolor(255, 255, 255*prog, 255))
 end
 
 function WareHUD:destructor()
