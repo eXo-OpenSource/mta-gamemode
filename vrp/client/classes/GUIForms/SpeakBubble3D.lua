965f3fe505c5d080f1c6715d91ddcfaf386b060f
gatno <danieltaucher@gmail.com>
2017-10-18 17:29:06 +0200

    Added Color Settings for SpeakBubble 3D

--- vrp/client/classes/GUIForms/SpeakBubble3D.lua
+++ vrp/client/classes/GUIForms/SpeakBubble3D.lua
@@ -9,13 +9,40 @@ function SpeakBubble3D:constructor(element, text, description, rotPlus)
 
 	self.m_Text = text
 	self.m_Description = description
+
+	self.m_TextColor = Color.LightBlue
+	self.m_BackgroundColor = Color.Black
+	self.m_BorderColor = Color.LightBlue
+	self.m_DescriptionColor = Color.White
+
 	rotPlus = rotPlus or 0
 	GUIForm3D.constructor(self, pos, element:getRotation()+Vector3(0,0,rotPlus), Vector2(1, 0.34), Vector2(200,70), 30)
 	SpeakBubble3D.Map[element] = self
 end
 
+function SpeakBubble3D:setTextColor(color)
+	self.m_TextColor = color
+	if self.m_TextLabel then self.m_TextLabel:setColor(color) end
+end
+
+function SpeakBubble3D:setBorderColor(color)
+	self.m_BorderColor = color
+	if self.m_Border then self.m_Border:setColor(color) end
+end
+
+function SpeakBubble3D:setBackgroundColor(color)
+	self.m_BackgroundColor = color
+	if self.m_BG then self.m_BG:setColor(color) end
+end
+
+function SpeakBubble3D:setDescriptionColor(color)
+	self.m_DescriptionColor = color
+	if self.m_DescriptionLabel then self.m_DescriptionLabel:setColor(color) end
+end
+
 function SpeakBubble3D:onStreamIn(surface)
-	GUIImage:new(0, 0, 200, 70, "files/images/Other/bubble.png", surface)
-	GUILabel:new(8, 2, 200, 25, self.m_Text, surface):setColor(Color.LightBlue)
-	GUILabel:new(8, 27, 200, 20, self.m_Description, surface)
+	self.m_BG = GUIImage:new(0, 0, 200, 70, "files/images/Other/bubble_bg.png", surface):setColor(self.m_BackgroundColor)
+	self.m_Border = GUIImage:new(0, 0, 200, 70, "files/images/Other/bubble_border.png", surface):setColor(self.m_BorderColor)
+	self.m_TextLabel = GUILabel:new(8, 2, 200, 25, self.m_Text, surface):setColor(self.m_TextColor)
+	self.m_DescriptionLabel = GUILabel:new(8, 27, 200, 20, self.m_Description, surface):setColor(self.m_DescriptionColor)
 end
