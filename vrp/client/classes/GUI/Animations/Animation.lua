6a7135a1af607d2131a84eac334b95c9cf38f861
StiviiK <stefan.kuerzeder@gmail.com>
2017-10-19 23:18:08 +0200

    moved to submodule

--- vrp/client/classes/GUI/Animations/Animation.lua
+++
@@ -1,6 +0,0 @@
-Animation = inherit(Object)
-
-function Animation:constructor () end
-function Animation:destructor () end
-function Animation:virtual_constructor () end
-function Animation:virtual_destructor () end
