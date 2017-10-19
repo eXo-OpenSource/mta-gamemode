57f4265d8eb831ca600726072f15d624a99d59dc
gatno <danieltaucher@gmail.com>
2017-10-19 15:54:12 +0200

    Added Halloween Sign [hide]

--- vrp/client/classes/Events/Halloween/Halloween.lua
+++ vrp/client/classes/Events/Halloween/Halloween.lua
@@ -14,4 +14,20 @@ function Halloween:constructor()
 			DrawContestOverviewGUI:new()
 		end
 	)
+
+	HalloweenSign:new()
+end
+
+HalloweenSign = inherit(GUIForm3D)
+inherit(Singleton, HalloweenSign)
+
+function HalloweenSign:constructor()
+	--1903, 1484.80, -1710.70
+	--rechts -> höher
+	GUIForm3D.constructor(self, Vector3(1484.86, -1710.80, 15.90), Vector3(0, 0, 180), Vector2(4.4, 2.09), Vector2(1200,600), 50)
+end
+
+function HalloweenSign:onStreamIn(surface)
+	self.m_Url = "http://exo-reallife.de/ingame/other/HalloweenSign.php"
+	GUIWebView:new(0, 0, 1200, 600, self.m_Url, true, surface)
 end
--- vrp/server/classes/Events/Halloween/Halloween.lua
+++ vrp/server/classes/Events/Halloween/Halloween.lua
@@ -2,4 +2,7 @@ Halloween = inherit(Singleton)
 
 function Halloween:constructor()
 	DrawContest:new()
+
+	self.m_EventSign = createObject(1903, 1484.80, -1710.70, 12.4, 0, 0, 90)
+	self.m_EventSign:setDoubleSided(true)
 end
