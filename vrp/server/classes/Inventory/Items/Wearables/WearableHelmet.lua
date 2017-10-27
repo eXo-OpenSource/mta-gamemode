eXo-OpenSource/mta-gamemode

commit 3a2d6a65f4575e96f1eda6ceab612380f0750637

fixed helmet issue, fixed FBI bug issue

vrp/server/classes/Inventory/Items/Wearables/WearableHelmet.lua
@@ -32,6 +32,11 @@ function WearableHelmet:use(player, itemId, bag, place, itemName)
 
 	end
 	if not player.m_IsWearingHelmet and not player.m_Helmet then --// if the player clicks onto the helmet without currently wearing one
+		if itemName == "Einsatzhelm" and not player:getFaction() or not player:getFaction():isStateFaction() or not player:isFactionDuty() then
+			player:sendError(_("Du bist nicht im Dienst! Das Item wurde abgenommen.", player))
+			player:getInventory():removeAllItem(self:getName())
+			return
+		end
 		if isElement(player.m_Helmet) then
 			destroyElement(player.m_Helmet)
 		end
@@ -64,6 +69,11 @@ function WearableHelmet:use(player, itemId, bag, place, itemName)
 		player:setData("isFaceConcealed", false)
 		player:meChat(true, "setzt "..WearableHelmet.objectTable[itemName][7].." ab!")
 	else --// else the player must have clicked on another helmet otherwise this instance of the class would have not been called
+		if itemName == "Einsatzhelm" and not player:getFaction() or not player:getFaction():isStateFaction() or not player:isFactionDuty() then
+			player:sendError(_("Du bist nicht im Dienst! Das Item wurde abgenommen.", player))
+			player:getInventory():removeAllItem(self:getName())
+			return
+		end
 		if isElement(player.m_Helmet) then
 			destroyElement(player.m_Helmet)
 		end

vrp/client/classes/GUIForms/PolicePanel.lua
@@ -267,8 +267,8 @@ function PolicePanel:onSelectBug(id)
 		if localPlayer:getFaction() and localPlayer:getFaction():getId() == 2 then
 			self.m_BugDisable:setEnabled(true)
 			self.m_BugClearLog:setEnabled(true)
-			self.m_BugLocate:setEnabled(true)
 		end
+		self.m_BugLocate:setEnabled(true)
 		self.m_BugRefresh:setEnabled(true)
 
 	else

commit d5afe7b1f820cf699ea139c6d7e47290e442b16c

Fixed Wearable Bug introduced in 3a2d6a65f4575e96f1eda6ceab612380f0750637

vrp/server/classes/Inventory/Items/Wearables/WearableHelmet.lua
@@ -32,7 +32,8 @@ function WearableHelmet:use(player, itemId, bag, place, itemName)
 
 	end
 	if not player.m_IsWearingHelmet and not player.m_Helmet then --// if the player clicks onto the helmet without currently wearing one
-		if itemName == "Einsatzhelm" and not player:getFaction() or not player:getFaction():isStateFaction() or not player:isFactionDuty() then
+		outputChatBox(itemName)
+		if itemName == "Einsatzhelm" and (not player:getFaction() or not player:getFaction():isStateFaction() or not player:isFactionDuty()) then
 			player:sendError(_("Du bist nicht im Dienst! Das Item wurde abgenommen.", player))
 			player:getInventory():removeAllItem(self:getName())
 			return