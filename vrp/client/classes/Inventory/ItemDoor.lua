3be5b02051585b7a0b0d2455a51753cfa816456f
Str0 <devcxo@gmail.com>
2017-12-09 18:41:08 +0100

    Add on Keypad

--- vrp/client/classes/Inventory/ItemDoor.lua
+++ vrp/client/classes/Inventory/ItemDoor.lua
@@ -1,31 +1,24 @@
-ItemKeyPad = inherit(GUIForm)
-inherit(Singleton, ItemKeyPad)
+ItemDoor = inherit(GUIForm)
+inherit(Singleton, ItemDoor)
 
 local instance = nil
-addEvent("promptKeyPad", true)
-addEventHandler("promptKeyPad", localPlayer,
+addEvent("promptDoorOption", true)
+addEventHandler("promptDoorOption", localPlayer,
 	function(id)
 		if not instance then
-			instance = ItemKeyPad:new(id)
+			instance = ItemDoor:new(id)
 		else 
 			instance:setIdLabel( id )
 		end
 	end
 )
 
-addEvent("playKeyPadSound", true)
-addEventHandler("playKeyPadSound", root, 
-	function( object, sound) 
-		local x,y,z = getElementPosition(object) 
-		setSoundMaxDistance(playSound3D("files/audio/Items/"..sound..".ogg", x, y, z), 30)
-	end
-)
 
-function ItemKeyPad:constructor( id )
+function ItemDoor:constructor( id )
 	GUIForm.constructor(self, screenWidth/2-(350/2)/2, 300, 350, 130, false)
 	self.m_Window = GUIWindow:new(0, 0, self.m_Width, self.m_Height, _"Keypad", true, false, self)
 	GUIRectangle:new(0, 30, self.m_Width, self.m_Height, tocolor(10, 0, 0, 150), self.m_Window)	
-	self.m_KeyPadCode = GUIEdit:new(0,40,self.m_Width, 40, self.m_Window):setColorRGB(0, 60, 0, 255):setNumeric(true, true):setMaxLength(4):setMasked("*"):setFont(VRPFont(20, Fonts.Digital))
+	self.m_DoorCode = GUIEdit:new(0,40,self.m_Width, 40, self.m_Window):setColorRGB(0, 60, 0, 255):setNumeric(true, true):setFont(VRPFont(20, Fonts.Digital))
 	self.m_AcceptButton = GUIButton:new(350/2 - 120, 90, 90, 30, FontAwesomeSymbols.Close, self.m_Window):setFont(FontAwesome(20)):setColor(tocolor(140, 0, 0, 255)):setBarEnabled(false):setBackgroundColor(tocolor(90, 90, 90, 255))
 	self.m_AcceptButton.onLeftClick = bind(self.closeForm, self)
 	self.m_DeclineButton = GUIButton:new(350/2+30, 90, 90, 30, FontAwesomeSymbols.Accept, self.m_Window):setFont(FontAwesome(20)):setColor(tocolor(0, 140, 0, 255)):setBarEnabled(false):setBackgroundColor(tocolor(90, 90, 90, 255))
@@ -33,25 +26,25 @@ function ItemKeyPad:constructor( id )
 	self:setIdLabel( id ) 
 end
 
-function ItemKeyPad:setIdLabel( id ) 
+function ItemDoor:setIdLabel( id ) 
 	if self.m_Window then 
-		self.m_Window:setTitleBarText ( "Keypad #"..id )
+		self.m_Window:setTitleBarText ( "Gehört zu Keypad #"..id )
 	end
 end
 
-function ItemKeyPad:destructor()
+function ItemDoor:destructor()
 	GUIForm.destructor(self)
 	instance = nil
 end
 
-function ItemKeyPad:closeForm() 
+function ItemDoor:closeForm() 
 	delete(self)
 end
 
-function ItemKeyPad:submitForm() 
-	local text = self.m_KeyPadCode:getText()
-	if #text == 4 then 
-		triggerServerEvent("onKeyPadSubmit", localPlayer, text)
+function ItemDoor:submitForm() 
+	local text = self.m_DoorCode:getText()
+	if #text > 0 and tonumber(text) then 
+		triggerServerEvent("onDoorLinkChange", localPlayer, tonumber(text))
 		delete(self)
 	end
 end
