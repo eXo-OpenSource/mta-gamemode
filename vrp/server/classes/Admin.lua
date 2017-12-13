2c75e679b9f33f7e0449b523de76611fb67f930c
StiviiK <stefan.kuerzeder@gmail.com>
2017-12-13 20:22:26 +0100

    Revert last two commits

--- vrp/server/classes/Admin.lua
+++ vrp/server/classes/Admin.lua
@@ -919,9 +919,7 @@ function Admin:onlineList(player)
 	end
 	if count > 0 then
 		outputChatBox("Folgende Teammitglieder sind derzeit online:",player,50,200,255)
-		local admins = table.copy(self.m_OnlineAdmins)
-		table.sort(admins, function(a, b) return self.m_RankNames[a] < self.m_RankNames[b] end)
-		for key, value in ipairs(admins) do
+		for key, value in pairs(self.m_OnlineAdmins) do
 			outputChatBox(("%s #ffffff%s"):format(self.m_RankNames[value], key:getName()),player, unpack(self.m_RankColors[value]))
 		end
 	else
