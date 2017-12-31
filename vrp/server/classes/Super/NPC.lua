83bdf590436ba8b523dada1b7f676400cb0f5ee4
MasterM <masterm4yt@gmail.com>
2017-12-31 20:15:46 +0100

    fixed ShopNPC event handler issue

--- vrp/server/classes/Super/NPC.lua
+++ vrp/server/classes/Super/NPC.lua
@@ -30,6 +30,6 @@ function NPC:toggleWanteds(state)
 end
 
 function NPC:onWasted(ammo, killer, weapon, bodypart, stealth)
-	killer:giveWanteds(3)
+	killer:giveWanteds(4)
 	killer:sendMessage("Verbrechen begangen: Mord, 3 Wanteds", 255, 255, 0)
 end
