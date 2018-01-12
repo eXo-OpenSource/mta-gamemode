0d6c329f92fac903f51fe9b2980e4163c46ef586
Str0 <devcxo@gmail.com>
2018-01-12 01:54:46 +0100

    Add #035ecd4e9

--- vrp/server/classes/Discord.lua
+++ vrp/server/classes/Discord.lua
@@ -16,16 +16,20 @@ function Discord:destructor()
 end
 
 function Discord:outputBreakingNews( text ) 
-	local postData = 
-	{
-		queueName = "Discord Breaking-News",
-		connectionAttempts = 3,
-		connectTimeout = 5000,
-		formFields = 
+	if not DEBUG then
+		local postData = 
 		{
-			content=text,
-			username="Breaking News",
-		},
-	}
-	fetchRemote ( "https://discordapp.com/api/webhooks/401093257481682944/xX4jy3rTLFByTtGoXJeT8efFcJhLlh09ny0DS3xvLHIlBvYl0-z_cbmcY8cUXEVRnVbv", postData, function() end )
+			queueName = "Discord Breaking-News",
+			connectionAttempts = 3,
+			connectTimeout = 5000,
+			formFields = 
+			{
+				content=text,
+				username="Breaking News",
+			},
+		}
+		fetchRemote ( "https://discordapp.com/api/webhooks/401093257481682944/xX4jy3rTLFByTtGoXJeT8efFcJhLlh09ny0DS3xvLHIlBvYl0-z_cbmcY8cUXEVRnVbv", postData, function() end )
+	else 
+		outputDebugString("Discord Breaking-News was not sent ( Debug-Mode )", 3)
+	end
 end
