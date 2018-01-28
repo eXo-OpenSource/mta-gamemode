c38b26faaacd25ce4ad3ff562e6ac6e03600b1ef
MegaThorx <megathorx@gmail.com>
2018-01-28 23:07:47 +0100

    Fixed bug with shops

--- vrp/server/classes/Gameplay/Shops/VehicleShop.lua
+++ vrp/server/classes/Gameplay/Shops/VehicleShop.lua
@@ -16,7 +16,7 @@ function VehicleShop:constructor(id, name, marker, npc, spawn, image, owner, pri
 	self.m_Money = money
 	self.m_BankAccountServer = BankServer.get("server.vehicle_shop")
 
-	self.m_BankAccount = BankAccount.loadByOwner(self.m_Id, BankAccountTypes.Shop)
+	self.m_BankAccount = BankAccount.loadByOwner(self.m_Id, BankAccountTypes.VehicleShop)
 
 	if not self.m_BankAccount then
 		self.m_BankAccount = BankAccount.create(BankAccountTypes.VehicleShop, self.m_Id)
