98bf0ac83080b9919a99e72401b9329fa5b036a5
MegaThorx <megathorx@gmail.com>
2018-04-10 19:10:33 +0200

    Fixed WebPanel, added url for whitelisting

--- vrp/client/classes/GUIForms/AdminGUI.lua
+++ vrp/client/classes/GUIForms/AdminGUI.lua
@@ -169,7 +169,7 @@ function AdminGUI:constructor(money)
 	self:refreshOnlinePlayers()
 
 	local tabWeb = self.m_TabPanel:addTab(_"WebPanel")
-	local webPanelUrl = (INGAME_WEB_PATH .. "/index.php?page=admin&site=ingame&player=%s&sessionID=%s"):format(localPlayer:getName(), localPlayer:getSessionId())
+	local webPanelUrl = ("https://exo-reallife.de/index.php?page=admin&site=ingame&player=%s&sessionID=%s"):format(localPlayer:getName(), localPlayer:getSessionId())
 	self.m_WebPanel = GUIWebView:new(0, 0, self.m_Width, self.m_Height, webPanelUrl, true, tabWeb)
 	self.m_FullScreen = GUIButton:new(self.m_Width-50, 5, 30, 30, FontAwesomeSymbols.Expand, tabWeb):setFont(FontAwesome(15))
 	self.m_FullScreen.onLeftClick = function ()
