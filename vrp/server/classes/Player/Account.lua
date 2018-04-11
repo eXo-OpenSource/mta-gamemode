e784d93c6f9942ab656235982c8574b0c451e0f8
MegaThorx <megathorx@gmail.com>
2018-04-11 18:37:14 +0200

    Fixed session id for ingame web

--- vrp/server/classes/Player/Account.lua
+++ vrp/server/classes/Player/Account.lua
@@ -173,7 +173,18 @@ function Account.loginSuccess(player, Id, Username, ForumId, RegisterDate, Teams
 
 	StatisticsLogger:addLogin( player, Username, "Login")
 	ClientStatistics:getSingleton():handle(player)
-	player:triggerEvent("loginsuccess", pwhash)
+	
+	if player:isActive() then
+		local header = toJSON({["alg"] = "HS256", ["typ"] = "JWT"}, true):sub(2, -2)
+		local payload = toJSON({["sub"] = player:getId(), ["name"] = player:getName(), ["exp"] = getRealTime().timestamp + 60 * 60 * 24}, true):sub(2, -2)
+
+		local jwtBase = base64Encode(header) .. "." .. base64Encode(payload)
+
+		fetchRemote(INGAME_WEB_PATH .. "/ingame/hmac.php?value=" .. jwtBase, function(responseData) 
+			player:setSessionId(jwtBase.."."..responseData)
+			player:triggerEvent("loginsuccess", pwhash)
+		end)
+	end
 end
 
 function Account.checkCharacter(Id)
