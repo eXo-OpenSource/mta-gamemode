cc3ccf0fbb7363d719bf81bb719f8446022ce1d5
Str0 <devcxo@gmail.com>
2018-04-26 22:42:19 +0200

    Add #93c1d7c4b

--- vrp/shared/debug.lua
+++ vrp/shared/debug.lua
@@ -8,7 +8,7 @@
 --- Validates the parameters of a function
 -- @param funcName The name of the function
 -- @param ... The parameters' types
-
+DEBUG = GIT_BRANCH ~= "release/production"
 if DEBUG then --important: DEBUG_-settings should always have a default value of false as this would be the case on release/prod.
 	DEBUG_LOAD_SAVE = false -- defines if "loaded X"-messages are outputted to the server console
 	DEBUG_AUTOLOGIN = not GIT_VERSION and true -- logs the player in automatically if they saved their pw
