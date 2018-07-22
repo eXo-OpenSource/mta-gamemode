41af71b70145f36c1c7dc3023550b5cbb88f5b19
MegaThorx <megathorx@gmail.com>
2018-07-22 13:22:06 +0200

    Reworked exo points and added new locations

--- vrp/client/classes/Collectables.lua
+++ vrp/client/classes/Collectables.lua
@@ -10,35 +10,31 @@ Collectables = inherit(Singleton)
 addEvent("reciveCollectables", true)
 
 function Collectables:constructor()
-	self.m_Pickups = {}
+    self.m_Pickups = {}
 
-	-- Request for pickup positions
-	triggerServerEvent("requestCollectables", localPlayer)
+    -- Request for pickup positions
+    triggerServerEvent("requestCollectables", localPlayer)
 
 
-	addEventHandler("reciveCollectables", root, bind(self.reciveCollectables,self))
-	addEventHandler("onClientPickupHit" , root, bind(self.onCollectableHit,self))
+    addEventHandler("reciveCollectables", root, bind(self.reciveCollectables,self))
+    addEventHandler("onClientPickupHit" , root, bind(self.onCollectableHit,self))
 end
 
 function Collectables:onCollectableHit(hitElement)
-	if hitElement ~= localPlayer then return end
+    if hitElement ~= localPlayer then return end
 
-	for key, value in ipairs(self.m_Pickups) do
-		if value == source then
-			if localPlayer:getDimension() == PRIVATE_DIMENSION_SERVER then return end
-			triggerServerEvent("checkCollectableHit",localPlayer,key)
-			setElementDimension(value,PRIVATE_DIMENSION_SERVER)
-		end
-	end
+    for key, value in pairs(self.m_Pickups) do
+        if value == source then
+            triggerServerEvent("checkCollectableHit", localPlayer, key)
+            self.m_Pickups[key]:destroy()
+        end
+    end
 end
 
-function Collectables:reciveCollectables(positions,progress)
-	for key, value in ipairs(positions) do
-		self.m_Pickups[key] = createPickup(value[1],value[2],value[3],3,2836)
-		if progress[tostring(key)] == "1" then
-			setElementDimension(self.m_Pickups[key],PRIVATE_DIMENSION_SERVER)
-		end
-	end
+function Collectables:reciveCollectables(positions)
+	for key, value in pairs(positions) do
+        self.m_Pickups[key] = createPickup(value[1], value[2], value[3], 3, 2836)
+    end
 end
 
 function Collectables:destructor()
--- vrp/server/classes/Gameplay/Collectables.lua
+++ vrp/server/classes/Gameplay/Collectables.lua
@@ -10,58 +10,75 @@ Collectables = inherit(Singleton)
 addEvent("requestCollectables", true)
 addEvent("checkCollectableHit", true)
 
+Collectables.COLLECT_COUNT = 40
+
 function Collectables:constructor()
-	addEventHandler("requestCollectables", root, bind(self.sendCollectables,self))
-	addEventHandler("checkCollectableHit", root, bind(self.checkCollectable,self))
+    addEventHandler("requestCollectables", root, bind(self.sendCollectables,self))
+    addEventHandler("checkCollectableHit", root, bind(self.checkCollectable,self))
+    self.m_Collectables = {}
+	self.m_CollectablesIds = {}
+
+    local result = sql:queryFetch("SELECT * FROM ??_collectables", sql:getPrefix())
+
+    for k, v in ipairs(result) do
+        self.m_Collectables[v.Id] = {v.PosX, v.PosY, v.PosZ}
+        table.insert(self.m_CollectablesIds, v.Id)
+    end
 end
 
 function Collectables:checkCollectable(collectableID)
-	if not client then return end
-	if client:getDimension() == PRIVATE_DIMENSION_SERVER then return end
-	local x,y,z = getElementPosition(client)
-	local px,py,pz = unpack(Collectables.POSITIONS[collectableID])
-	if getDistanceBetweenPoints3D (x,y,z,px,py,pz) >= 10 then
-		print(("WARNING: %s is maybe cheatin' @ collectables"):format(getPlayerName(client)))
-	else
-		local collectables = client:getCollectables() or {}
-		collectables[collectableID] = "1"
-		client:setCollectables(collectables)
+    if not client then return end
+    local x, y, z = getElementPosition(client)
+	local px, py, pz = unpack(self.m_Collectables[collectableID])
 
-		client:sendShortMessage(_("Du hast ein eXo-Logo gefunden!\nDafür erhälst du %s eXo-Points!", client, 250))
-		client:givePoints(250)
+    if getDistanceBetweenPoints3D(x, y, z, px, py, pz) >= 10 then
+        print(("WARNING: %s is maybe cheatin' @ collectables"):format(getPlayerName(client)))
+    else
+		local collectables = client:getCollectables()
+
+		if (table.find(collectables.collectable, collectableID)) then
+			table.removevalue(collectables.collectable, collectableID)
+			table.insert(collectables.collected, collectableID)
+			client:setCollectables(collectables)
+
+			client:sendShortMessage(_("Du hast ein eXo-Logo gefunden!\nDafür erhälst du %s eXo-Points!", client, 250))
+			client:givePoints(250)
+			sql:queryExec("UPDATE ??_collectables SET CollectCount = CollectCount + 1 WHERE Id = ?", sql:getPrefix(), collectableID)
+		else
+			print(("WARNING: %s is maybe cheatin, already collected?' @ collectables"):format(getPlayerName(client)))
+		end
+    end
+end
+
+function Collectables:generateForPlayer(player)
+    local collectables = table.deepcopy(self.m_CollectablesIds)
+    local collectablesForPlayer = {
+		collectable = {},
+		collected = {}
+	}
+
+    for i = 1, Collectables.COLLECT_COUNT do
+        local item = math.random(1, table.size(collectables))
+        table.insert(collectablesForPlayer.collectable, item)
+        table.remove(collectables, item)
 	end
+
+	player:setCollectables(collectablesForPlayer)
+	return collectablesForPlayer
 end
 
 function Collectables:sendCollectables()
-	if not client then return end
+    if not client then return end
 
-	triggerClientEvent(client,"reciveCollectables",client,Collectables.POSITIONS,client:getCollectables() or {})
+    if not client:getCollectables() or table.size(client:getCollectables()) ~= 2 then
+        self:generateForPlayer(client)
+	end
+
+	local collectable = {}
+
+	for k, v in ipairs(client:getCollectables().collectable) do
+		collectable[v] = self.m_Collectables[v]
+	end
+
+    triggerClientEvent(client, "reciveCollectables", client, collectable)
 end
-
-Collectables.POSITIONS = {
-	{1117.60, -2037.10, 78.80},
-	{1118.50, -1619.70, 20.5},
-	{1349.60, -1665.50, 13.5},
-	{1681.40, -1673.00, 20},
-	{1691.30, -1951.90, 8.2},
-	{1474.40, -2256.20, -3},
-	{2370.30, -2544.00, 3},
-	{2661.90, -1441.50, 16.30},
-	{1971.20, -1157.00, 20.7},
-	{1295.80, -985.30 , 32.5},
-	{741.00 , -1018.00, 42.5},
-	{854.00 , -1386.80, -0.5},
-	{325.70 , -1495.60, 24.6},
-	{401.90 , -2066.10, 10.5},
-	{2955.60, -1486.50, 1.4},
-	{2162.00, -103.00 , 2.4},
-	{-2235.0, -1743.10, 481},
-	{-47.80 , 29.60   , 6.1},
-	{-210.50, 1770.00 , 101.3},
-	{2186.70, 2416.50 , 72.8},
-	{1954.50, 1343.00 , 15},
-	{-2237.2, 2467.20 , 4.9},
-	{-1426.8, 1490.00 , 1.6},
-	{-2079.7, 1065.00 , 65.6},
-	{-2481.7, -284.70 , 40.5}
-}
