f119381d71697b35cc0db55326e5a74b8928326e
Florian <>
2018-07-27 21:09:36 +0200

    Allow to store weapons as member of an evil faction

--- vrp/server/classes/Player/PlayerManager.lua
+++ vrp/server/classes/Player/PlayerManager.lua
@@ -815,7 +815,7 @@ function PlayerManager:Event_requestGunBoxData()
 end
 
 function PlayerManager:Event_gunBoxAddWeapon(weaponId, muni)
-	if client:getFaction() and client:isFactionDuty() then
+	if client:getFaction() and client:getFaction():isStateFaction() and client:isFactionDuty() then
 		client:sendError(_("Du darfst im Dienst keine Waffen einlagern!", client))
 		return
 	end
