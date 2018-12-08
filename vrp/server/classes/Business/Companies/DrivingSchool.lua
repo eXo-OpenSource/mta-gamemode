76fb97d8b8ec41a5da125a5fbe2bb07bccdea8c9
Snake <ic3reflux@gmail.com>
2018-12-08 12:51:52 +0100

    Add full percentage of money for driving school

--- vrp/server/classes/Business/Companies/DrivingSchool.lua
+++ vrp/server/classes/Business/Companies/DrivingSchool.lua
@@ -415,7 +415,7 @@ function DrivingSchool:startLession(instructor, target, type)
 						}
 
 						target:transferMoney(self.m_BankAccountServer, costs, ("%s-Prüfung"):format(DrivingSchool.TypeNames[type]), "Company", "License")
-						self.m_BankAccountServer:transferMoney({self, nil, true}, costs*0.7, ("%s-Prüfung"):format(DrivingSchool.TypeNames[type]), "Company", "License")
+						self.m_BankAccountServer:transferMoney({self, nil, true}, costs*0.85, ("%s-Prüfung"):format(DrivingSchool.TypeNames[type]), "Company", "License")
 						self.m_BankAccountServer:transferMoney(instructor, costs*0.15, ("%s-Prüfung"):format(DrivingSchool.TypeNames[type]), "Company", "License")
 
                         target:setPublicSync("inDrivingLession",true)
