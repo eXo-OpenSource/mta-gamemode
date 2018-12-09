612bfb854d0ae6f6fe61923d491448ef058d145e
gatno <danieltaucher@gmail.com>
2018-12-09 03:54:27 +0100

    Fixed Bomb place issue

--- vrp/server/classes/Gameplay/BombArea.lua
+++ vrp/server/classes/Gameplay/BombArea.lua
@@ -47,6 +47,11 @@ function BombArea:fire(player)
 		return
 	end
 
+    if not player:getFaction():isEvilFaction() then
+		player:sendError("Nur Spieler in bösen Fraktionen können Bomben legen!")
+		return
+	end
+
     self.m_BombObject = createObject(1654, player:getPosition() + Vector3(0, 0, -0.9), 270, 0, 0)
     self.m_BombObject:setInterior(player:getInterior())
     self.m_BombObject:setDimension(player:getDimension())
