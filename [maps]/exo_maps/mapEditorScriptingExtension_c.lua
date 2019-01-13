9284575cc85a2892ceebfb1a91817a799faf5818
HorrorClown <admin@pewx.de>
2019-01-13 16:22:49 +0100

    Glass at rescue base is now unbreakable

--- [maps]/exo_maps/mapEditorScriptingExtension_c.lua
+++ [maps]/exo_maps/mapEditorScriptingExtension_c.lua
@@ -2,15 +2,24 @@
 -- PURPOSE:	Prevent the map editor feature set being limited by what MTA can load from a map file by adding a script file to maps
 -- VERSION:	RemoveWorldObjects (v1) AutoLOD (v1)
 
-function requestLODsClient()
+local function requestLODsClient()
 	triggerServerEvent("requestLODsClient", resourceRoot)
+	triggerServerEvent("requestBreakablesClient", resourceRoot)
 end
 addEventHandler("onClientResourceStart", resourceRoot, requestLODsClient)
 
-function setLODsClient(lodTbl)
-	for i, model in ipairs(lodTbl) do
+local function setLODsClient(lodTbl)
+	for i, model in pairs(lodTbl) do
 		engineSetModelLODDistance(model, 300)
 	end
 end
 addEvent("setLODsClient", true)
 addEventHandler("setLODsClient", resourceRoot, setLODsClient)
+
+local function setBreakablesClient(breakableTbl)
+	for i, model in pairs(breakableTbl) do
+		setObjectBreakable(model, false)
+	end
+end
+addEvent("setBreakablesClient", true)
+addEventHandler("setBreakablesClient", resourceRoot, setBreakablesClient)
