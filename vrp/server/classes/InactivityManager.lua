180871ba9fc67cb6ba2d16cc281f0d13ec0b815d
Snake <ic3reflux@gmail.com>
2019-03-27 17:25:42 +0100

    Add Inactivity-System for GroupProperties

--- vrp/server/classes/InactivityManager.lua
+++ vrp/server/classes/InactivityManager.lua
@@ -9,6 +9,7 @@ InactivityManager = inherit(Singleton)
 
 function InactivityManager:constructor()
 	self:clearHouses()
+	self:clearProperties()
 end
 
 function InactivityManager:clearHouses()
@@ -26,6 +27,26 @@ function InactivityManager:clearHouses()
 
 			sqlLogs:queryExec("INSERT INTO ??_HousesFreed (Date, UserID, HouseID) VALUES (Now(), ?, ?)",
 				sqlLogs:getPrefix(), v.UserID, v.HouseID)
+		end
+	end
+end
+
+function InactivityManager:isGroupInactive(id)
+	local result, numrows = sql:queryFetch("SELECT ac.LastLogin FROM ??_character ch INNER JOIN ??_account ac ON ac.Id = ch.Id AND ch.GroupId = ? WHERE ac.LastLogin > DATE_SUB(DATE(NOW()), INTERVAL 2 MONTH)", sql:getPrefix(), sql:getPrefix(), id)
+	if numrows == 0 then
+		return true
+	else
+		return false
+	end
+end
+
+function InactivityManager:clearProperties()
+	local result = sql:queryFetch("SELECT Id, GroupId, Price FROM ??_group_property", sql:getPrefix())
+	for _, row in ipairs(result) do
+		if row.GroupId ~= 0 then
+			if self:isGroupInactive(row.GroupId) then
+				GroupPropertyManager:getSingleton():clearProperty(row.Id, row.GroupId, row.Price)
+			end
 		end
 	end
 end
