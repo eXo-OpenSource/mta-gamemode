7e63e8a18ca23c12ba42cefd08b288aae7453631
Strobe <devcxo@gmail.com>
2019-08-12 06:46:55 +0200

    Adjust Triad-Base Col-Shape

--- vrp/client/classes/LocalPlayer.lua
+++ vrp/client/classes/LocalPlayer.lua
@@ -78,7 +78,7 @@ function LocalPlayer:constructor()
 	local col2 = createColRectangle(2401.71, -2190.33, 210, 150) -- mech and tow
 	self.m_NoOcclusionZone = NonOcclusionZone:new(col2)
 
-	local col3 =  createColRectangle( 1875.939, 939.491, 60, 80) -- triad base
+	local col3 =  createColRectangle( 1837.515,  902.93, 160, 120) -- triad base
 	self.m_NoOcclusionZone = NonOcclusionZone:new(col3)
 	
 	local col4 = createColCuboid(2305.70, -0.12, 24.74, 2316.60-2305.70, 22.43, 5 ) -- palo bank
