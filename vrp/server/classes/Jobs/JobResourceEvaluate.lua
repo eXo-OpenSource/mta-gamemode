20a33668a9bd70d381703095daf374059acaa6d1
Strobe <devcxo@gmail.com>
2019-08-14 00:04:10 +0200

    Remove JobServiceTechnician; Add Manager/Evaluate

---
+++ vrp/server/classes/Jobs/JobResourceEvaluate.lua
@@ -0,0 +1,17 @@
+-- ****************************************************************************
+-- *
+-- *  PROJECT:     vRoleplay
+-- *  FILE:        server/classes/Jobs/JobResourceManager.lua
+-- *  PURPOSE:     Job-Resource Manager
+-- *
+-- ****************************************************************************
+
+JobResourceEvaluate = inherit(Singleton)
+
+function JobResourceEvaluate:constructor() 
+	
+end
+
+function JobResourceEvaluate:destructor()
+
+end
---
+++ vrp/server/classes/Jobs/JobResourceManager.lua
@@ -0,0 +1,17 @@
+-- ****************************************************************************
+-- *
+-- *  PROJECT:     vRoleplay
+-- *  FILE:        server/classes/Jobs/JobResourceManager.lua
+-- *  PURPOSE:     Job-Resource Manager
+-- *
+-- ****************************************************************************
+
+JobResourceManager = inherit(Singleton)
+
+function JobResourceManager:constructor() 
+	
+end
+
+function JobResourceManager:destructor()
+
+end
