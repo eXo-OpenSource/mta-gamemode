e4787f146754e16ed048ca119f5de48d095b48fc
MegaThorx <megathorx@gmail.com>
2020-01-16 18:55:01 +0100

    Fix bug with stretcher

--- vrp/client/classes/GUIForms/VehicleInteraction.lua
+++ vrp/client/classes/GUIForms/VehicleInteraction.lua
@@ -97,7 +97,7 @@ function VehicleInteraction:render()
 								end
                             elseif doorId == 0 then
                                 self:drawTextBox(_("#FFFFFFDrücke #FF0000 %s #FFFFFF um den Motor zu reparieren!", self.m_actionButton), 1)
-                            elseif vehicleModel == 416 then
+                            elseif ((vehicleModel == 416 or vehicleModel == 497) and (doorId == 4 or doorId == 5)) or (vehicleModel == 563 and door == 3) then
                                 if doorId == 4 or doorId == 5 then
 									if localPlayer:getPublicSync("Faction:Duty") and localPlayer:getPublicSync("Rescue:Type") == "medic" then
 										self:drawTextBox(_("#FFFFFFDrücke #00FF00 %s #FFFFFF zum ein- oder ausladen der Trage!", self.m_actionButton), 1)
@@ -197,7 +197,7 @@ function VehicleInteraction:action()
                         end
                     end
                 end
-			elseif ((door == 4 or door == 5) and (vehicleModel == 416 or vehicleModel == 497)) or (door == 3 and vehicleModel == 417) then
+			elseif ((door == 4 or door == 5) and (vehicleModel == 416 or vehicleModel == 497)) or (door == 3 and vehicleModel == 563) then
                 if doorRatio > 0 and localPlayer:getPublicSync("Faction:Duty") and localPlayer:getPublicSync("Rescue:Type") == "medic" then
 					self.m_LastInteraction = getTickCount()
 					triggerServerEvent("factionRescueToggleStretcher", localPlayer, self.m_lookAtVehicle)
