773b0cdba90766e44c326fd84aaf6807f10645b3
Snake <ic3reflux@gmail.com>
2020-04-06 00:54:37 +0200

    Temporarily deactivate Deathmatch-Lobbys

--- vrp/server/classes/Gameplay/Deathmatch/DeathmatchManager.lua
+++ vrp/server/classes/Gameplay/Deathmatch/DeathmatchManager.lua
@@ -91,6 +91,7 @@ function DeathmatchManager:constructor()
 
 	self:loadServerLobbys()
 	self.m_BankServer = BankServer.get("gameplay.deathmatch")
+	--[[
 	local b = Blip:new("SniperGame.png", 1327.88, -1556.25)
 	b:setDisplayText("Paintball-Arena", BLIP_CATEGORY.Leisure)
 	self.m_Marker = createMarker(1327.88, -1556.25, 13.55, "corona", 2, 255, 125, 0)
@@ -99,7 +100,7 @@ function DeathmatchManager:constructor()
 			hitElement:triggerEvent("deathmatchOpenLobbyGUI")
 		end
 	end)
-
+	]]
 	PlayerManager:getSingleton():getWastedHook():register(
 		function(player, killer, weapon)
 			if player.deathmatchLobby then
