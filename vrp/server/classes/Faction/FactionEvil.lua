5ad864c2bf3bf927a22566f3e742f7a8dda758d8
Snake <ic3reflux@gmail.com>
2020-05-16 17:15:12 +0200

    Remove Brigada Gate

--- vrp/server/classes/Faction/FactionEvil.lua
+++ vrp/server/classes/Faction/FactionEvil.lua
@@ -17,7 +17,7 @@ function FactionEvil:constructor()
 	self.m_Raids = {}
 
 	nextframe(function()
-		self:loadLCNGates(12)
+		--self:loadLCNGates(12)
 		--self:loadCartelGates(11)
 		self:loadYakGates(6)
 		--self:loadTriadGates(11)
