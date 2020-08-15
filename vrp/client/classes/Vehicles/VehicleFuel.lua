a6bce4b123a1758bf4cb104a794fe619390a065f
MasterM <masterm4yt@gmail.com>
2020-08-15 17:01:46 +0200

    fix vehicle fuel display

--- vrp/client/classes/Vehicles/VehicleFuel.lua
+++ vrp/client/classes/Vehicles/VehicleFuel.lua
@@ -30,7 +30,7 @@ function VehicleFuel:constructor(vehicle, confirmCallback, confirmWithSpace, gas
 	Cursor:getHook():register(self.m_StopInteraction)
 
 	local pos = vehicle:getPosition()
-	local __,__,__,__,__,bbz2 = vehicle:getBoundingBox()
+	local __,__,__,__,__,bbz2 = getElementBoundingBox(vehicle)
 	pos = pos + vehicle.matrix.up*(bbz2 + 0.5)
 	GUIForm3D.constructor(self, pos, vehicle:getRotation(), Vector2(1, 0.34), Vector2(200,70), 30, true)
 	self.m_ShortMessage = ShortMessage:new(_("Halte die Linke Maustaste gedrückt und schaue dabei auf das Fahrzeug, um zu tanken (der Mauszeiger darf nicht aktiv sein). %s", confirmWithSpace and "\nDrücke die Leertaste, um den Kunden zum Bezahlen aufzufordern." or ""), "Fahrzeug befüllen", {230, 100, 0}, -1)
