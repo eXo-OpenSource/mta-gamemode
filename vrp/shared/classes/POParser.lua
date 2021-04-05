2d3fe5a721b27b6274c9f627b8546c59d5341f95
MegaThorx <git@merx.dev>
2021-04-05 15:06:02 +0200

    Improve POParser

--- vrp/shared/classes/POParser.lua
+++ vrp/shared/classes/POParser.lua
@@ -12,7 +12,7 @@ function POParser:constructor(poPath)
 
 	local file = File.Open(poPath, true)
 	local lines = split(assert(file:getContent(), "Reading the translation file failed"), "\n")
-	
+	--[[
 	local lastKey
 	for i, line in ipairs(lines) do
 		local pos = line:find(' ')
@@ -33,6 +33,35 @@ function POParser:constructor(poPath)
 			end
 		end
 	end
+	]]
+
+	local lastKey
+	local lastInstruction
+	for i, line in ipairs(lines) do
+		if line:sub(0, 5) == "msgid" then
+			lastInstruction = line:sub(0, 5)
+			lastKey = line:sub(8, -3)
+			self.m_Strings[lastKey] = false
+		elseif line:sub(0, 6) == "msgstr" then
+			lastInstruction = line:sub(0, 6)
+			self.m_Strings[lastKey] = line:sub(9, -3)
+		elseif line:sub(0, 1) == "\"" then
+			local value = line:sub(2, -3)
+			if lastKey then
+				if self.m_Strings[lastKey] then
+					self.m_Strings[lastKey] = self.m_Strings[lastKey] .. value
+				else
+					self.m_Strings[lastKey] = value
+				end
+			end
+		else
+			outputConsole(tostring(lastKey) .. " - " .. tostring(self.m_Strings[lastKey]))
+			lastKey = nil
+			lastInstruction = nil
+		end
+	end
+
+
 	file:close()
 end
 
