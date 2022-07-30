7f5dd0aeaf833e100fce41c1cb3f43d8697c25a0
Maurice <maurice009mta@gmail.com>
2022-07-30 18:03:25 +0200

    Add wasted and afk hook for ColorCars, Prevent placing objects and join lobby while duty

--- vrp/server/classes/Inventory/Items/Item.lua
+++ vrp/server/classes/Inventory/Items/Item.lua
@@ -55,8 +55,8 @@ function Item:startObjectPlacing(player, callback, hideObject, customModel)
 		player:sendError(_("Du kannst hier keine Objekte platzieren.", player)) 
 		return false
 	end
-	if player:isDead() then
-		player:sendError(_("Du kannst keine Objekte platzieren", player))
+	if player:isDead() or player:getData("isInDeathMatch") or player:getData("isInColorCars") then
+		player:sendError(_("Du kannst keine Objekte platzieren.", player))
 	end
 
 	-- Start the object placer on the client
